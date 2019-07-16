577373f10793ffa6cb3eff2366e7cfd42442f14e
shreyas <sharora@gmail.com>
2019-07-15 21:26:26 -0700

    Finished testing loop speed on sdk 5.0. 50-53 hz w/4 motor writes & 1 bulk read, maybe sufficient but not optimal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LoopTimeTest.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.hardware.lynx.commands.core.*;
+
+@Autonomous
+public class LoopTimeTest extends LinearOpMode {
+
+
+    // declaring all hardware objects
+    private BNO055IMU imu;
+    private DcMotorEx m1;
+    private DcMotorEx m2;
+    private DcMotorEx m3;
+    private DcMotorEx m4;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        m1 = hardwareMap.get(DcMotorEx.class, "m1");
+        m2 = hardwareMap.get(DcMotorEx.class, "m2");
+        m3 = hardwareMap.get(DcMotorEx.class, "m3");
+        m4 = hardwareMap.get(DcMotorEx.class, "m4");
+
+        LynxModule module = hardwareMap.getAll(LynxModule.class).iterator().next();
+
+        waitForStart();
+
+        //code that executes in match
+
+        ElapsedTime time = new ElapsedTime();
+        double average = 0.0;
+        double count = 0;
+
+        while(opModeIsActive()){
+
+            count += 1;
+            LynxGetBulkInputDataResponse response;
+            try {
+                LynxGetBulkInputDataCommand command = new LynxGetBulkInputDataCommand(module);
+                response = command.sendReceive();
+            }
+            catch (Exception e) {
+                telemetry.addData("Exception", "bulk read exception");
+            }
+
+            double randomPow = Math.random();
+            m1.setPower(randomPow);
+            m2.setPower(randomPow);
+            m3.setPower(randomPow);
+            m4.setPower(randomPow);
+
+            average += (time.seconds() - average)/count;
+
+            telemetry.addData("update frequency", 1.0/average);
+            telemetry.update();
+
+            time.reset();
+
+        }
+
+
+    }
+}

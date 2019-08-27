909fee289b931f384c650bc30fbd6cafe85a6988
Windy Feng <1745500559@qq.com>
2019-08-27 09:44:30 +0800

    Add DarbotsFTCLib Examples

---
+++ TeamCode/src/main/java/org/darbots/darbotsftclib/testcases/OmniDriveTest/OmniCore.java
@@ -0,0 +1,65 @@
+package org.darbots.darbotsftclib.testcases;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.darbots.darbotsftclib.libcore.calculations.dimentionalcalculation.Robot2DPositionIndicator;
+import org.darbots.darbotsftclib.libcore.chassiscontrollers.OmniDrive;
+import org.darbots.darbotsftclib.libcore.motortypes.RevHDHex20Motor;
+import org.darbots.darbotsftclib.libcore.sensors.motion_related.RobotMotion;
+import org.darbots.darbotsftclib.libcore.sensors.motion_related.RobotWheel;
+import org.darbots.darbotsftclib.libcore.sensors.motors.RobotMotorController;
+import org.darbots.darbotsftclib.libcore.sensors.motors.RobotMotorWithEncoder;
+import org.darbots.darbotsftclib.libcore.templates.RobotCore;
+import org.darbots.darbotsftclib.libcore.templates.motion_related.RobotMotionSystem;
+import org.darbots.darbotsftclib.libcore.templates.motor_related.MotorType;
+
+public class OmniCore extends RobotCore {
+    private static double[] WHEEL_POS = {15,15}; //{X, Z}
+    private static double WHEEL_RADIUS = 5;
+    private static boolean TIMEOUT_ENABLED = true;
+    private static double TIMEOUT_FACTOR = 1.5;
+    private static MotorType MOTOR_TYPE = new RevHDHex20Motor();
+
+
+    private RobotMotion m_LeftTopMotion, m_LeftBottomMotion, m_RightTopMotion, m_RightBottomMotion;
+    private OmniDrive m_Drive;
+
+    public OmniCore(HardwareMap Hardwares) {
+        super("OmniDriveTest.log");
+        this.getLogger().setDebugOn(true);
+        DcMotor m_LeftTopDC = Hardwares.dcMotor.get("LT");
+        DcMotor m_LeftBottomDC = Hardwares.dcMotor.get("LB");
+        DcMotor m_RightTopDC = Hardwares.dcMotor.get("RT");
+        DcMotor m_RightBottomDC = Hardwares.dcMotor.get("RB");
+        RobotWheel m_LeftTopWheel = new RobotWheel(new Robot2DPositionIndicator(-WHEEL_POS[0],WHEEL_POS[1],45),WHEEL_RADIUS);
+        RobotWheel m_RightTopWheel = new RobotWheel(new Robot2DPositionIndicator(WHEEL_POS[0],WHEEL_POS[1],-45),WHEEL_RADIUS);
+        RobotWheel m_LeftBottomWheel = new RobotWheel(new Robot2DPositionIndicator(-WHEEL_POS[0],-WHEEL_POS[1],135),WHEEL_RADIUS);
+        RobotWheel m_RightBottomWheel = new RobotWheel(new Robot2DPositionIndicator(WHEEL_POS[0],-WHEEL_POS[1],-135),WHEEL_RADIUS);
+        this.m_LeftTopMotion = new RobotMotion(new RobotMotorController(new RobotMotorWithEncoder(m_LeftTopDC,MOTOR_TYPE),TIMEOUT_ENABLED,TIMEOUT_FACTOR),m_LeftTopWheel);
+        this.m_LeftBottomMotion = new RobotMotion(new RobotMotorController(new RobotMotorWithEncoder(m_LeftBottomDC,MOTOR_TYPE),TIMEOUT_ENABLED,TIMEOUT_FACTOR),m_LeftBottomWheel);
+        this.m_RightTopMotion = new RobotMotion(new RobotMotorController(new RobotMotorWithEncoder(m_RightTopDC,MOTOR_TYPE),TIMEOUT_ENABLED,TIMEOUT_FACTOR),m_RightTopWheel);
+        this.m_RightBottomMotion = new RobotMotion(new RobotMotorController(new RobotMotorWithEncoder(m_RightBottomDC,MOTOR_TYPE),TIMEOUT_ENABLED,TIMEOUT_FACTOR),m_RightBottomWheel);
+        this.m_Drive = new OmniDrive(this.m_LeftTopMotion,this.m_RightTopMotion,this.m_LeftBottomMotion,this.m_RightBottomMotion,null);
+    }
+
+    @Override
+    public void stop() {
+        this.m_Drive.deleteAllTasks();
+    }
+
+    @Override
+    public RobotMotionSystem getChassis() {
+        return this.m_Drive;
+    }
+
+    @Override
+    public boolean isBusy() {
+        return false;
+    }
+
+    @Override
+    public void updateStatus() {
+        m_Drive.updateStatus();
+    }
+}
---
+++ TeamCode/src/main/java/org/darbots/darbotsftclib/testcases/OmniDriveTest/OmniTeleOpDarbots.java
@@ -0,0 +1,37 @@
+package org.darbots.darbotsftclib.testcases;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.darbots.darbotsftclib.libcore.OpModes.DarbotsBasicOpMode;
+import org.darbots.darbotsftclib.libcore.chassiscontrollers.OmniDrive;
+
+@TeleOp(group = "DarbotsLib-TestCases", name = "OmniTest")
+public class OmniTeleOpDarbots extends DarbotsBasicOpMode<OmniCore> {
+    private OmniCore m_RobotCore;
+    @Override
+    public OmniCore getRobotCore() {
+        return m_RobotCore;
+    }
+
+    @Override
+    public void hardwareInitialize() {
+        this.m_RobotCore = new OmniCore(this.hardwareMap);
+        m_RobotCore.getChassis().replaceTask(this.m_RobotCore.getChassis().getTeleOpTask());
+    }
+
+    @Override
+    public void hardwareDestroy() {
+        this.m_RobotCore = null;
+    }
+
+    @Override
+    public void RunThisOpMode() {
+        OmniDrive.TeleOpControlTask TeleOpTask = (OmniDrive.TeleOpControlTask) m_RobotCore.getChassis().getCurrentTask();
+        while(this.opModeIsActive()){
+            TeleOpTask.setDriveRotationSpeed(gamepad1.right_stick_x);
+            TeleOpTask.setDriveXSpeed(gamepad1.left_stick_x);
+            TeleOpTask.setDriveZSpeed(-gamepad1.left_stick_y);
+            this.getRobotCore().updateStatus();
+        }
+    }
+}

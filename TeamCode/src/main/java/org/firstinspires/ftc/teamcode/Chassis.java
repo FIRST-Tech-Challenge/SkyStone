447530ed38717b83b1d4f59bfacffa9d96cfebde
Shaunak Kale <epicshaunak@gmail.com>
2019-08-31 11:39:49 -0700

    add new classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//@Disabled
+public class Chassis {
+    /* Public OpMode members. */
+    public DcMotor leftRear = null;
+    public DcMotor rightRear = null;
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    BNO055IMU imu = null;
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        // The IMU sensor object
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Define and Initialize Motors
+        leftRear = hwMap.get(DcMotor.class, "leftRear");
+        rightRear = hwMap.get(DcMotor.class, "rightRear");
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+        leftRear.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        rightRear.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        leftFront.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        rightFront.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+
+        //Set to brake mode
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set all motors to zero power
+        leftRear.setPower(0);
+        rightRear.setPower(0);
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
+
+@Autonomous(name = "GyroTest", group = "8872")
+//@Disabled
+public class GyroTest extends LinearOpMode {
+    double currentHeading = 0;
+    Chassis robot = new Chassis();
+    Orientation angles;
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        composeTelemetry();
+        telemetry.update();
+        angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        waitForStart();
+        gyroTurn(84);
+
+        sleep(3000);
+        angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        telemetry.update();
+        sleep(10000);
+
+
+    }
+
+    public void gyroTurn(double targetAngle) {
+        double currentSpeed = 0.2;
+        angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double headingAngle = angles.firstAngle + 180;
+        if (targetAngle > 0) {
+            while (headingAngle < targetAngle && opModeIsActive()) {
+                robot.leftFront.setPower(currentSpeed);
+                robot.rightFront.setPower(-currentSpeed);
+                robot.leftRear.setPower(currentSpeed);
+                robot.rightRear.setPower(-currentSpeed);
+                angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                headingAngle = angles.firstAngle + 180;
+                telemetry.update();
+            }
+        } else if (targetAngle < 0) {
+            while (headingAngle > targetAngle && opModeIsActive()) {
+                robot.leftFront.setPower(-currentSpeed);
+                robot.rightFront.setPower(currentSpeed);
+                robot.leftRear.setPower(-currentSpeed);
+                robot.rightRear.setPower(currentSpeed);
+                angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                headingAngle = angles.firstAngle + 180;
+                telemetry.update();
+            }
+        }
+        currentHeading += targetAngle;
+        brake();
+
+
+    }
+
+    public void brake() {
+        robot.leftRear.setPower(0);
+        robot.rightRear.setPower(0);
+        robot.leftFront.setPower(0);
+        robot.rightFront.setPower(0);
+    }
+
+    public void composeTelemetry() {
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override
+                    public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                });
+
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees) {
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}

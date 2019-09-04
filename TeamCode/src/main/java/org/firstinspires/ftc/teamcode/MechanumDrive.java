17d1a4544f5e1c815c3427100a865cd2df5e66d6
Shaunak Kale <epicshaunak@gmail.com>
2019-09-04 16:25:50 -0700

    fix mechanum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name="mechanumDrive", group="8872")
+@Disabled
+public class MechanumDrive extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftRear;
+    DcMotor rightRear;
+
+    boolean slowMode = false;
+
+
+
+
+    static final double APPROACH_SPEED = 0.2;
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * Math.PI);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        System.out.println();
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightRear = hardwareMap.dcMotor.get("rightRear");
+
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightRear.setDirection(DcMotor.Direction.REVERSE);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            double r = Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
+            double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX =  gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+            if (slowMode) {
+                leftFront.setPower(v1 / 2);
+                rightFront.setPower(v2 / 2);
+                leftRear.setPower(v3 / 2);
+                rightRear.setPower(v4 / 2);
+            } else {
+                leftFront.setPower(v1);
+                rightFront.setPower(v2);
+                leftRear.setPower(v3);
+                rightRear.setPower(v4);
+            }
+
+            if (gamepad1.a) {
+                slowMode = true;
+            }
+
+            if (gamepad1.b) {
+                slowMode = false;
+            }
+
+            telemetry.addData("Slow Mode : ", slowMode);
+            telemetry.update();
+
+
+
+
+
+
+        }
+
+    }
+
+
+}

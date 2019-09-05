71bd91e182fcf681d5bb9ed2bea8db0b4085e2e7
年迈的老秋风Windy <cxcyhdyx@sina.com>
2019-09-05 13:14:03 -0400

    Add TeleOp Annotation in ServoUsingMotor Tester

--- TeamCode/src/main/java/org/darbots/darbotsftclib/testcases/ServoUsingMotorTest/ServoUsingMotorTester.java
+++ TeamCode/src/main/java/org/darbots/darbotsftclib/testcases/ServoUsingMotorTest/ServoUsingMotorTester.java
@@ -1,10 +1,13 @@
 package org.darbots.darbotsftclib.testcases.ServoUsingMotorTest;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.darbots.darbotsftclib.libcore.OpModes.DarbotsBasicOpMode;
 import org.darbots.darbotsftclib.libcore.tasks.servo_tasks.motor_powered_servo_tasks.TargetPosSpeedCtlTask;
 import org.darbots.darbotsftclib.libcore.tasks.servo_tasks.motor_powered_servo_tasks.TargetPosTask;
 import org.darbots.darbotsftclib.libcore.templates.RobotCore;
 
+@TeleOp(group = "DarbotsLib-TestCases",name="ServoUsingMotorTester")
 public class ServoUsingMotorTester extends DarbotsBasicOpMode<ServoUsingMotorCore> {
     private static final double SERVOUSINGMOTOR_SPEED = 0.2;
 

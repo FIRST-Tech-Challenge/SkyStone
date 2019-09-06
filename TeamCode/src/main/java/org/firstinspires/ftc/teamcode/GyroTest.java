20712a8d76d7d2786861f2a521138160959c13c1
robopocalypse <teamrobotics8872@gmail.com>
2019-09-06 14:50:32 -0700

    Final gyroTurn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -17,8 +19,15 @@ import java.util.Locale;
 public class GyroTest extends LinearOpMode {
     Chassis robot = new Chassis();
     Orientation angles;
-    double adjust=4;
+    double adjust = 4;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     @Override
+
     public void runOpMode() {
         robot.init(hardwareMap);
         composeTelemetry();
@@ -26,30 +35,11 @@ public class GyroTest extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
-        gyroTurn(20);
+        encoderDrive(1,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         sleep(1000);
-        normalizeAngle();
-        sleep(3000);
-        gyroTurn(0);
-        sleep(1000);
-        normalizeAngle();
-        sleep(3000);
-        gyroTurn(90);
-        sleep(1000);
-        normalizeAngle();
-        sleep(3000);
-        gyroTurn(45);
-        sleep(1000);
-        normalizeAngle();
-        sleep(3000);
-        gyroTurn(180);
-        sleep(1000);
-        normalizeAngle();
-        sleep(3000);
-        gyroTurn(0);
 
 
-        telemetry.addData("DONE", angles);
+        telemetry.addData("DONE", angles.firstAngle);
 
 
         sleep(3000);
@@ -64,14 +54,14 @@ public class GyroTest extends LinearOpMode {
         double error = 2;
         double currentSpeed = 1;
         double headingAngle = normalizeAngle();
-        double originalTargetAngle=targetAngle;
-        adjust+=Math.abs(targetAngle-headingAngle)*.012;
+        double originalTargetAngle = targetAngle;
+        adjust += Math.abs(targetAngle - headingAngle) * .012;
         if (shortestDirection(targetAngle)) {
-            targetAngle-=adjust;
-            if(targetAngle<-180) targetAngle+=360;
+            targetAngle -= adjust;
+            if (targetAngle < -180) targetAngle += 360;
             while (headingAngle > targetAngle + error / 2 || headingAngle < targetAngle - error / 2 && opModeIsActive()) {
-                if(Math.abs(targetAngle-headingAngle)<40) currentSpeed=.2;
-                else currentSpeed=1;
+                if (Math.abs(targetAngle - headingAngle) < 30) currentSpeed = .2;
+                else currentSpeed = 1;
                 robot.leftFront.setPower(currentSpeed);
                 robot.rightFront.setPower(-currentSpeed);
                 robot.leftRear.setPower(currentSpeed);
@@ -80,11 +70,11 @@ public class GyroTest extends LinearOpMode {
 
             }
         } else {
-            targetAngle+=adjust;
-            if(targetAngle>180) targetAngle-=360;
+            targetAngle += adjust;
+            if (targetAngle > 180) targetAngle -= 360;
             while (headingAngle > targetAngle + error / 2 || headingAngle < targetAngle - error / 2 && opModeIsActive()) {
-                if(Math.abs(targetAngle-headingAngle)<40) currentSpeed=.2;
-                else currentSpeed=1;
+                if (Math.abs(targetAngle - headingAngle) < 30) currentSpeed = .2;
+                else currentSpeed = 1;
                 robot.leftFront.setPower(-currentSpeed);
                 robot.rightFront.setPower(currentSpeed);
                 robot.leftRear.setPower(-currentSpeed);
@@ -99,11 +89,87 @@ public class GyroTest extends LinearOpMode {
         headingAngle = normalizeAngle();
     }
 
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftRearTarget;
+        int newRightRearTarget;
+        int newLeftFrontTarget;
+        int newRightFrontTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftRearTarget = robot.leftRear.getCurrentPosition() - (int)(leftInches * COUNTS_PER_INCH);
+            newLeftFrontTarget = robot.leftFront.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+
+            newRightRearTarget = robot.rightRear.getCurrentPosition() - (int)(rightInches * COUNTS_PER_INCH);
+            newRightFrontTarget = robot.rightFront.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+
+            robot.leftRear.setTargetPosition(newLeftRearTarget);
+            robot.leftFront.setTargetPosition(newLeftFrontTarget);
+
+            robot.rightRear.setTargetPosition(newRightRearTarget);
+            robot.rightFront.setTargetPosition(newRightFrontTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftFront.setPower(speed);
+            robot.rightFront.setPower(speed);
+            robot.leftRear.setPower(speed);
+            robot.rightRear.setPower(speed);
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftFront.isBusy() && robot.rightFront.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftFrontTarget,  newRightFrontTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        robot.leftFront.getCurrentPosition(),
+                        robot.rightFront.getCurrentPosition());
+                telemetry.update();
+            }
+
+            
+
+            // Stop all motion;
+
+            brake();
+            sleep(1000);
+            brake();
+            sleep(1000);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+
+
     public boolean shortestDirection(double angle) {
         if (normalizeAngle() < angle) return true;
         else return false;
 
     }
+
     public double normalizeAngle() {
         angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double headingAngle = angles.firstAngle;

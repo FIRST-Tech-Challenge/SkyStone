a9f6ef119d274da257d255bac25582dff5560350
hendersm21@pwcs-edu.org <hendersm21@pwcs-edu.org>
2019-09-07 14:25:39 -0400

    Base TeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterAutonomous.java
@@ -1,4 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
-public class MasterAutonomous {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name="Master Autonomous", group="Master")
+public class MasterAutonomous extends LinearOpMode {
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Declare hardware mapping
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection((DcMotor.Direction.FORWARD));
+
+        waitForStart();
+
+        //List robot instructions here
+
+
+    }
+        // Methods
+
+        public void driveForward(double power)
+        {
+            leftDrive.setPower(power);
+            rightDrive.setPower(power);
+        }
+        public void turnLeft(double power)
+        {
+            leftDrive.setPower(-power);
+            rightDrive.setPower(power);
+        }
+        public void turnRight(double power)
+        {
+            turnLeft(-power);
+        }
+        public void driveBackward(double power)
+        {
+            driveForward(-power);
+        }
+        public void stopDriving()
+        {
+            driveForward(0);
+        }
+
+
+
+
+
 }

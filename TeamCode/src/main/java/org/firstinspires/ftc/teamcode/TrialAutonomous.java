877d9a7313d7f4c6e6bf27e20c359aad9ddbec8e
hendersm21@pwcs-edu.org <hendersm21@pwcs-edu.org>
2019-09-07 14:41:34 -0400

    Base TeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrialAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrialAutonomous.java
@@ -20,32 +20,38 @@ public class TrialAutonomous extends LinearOpMode {
         waitForStart();
 
         //List robot instructions here
-
-
+        driveForward(1, 5000);
+        turnLeft(1, 5000);
+        turnRight(1, 5000);
+        driveBackward(1, 5000);
+        stopDriving();
     }
         // Methods
 
-        public void driveForward(double power)
+        public void driveForward(double power, int time)
         {
             leftDrive.setPower(power);
             rightDrive.setPower(power);
+            sleep(time);
         }
-        public void turnLeft(double power)
+        public void turnLeft(double power, int time)
         {
             leftDrive.setPower(-power);
             rightDrive.setPower(power);
+            sleep(time);
         }
-        public void turnRight(double power)
+        public void turnRight(double power, int time)
         {
-            turnLeft(-power);
+            turnLeft(-power, time);
         }
-        public void driveBackward(double power)
+        public void driveBackward(double power, int time)
         {
-            driveForward(-power);
+            driveForward(-power, time);
         }
         public void stopDriving()
         {
-            driveForward(0);
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
         }
 
 

f8105f1607e8205b09ab69489c7b4d274fd5d23b
Oliver Yip <wingkong.yip@coxautoinc.com>
2019-09-09 18:40:55 -0500

    Changes on Sep 8 to make robot move and turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -11,17 +11,24 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class MyFIRSTJavaOpMode extends LinearOpMode {
     private Gyroscope imu;
-    private DcMotor motorTest;
+    private DcMotor motorTest1;
+    private DcMotor motorTest2;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange; private Servo servoTest;
+    double left;
+    double right;
+    double drive;
+    double turn;
+    double max;
 
     @Override
     public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servoTest");
+//        imu = hardwareMap.get(Gyroscope.class, "imu");
+        motorTest1 = hardwareMap.get(DcMotor.class, "left_drive");
+        motorTest2 = hardwareMap.get(DcMotor.class, "right_drive");
+//        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+//        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+//        servoTest = hardwareMap.get(Servo.class, "servoTest");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -30,12 +37,31 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-//        double tgtPower = 0;
+        double tgtPower = 0;
         while (opModeIsActive()) {
 //            tgtPower = -this.gamepad1.left_stick_y;
-//            motorTest.setPower(tgtPower);
-//            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor Power", motorTest.getPower());
+            drive = -gamepad1.left_stick_y;
+            turn  =  gamepad1.right_stick_x;
+//            turn = 0;
+
+            // Combine drive and turn for blended motion.
+            left  = -( drive + turn );
+            right = (drive - turn);
+
+            // Normalize the values so neither exceed +/- 1.0
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0)
+            {
+                left /= max;
+                right /= max;
+            }
+
+            motorTest1.setPower(right);
+            motorTest2.setPower(left);
+            telemetry.addData("Target Power 1", left);
+            telemetry.addData("Target Power 2", right);
+            telemetry.addData("Motor Power", motorTest1.getPower());
+            telemetry.addData("Motor Power", motorTest2.getPower());
             telemetry.addData("Status", "Running");
             telemetry.update();
         }

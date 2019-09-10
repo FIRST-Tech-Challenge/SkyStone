f41b1c64d9b74fb678f02cb9eec0acf38862f689
AlphaLocke <47842170+AlphaLocke@users.noreply.github.com>
2019-09-10 12:02:33 -0500

    Update encoderTest.java
    Added a change so it would show up on the phones

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@TeleOp(name = "EncoderTestv2", group = "basic")
 public class encoderTest extends LinearOpMode {
 
     public static DcMotor test1;

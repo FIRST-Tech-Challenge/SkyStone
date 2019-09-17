68999aa97796b164c8de33dddea3f6419577cfee
robopocalypse <teamrobotics8872@gmail.com>
2019-09-17 16:07:22 -0700

    Fixed multiple gyroTurns showing up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -15,44 +14,31 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import java.util.Locale;
 
-@Autonomous(name = "GyroTest1", group = "8872")
-//@Disabled
+@Autonomous(name = "GyroTest", group = "8872")
 public class GyroTest extends LinearOpMode {
     Chassis robot = new Chassis();
     Orientation angles;
     double adjust = 4;
     private ElapsedTime runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 537.6;    // Neverest 20
-    static final double     DRIVE_GEAR_REDUCTION    = 1 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double COUNTS_PER_MOTOR_REV = 537.6;    // Neverest 20
+    static final double DRIVE_GEAR_REDUCTION = 1;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
 
-    static final double coeff = 0.3;
     @Override
-
     public void runOpMode() {
         robot.init(hardwareMap);
         composeTelemetry();
         angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         telemetry.update();
-
         waitForStart();
-        gyroDrive(0.2, 30, 20);
-        sleep(1000);
         gyroTurn(90);
-        sleep(1000);
-
-
         telemetry.addData("DONE", angles.firstAngle);
-
-
-        sleep(3000);
+        sleep(100);
         angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         telemetry.update();
         sleep(10000);
-
-
     }
 
     public void gyroTurn(double targetAngle) {
@@ -65,8 +51,11 @@ public class GyroTest extends LinearOpMode {
             targetAngle -= adjust;
             if (targetAngle < -180) targetAngle += 360;
             while (headingAngle > targetAngle + error / 2 || headingAngle < targetAngle - error / 2 && opModeIsActive()) {
-                if (Math.abs(targetAngle - headingAngle) < 30) currentSpeed = .2;
-                else currentSpeed = 1;
+                if (Math.abs(targetAngle - headingAngle) < 30) {
+                    currentSpeed = .2;
+                } else {
+                    currentSpeed = 1;
+                }
                 robot.leftFront.setPower(currentSpeed);
                 robot.rightFront.setPower(-currentSpeed);
                 robot.leftRear.setPower(currentSpeed);
@@ -78,8 +67,11 @@ public class GyroTest extends LinearOpMode {
             targetAngle += adjust;
             if (targetAngle > 180) targetAngle -= 360;
             while (headingAngle > targetAngle + error / 2 || headingAngle < targetAngle - error / 2 && opModeIsActive()) {
-                if (Math.abs(targetAngle - headingAngle) < 30) currentSpeed = .2;
-                else currentSpeed = 1;
+                if (Math.abs(targetAngle - headingAngle) < 30) {
+                    currentSpeed = .2;
+                } else {
+                    currentSpeed = 1;
+                }
                 robot.leftFront.setPower(-currentSpeed);
                 robot.rightFront.setPower(currentSpeed);
                 robot.leftRear.setPower(-currentSpeed);
@@ -106,11 +98,11 @@ public class GyroTest extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newLeftRearTarget = robot.leftRear.getCurrentPosition() - (int)(leftInches * COUNTS_PER_INCH);
-            newLeftFrontTarget = robot.leftFront.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newLeftRearTarget = robot.leftRear.getCurrentPosition() - (int) (leftInches * COUNTS_PER_INCH);
+            newLeftFrontTarget = robot.leftFront.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
 
-            newRightRearTarget = robot.rightRear.getCurrentPosition() - (int)(rightInches * COUNTS_PER_INCH);
-            newRightFrontTarget = robot.rightFront.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newRightRearTarget = robot.rightRear.getCurrentPosition() - (int) (rightInches * COUNTS_PER_INCH);
+            newRightFrontTarget = robot.rightFront.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
 
             robot.leftRear.setTargetPosition(newLeftRearTarget);
             robot.leftFront.setTargetPosition(newLeftFrontTarget);
@@ -142,13 +134,12 @@ public class GyroTest extends LinearOpMode {
                     (robot.leftFront.isBusy() && robot.rightFront.isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftFrontTarget,  newRightFrontTarget);
-                telemetry.addData("Path2",  "Running at %7d :%7d",
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftFrontTarget, newRightFrontTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
                         robot.leftFront.getCurrentPosition(),
                         robot.rightFront.getCurrentPosition());
                 telemetry.update();
             }
-
 
 
             // Stop all motion;
@@ -175,16 +166,16 @@ public class GyroTest extends LinearOpMode {
 
         runtime.reset();
 
-        if(encoderCount > 0) {
+        if (encoderCount > 0) {
 
             while (robot.leftRear.getCurrentPosition() < (encoderCount + startPosition)) {
                 if (!opModeIsActive()) {
                     return;
                 }
 
-                robot.leftFront.setPower(-speed * coeff); //negative because random problem with TMK chassis
+                robot.leftFront.setPower(-speed); //negative because random problem with TMK chassis
                 robot.rightFront.setPower(-speed); //negative because random problem with TMK chassis
-                robot.leftRear.setPower(speed * coeff);
+                robot.leftRear.setPower(speed);
                 robot.rightRear.setPower(speed);
 
                 telemetry.addData("encoder value", robot.leftRear.getCurrentPosition());
@@ -192,9 +183,7 @@ public class GyroTest extends LinearOpMode {
                 telemetry.update();
                 sleep(50);
             }
-        }
-
-        else if(encoderCount < 0) {
+        } else if (encoderCount < 0) {
 
             while (robot.leftRear.getCurrentPosition() > (encoderCount + startPosition)) {
                 if (!opModeIsActive()) {
@@ -202,9 +191,9 @@ public class GyroTest extends LinearOpMode {
                 }
 
                 robot.leftFront.setPower(speed);
-                robot.rightFront.setPower(speed * coeff);
+                robot.rightFront.setPower(speed);
                 robot.leftRear.setPower(-speed);
-                robot.rightRear.setPower(-speed * coeff);
+                robot.rightRear.setPower(-speed);
 
                 telemetry.addData("encoder value", robot.leftRear.getCurrentPosition());
                 telemetry.update();

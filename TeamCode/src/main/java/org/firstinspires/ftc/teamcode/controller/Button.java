46d366173a6ea5fa2c3626da5a357e7a31f055f8
LexiShew <lexishew@live.com>
2019-09-18 16:01:36 -0700

    Folders to make DriveSystem run

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controller/Button.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.controller;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+
+/**
+ * The Button class represents a button on the gamepad and handles all the reactions done on the
+ * gamepad.
+ */
+public class Button
+{
+    public Func<Boolean> isPressed;
+    public Handler pressedHandler;
+    public Handler releasedHandler;
+
+    private boolean wasPressed;
+    public boolean justPressed;
+    public boolean justReleased;
+
+    /**
+     * Creates a new Button
+     */
+    public Button()
+    {
+    }
+
+    /**
+     * Creates a new Button given an isPressed Handler
+     * @param isPressed handler when the button is pressed
+     */
+    public Button(Func<Boolean> isPressed)
+    {
+        this.isPressed = isPressed;
+    }
+
+    /**
+     * Creates a new Button given an isPressed Handler, and a pressed handler
+     * @param isPressed is pressed check
+     * @param pressedHandler pressed handler
+     */
+    public Button(Func<Boolean> isPressed, Handler pressedHandler)
+    {
+        this.isPressed = isPressed;
+        this.pressedHandler = pressedHandler;
+    }
+
+    /**
+     * Creates a new button given an is pressed handler, pressed handler, and released handler
+     * @param isPressed checks if button is pressed
+     * @param pressedHandler handles when the button is pressed
+     * @param releasedHandler handles when the button is released
+     */
+    public Button(Func<Boolean> isPressed, Handler pressedHandler, Handler releasedHandler)
+    {
+        this.isPressed = isPressed;
+        this.pressedHandler = pressedHandler;
+        this.releasedHandler = releasedHandler;
+    }
+
+    /**
+     * Tests the button for the button to be pressed and calls the necessary methods depending
+     * on previous actions.
+     */
+    public void testAndHandle()
+    {
+        Boolean pressed = this.isPressed.value();
+
+        this.justPressed = (pressed && !this.wasPressed);
+        this.justReleased   = (!pressed && this.wasPressed);
+        this.wasPressed = pressed;
+
+        if (this.justPressed && this.pressedHandler != null)
+            try
+            {
+                this.pressedHandler.invoke();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        if (this.justReleased && this.releasedHandler != null)
+            try
+            {
+                this.releasedHandler.invoke();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/logging/LogWritter.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.systems.logging;
+
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * A writer used to write to the log file on the phone
+ */
+public class LogWritter
+{
+    private StringBuffer buffer;
+    private File logFile;
+
+    /**
+     * Creates a new Log Writter given a log file name
+     * @param logFile the name of the log file
+     */
+    public LogWritter(File logFile) {
+        this.buffer = new StringBuffer();
+        this.logFile = logFile;
+    }
+
+    /**
+     * appends line to the writter
+     * @param line the line to be added
+     */
+    public void appendLine(String line) {
+        buffer.ensureCapacity(line.length() + 1);
+        buffer.append(line + "\n");
+    }
+
+    /**
+     * Flushes the string buffer
+     */
+    public void flush() {
+        try {
+            writeToFile();
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not close to file", e);
+        }
+    }
+
+    /**
+     * Writes to the file
+     * @throws Exception IOException is thrown if there is an error appending the buffer to the file
+     */
+    private void writeToFile() throws Exception {
+        FileWriter fileWriter = getFileWriter();
+        appendBufferToFile(fileWriter);
+        buffer = new StringBuffer();
+    }
+
+    /**
+     * Gets the file writer need to write to the log files
+     * @return Returns the file writer used to write to log files
+     * @throws Exception IOException if the file writer causes
+     * @see FileWriter
+     */
+    private FileWriter getFileWriter() throws Exception {
+        return new FileWriter(logFile);
+    }
+
+    /**
+     * Appends the content of the string buffer to the file writter
+     * @param fileWriter writer to the log files
+     * @throws Exception IOException is thrown if an error occurs while handling the file
+     * @see FileWriter
+     */
+    private void appendBufferToFile(FileWriter fileWriter) throws Exception {
+        fileWriter.append(buffer.toString());
+        fileWriter.flush();
+        fileWriter.close();
+    }
+}

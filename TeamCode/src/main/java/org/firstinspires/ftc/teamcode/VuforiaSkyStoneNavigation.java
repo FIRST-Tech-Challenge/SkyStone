67a23b40f6f11ac386e78443193d3871a7b27904
stharasingh <54640099+stharasingh@users.noreply.github.com>
2019-09-19 05:18:47 +0530

    Committing Tensorflow and Vuforia first version code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaSkyStoneNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaSkyStoneNavigation.java
@@ -83,7 +83,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 
 
 @TeleOp(name="SKYSTONE Vuforia Nav", group ="Concept")
-@Disabled
+//@Disabled
 public class VuforiaSkyStoneNavigation extends LinearOpMode {
 
     // IMPORTANT:  For Phone Camera, set 1) the camera source and 2) the orientation, based on how your phone is mounted:

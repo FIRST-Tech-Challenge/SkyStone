effe5b806caa9283fe78143beab411a7e56f1a91
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2019-09-18 18:59:38 -0500

    Update SkyStoneSampleTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneSampleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneSampleTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;

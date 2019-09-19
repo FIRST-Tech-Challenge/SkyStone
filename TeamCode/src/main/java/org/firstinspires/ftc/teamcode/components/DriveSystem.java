87defdf291e04a5e06dcf84eb5759c9c20c04e12
LexiShew <lexishew@live.com>
2019-09-19 15:47:54 -0700

    Added strafing + turning to DriveSystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveSystem.java
@@ -6,8 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.components.scale.ExponentialRamp;
 import org.firstinspires.ftc.teamcode.components.scale.IScale;
 import org.firstinspires.ftc.teamcode.components.scale.LinearScale;
+import org.firstinspires.ftc.teamcode.components.scale.Point;
+import org.firstinspires.ftc.teamcode.components.scale.Ramp;
 import org.firstinspires.ftc.teamcode.systems.imu.IMUSystem;
 import org.firstinspires.ftc.teamcode.systems.logging.PhoneLogger;
 
@@ -29,10 +32,13 @@ public class DriveSystem {
 
     private final int FORWARD = 1;
     private final int BACKWARD = 0;
+    private final int STRAFE_LEFT = 0;
+    private final int STRAFE_RIGHT = 1;
 
     private double RAMP_POWER_CUTOFF = 0.3;
     private int RAMP_DISTANCE_TICKS;
     private final int TICKS_IN_INCH = 69;
+    private double TURN_RAMP_POWER_CUTOFF = 0.1;
 
 
 
@@ -184,99 +190,212 @@ public class DriveSystem {
 
 
 
-//    private void driveToPositionTicks(int ticks, double power, boolean shouldRamp) {
-//        setMotorPower(0);
-//
-//        motorFrontRight.setTargetPosition(motorFrontRight.getCurrentPosition() + ticks);
-//        motorFrontLeft.setTargetPosition(motorFrontLeft.getCurrentPosition() + ticks);
-//        motorBackRight.setTargetPosition(motorBackRight.getCurrentPosition() + ticks);
-//        motorBackLeft.setTargetPosition(motorBackLeft.getCurrentPosition() + ticks);
-//
-//        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//        Ramp ramp = new ExponentialRamp(new Point(0, RAMP_POWER_CUTOFF),
-//                new Point(RAMP_DISTANCE_TICKS, power));
-//
-//        double adjustedPower = Range.clip(power, -1.0, 1.0);
-//        setMotorPower(adjustedPower);
-//
-//        while (anyMotorsBusy()) {
-//            int distance = getMinDistanceFromTarget();
-//
-//            if (distance < 50) {
-//                break;
-//            }
-//
-//            double direction = 1.0;
-//            if (distance < 0) {
-//                distance = -distance;
-//                direction = -1.0;
-//            }
-//
-//            double scaledPower = shouldRamp ? ramp.scaleX(distance) : power;
-//
-//            setMotorPower(direction * scaledPower);
-//            telemetry.log("MecanumDriveSystem", "distance left (ticks): " + getMinDistanceFromTarget());
-//            telemetry.log("MecanumDriveSystem","scaled power: " + scaledPower);
-//            telemetry.write();
-//        }
-//        setMotorPower(0);
-//    }
-//
-//    public void setRunMode(DcMotor.RunMode runMode) {
-//        for (DcMotor motor : motors) {
-//            motor.setMode(runMode);
-//        }
-//    }
-//
-//    public void mecanumDriveXY(double x, double y) {
-//        this.motorFrontRight.setPower(Range.clip(y + x, -1, 1));
-//        this.motorBackRight.setPower(Range.clip(y - x, -1, 1));
-//        this.motorFrontLeft.setPower(Range.clip(y - x, -1, 1));
-//        this.motorBackLeft.setPower(Range.clip(y + x, -1, 1));
-//    }
-//
-//    /**
-//     * Checks if any of the motors are currently running
-//     * @return Returns true if any motors are busy
-//     */
-//    public boolean anyMotorsBusy()
-//    {
-//        return motorFrontLeft.isBusy() ||
-//                motorFrontRight.isBusy() ||
-//                motorBackLeft.isBusy() ||
-//                motorBackRight.isBusy();
-//    }
-//
-//    /**
-//     * Gets the minimum distance from the target
-//     * @return
-//     */
-//    public int  getMinDistanceFromTarget() {
-//        int d = this.motorFrontLeft.getTargetPosition() - this.motorFrontLeft.getCurrentPosition();
-//        d = Math.min(d, this.motorFrontRight.getTargetPosition() - this.motorFrontRight.getCurrentPosition());
-//        d = Math.min(d, this.motorBackLeft.getTargetPosition() - this.motorBackLeft.getCurrentPosition());
-//        d = Math.min(d, this.motorBackRight.getTargetPosition() - this.motorBackRight.getCurrentPosition());
-//        return d;
-//    }
-//
-//    public void driveToPositionInches(int inches, double power, boolean shouldRamp) {
-//         if (power <= 0 || inches <= 0) {
-//             setMotorDirection(FORWARD);
-//         } else {
-//             setMotorDirection(BACKWARD);
-//         }
-//        int ticks = (int) inchesToTicks(inches);
-//        driveToPositionTicks(ticks, power, shouldRamp);
-//    }
-//
-//    /**
-//     * Converts inches to ticks
-//     * @param inches Inches to convert to ticks
-//     * @return
-//     */
-//    public int inchesToTicks(int inches) {
-//        return inches * TICKS_IN_INCH;
-//    }
+    private void driveToPositionTicks(int ticks, double power, boolean shouldRamp) {
+        setMotorPower(0);
+
+        motorFrontRight.setTargetPosition(motorFrontRight.getCurrentPosition() + ticks);
+        motorFrontLeft.setTargetPosition(motorFrontLeft.getCurrentPosition() + ticks);
+        motorBackRight.setTargetPosition(motorBackRight.getCurrentPosition() + ticks);
+        motorBackLeft.setTargetPosition(motorBackLeft.getCurrentPosition() + ticks);
+
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        Ramp ramp = new ExponentialRamp(new Point(0, RAMP_POWER_CUTOFF),
+                new Point(RAMP_DISTANCE_TICKS, power));
+
+        double adjustedPower = Range.clip(power, -1.0, 1.0);
+        setMotorPower(adjustedPower);
+
+        while (anyMotorsBusy()) {
+            int distance = getMinDistanceFromTarget();
+
+            if (distance < 50) {
+                break;
+            }
+
+            double direction = 1.0;
+            if (distance < 0) {
+                distance = -distance;
+                direction = -1.0;
+            }
+
+            double scaledPower = shouldRamp ? ramp.scaleX(distance) : power;
+
+            setMotorPower(direction * scaledPower);
+            telemetry.log("MecanumDriveSystem", "distance left (ticks): " + getMinDistanceFromTarget());
+            telemetry.log("MecanumDriveSystem","scaled power: " + scaledPower);
+            telemetry.write();
+        }
+        setMotorPower(0);
+    }
+
+    public void setRunMode(DcMotor.RunMode runMode) {
+        for (DcMotor motor : motors) {
+            motor.setMode(runMode);
+        }
+    }
+
+    public void mecanumDriveXY(double x, double y) {
+        this.motorFrontRight.setPower(Range.clip(y + x, -1, 1));
+        this.motorBackRight.setPower(Range.clip(y - x, -1, 1));
+        this.motorFrontLeft.setPower(Range.clip(y - x, -1, 1));
+        this.motorBackLeft.setPower(Range.clip(y + x, -1, 1));
+    }
+
+    /**
+     * Checks if any of the motors are currently running
+     * @return Returns true if any motors are busy
+     */
+    public boolean anyMotorsBusy()
+    {
+        return motorFrontLeft.isBusy() ||
+                motorFrontRight.isBusy() ||
+                motorBackLeft.isBusy() ||
+                motorBackRight.isBusy();
+    }
+
+    /**
+     * Gets the minimum distance from the target
+     * @return
+     */
+    public int  getMinDistanceFromTarget() {
+        int d = this.motorFrontLeft.getTargetPosition() - this.motorFrontLeft.getCurrentPosition();
+        d = Math.min(d, this.motorFrontRight.getTargetPosition() - this.motorFrontRight.getCurrentPosition());
+        d = Math.min(d, this.motorBackLeft.getTargetPosition() - this.motorBackLeft.getCurrentPosition());
+        d = Math.min(d, this.motorBackRight.getTargetPosition() - this.motorBackRight.getCurrentPosition());
+        return d;
+    }
+
+    public void driveToPositionInches(int inches, double power, boolean shouldRamp) {
+         if (power <= 0 || inches <= 0) {
+             setMotorDirection(FORWARD);
+         } else {
+             setMotorDirection(BACKWARD);
+         }
+        int ticks = (int) inchesToTicks(inches);
+        driveToPositionTicks(ticks, power, shouldRamp);
+    }
+
+    /**
+     * Converts inches to ticks
+     * @param inches Inches to convert to ticks
+     * @return
+     */
+    public int inchesToTicks(int inches) {
+        return inches * TICKS_IN_INCH;
+    }
+
+    /**
+     * Strafes left for the given amount of inches
+     * @param inches Amount of inches to strafe
+     * @param power Power of the motors
+     */
+    public void strafeLeftToPositionInches(int inches, double power) {
+        setMotorDirection(STRAFE_LEFT);
+        int ticks = inchesToTicks(inches);
+        driveToPositionTicks(ticks, power, true);
+    }
+    /**
+     * Strafes right for the given amount of inches
+     * @param inches Amount of inches to strafe
+     * @param power Power of the motors
+     */
+    public void strafeRightToPositionInches(int inches, double power) {
+        setMotorDirection(STRAFE_RIGHT);
+        int ticks =  inchesToTicks(inches);
+        driveToPositionTicks(ticks, power, true);
+    }
+
+
+
+    /**
+     * Turns relative the heading upon construction
+     * @param degrees The degrees to turn the robot by
+     * @param maxPower The maximum power of the motors
+     */
+    public void turnAbsolute(double degrees, double maxPower) {
+        turn(degrees, maxPower, initialImuHeading);
+    }
+
+    /**
+     * Turns the robot by a given amount of degrees using the current heading
+     * @param degrees The degrees to turn the robot by
+     * @param maxPower The maximum power of the motors
+     */
+    public void turn(double degrees, double maxPower) {
+        turn(degrees, maxPower, imuSystem.getHeading());
+    }
+
+    /**
+     * Turns the robot by a given amount of degrees
+     * @param degrees The degrees to turn the robot by
+     * @param maxPower The maximum power of the motors
+     * @param initialHeading The initial starting point
+     */
+    private void turn(double degrees, double maxPower, double initialHeading) {
+        setMotorDirection(FORWARD);
+
+        double heading = -initialHeading;
+        double targetHeading = 0;
+
+        if ((degrees % 360) > 180) {
+            targetHeading = heading + ((degrees % 360) - 360);
+        } else {
+            targetHeading = heading + (degrees % 360);
+        }
+
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Between 90 (changed from 130) and 2 degrees away from the target
+        // we want to slow down from maxPower to 0.1
+        ExponentialRamp ramp = new ExponentialRamp(new Point(2.0, TURN_RAMP_POWER_CUTOFF), new Point(90, maxPower));
+
+        while (Math.abs(computeDegreesDiff(targetHeading, heading)) > 1) {
+            double power = getTurnPower(ramp, targetHeading, heading);
+            telemetry.log("MecanumDriveSystem","heading: " + heading);
+            telemetry.log("MecanumDriveSystem","target heading: " + targetHeading);
+            telemetry.log("MecanumDriveSystem","power: " + power);
+            telemetry.log("MecanumDriveSystem","distance left: " + Math.abs(targetHeading - heading));
+            telemetry.write();
+
+
+            tankDrive(power, -power);
+            heading = -imuSystem.getHeading();
+        }
+        this.setMotorPower(0);
+    }
+
+    /**
+     * Causes the system to tank drive
+     * @param leftPower sets the left side power of the robot
+     * @param rightPower sets the right side power of the robot
+     */
+    public void tankDrive(double leftPower, double rightPower) {
+        this.motorFrontLeft.setPower(leftPower);
+        this.motorBackLeft.setPower(leftPower);
+        this.motorFrontRight.setPower(rightPower);
+        this.motorBackRight.setPower(rightPower);
+    }
+
+    /**
+     * Gets the turn power needed
+     * @param ramp the ramp
+     * @param targetHeading the target heading
+     * @param heading the heading
+     * @return
+     */
+    private double getTurnPower(Ramp ramp, double targetHeading, double heading) {
+        double diff = computeDegreesDiff(targetHeading, heading);
+
+        if (diff < 0) {
+            return -ramp.scaleX(Math.abs(diff));
+        } else {
+            return ramp.scaleX(Math.abs(diff));
+        }
+    }
+
+    private double computeDegreesDiff(double targetHeading, double heading) {
+        return targetHeading - heading;
+    }
 
 }

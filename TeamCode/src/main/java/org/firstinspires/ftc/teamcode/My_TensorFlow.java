8c66620ed151543d67410e7abf2fcb3be110902d
David Atkinson <david.atkinson@netzero.com>
2019-09-21 17:55:28 -0500

    Updated no majot change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/My_TensorFlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/My_TensorFlow.java
@@ -52,7 +52,7 @@ import java.util.List;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
+//@Disabled
 public class My_TensorFlow extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";

ae06489c95e25406b64b24976d80db33e9872b8d
Ethan <e3lcool@gmail.com>
2019-09-21 14:46:01 -0700

    Tweaked tensorflow test, angles still do not output accurately

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/TestTensorFlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/TestTensorFlow.java
@@ -6,8 +6,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaSkyStone;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.TfodSkyStone;
+import org.firstinspires.ftc.teamcode.Skystone.Robot;
 
 import java.util.List;
 
@@ -15,45 +18,44 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 import static org.firstinspires.ftc.teamcode.RoverRuckus.RR2.Auto.TensorFlowMineralDetection.VUFORIA_KEY;
 
 @Autonomous
-public class TestTensorFlow extends LinearOpMode {
+public class TestTensorFlow extends AutoBase {
     protected static final String VUFORIA_KEY = "AbSCRq//////AAAAGYEdTZut2U7TuZCfZGlOu7ZgOzsOlUVdiuQjgLBC9B3dNvrPE1x/REDktOALxt5jBEJJBAX4gM9ofcwMjCzaJKoZQBBlXXxrOscekzvrWkhqs/g+AtWJLkpCOOWKDLSixgH0bF7HByYv4h3fXECqRNGUUCHELf4Uoqea6tCtiGJvee+5K+5yqNfGduJBHcA1juE3kxGMdkqkbfSjfrNgWuolkjXR5z39tRChoOUN24HethAX8LiECiLhlKrJeC4BpdRCRazgJXGLvvI74Tmih9nhCz6zyVurHAHttlrXV17nYLyt6qQB1LtVEuSCkpfLJS8lZWS9ztfC1UEfrQ8m5zA6cYGQXjDMeRumdq9ugMkS";
 
-    protected VuforiaLocalizer vuforia;
+    protected VuforiaSkyStone vuforiaSkystone;
+    protected TfodSkyStone tfodSkyStone;
 
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";
     private static final String LABEL_SECOND_ELEMENT = "Skystone";
 
-    private TFObjectDetector tfod;
-
     public void runOpMode() {
-        initVuforia();
-        initTfod();
+        vuforiaSkystone = new VuforiaSkyStone();
+        tfodSkyStone = new TfodSkyStone();
 
-        if (tfod != null) {
-            tfod.activate();
-        }
+        initTfod();
 
         waitForStart();
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                if (tfod != null) {
+                if (tfodSkyStone != null) {
                     // getUpdatedRecognitions() will return null if no new information is available since
                     // the last time that call was made.
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    List<Recognition> updatedRecognitions = tfodSkyStone.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
                         telemetry.addData("# Object Detected", updatedRecognitions.size());
 
                         // step through the list of recognitions and display boundary info.
                         int i = 0;
                         for (Recognition recognition : updatedRecognitions) {
-                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-//                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-//                                    recognition.getLeft(), recognition.getTop());
-//                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-//                                    recognition.getRight(), recognition.getBottom());
-                            telemetry.addData("angle", recognition.estimateAngleToObject(AngleUnit.DEGREES));
+                            if(recognition.getLabel() == "Skystone") {
+                                // telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                // recognition.getLeft(), recognition.getTop());
+                                // telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                // recognition.getRight(), recognition.getBottom());
+                                double angleToObject = recognition.estimateAngleToObject(AngleUnit.DEGREES);
+                                telemetry.addData("Angle", angleToObject);
+                            }
                         }
                         telemetry.update();
                     }
@@ -62,22 +64,21 @@ public class TestTensorFlow extends LinearOpMode {
         }
     }
 
-    protected void initVuforia() {
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        parameters.cameraMonitorFeedback = AXES;
-
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-    }
-
     protected void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minimumConfidence = 0.8;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+        vuforiaSkystone.initialize(VUFORIA_KEY,
+                VuforiaLocalizer.CameraDirection.BACK,
+                false,
+                true,
+                VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES,
+                0, 0, 0, 0, 0, 0,
+                false);
+
+        if(vuforiaSkystone != null) {
+            tfodSkyStone.initialize(vuforiaSkystone, .7f, false, true);
+            tfodSkyStone.activate();
+        } else {
+            telemetry.addData("status","not working");
+            telemetry.update();
+        }
     }
 }

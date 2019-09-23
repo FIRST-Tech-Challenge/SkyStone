8f26f72ae1139695767eb1447d5d1f39f61c4d0e
Randall-Scharpf <randall.scharpf@gmail.com>
2019-09-23 10:27:26 -0700

    Started Parking Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBuildZoneBlue.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Blue Build Zone", group = "none")
+public class AutoBuildZoneBlue extends LinearOpMode {
+}

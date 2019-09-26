e8df41d250da365d3a3031cc7ba6b6df5a2179f0
Aditya Kishore <akushwarrior@gmail.com>
2019-09-25 18:24:22 -0700

    Update Omni.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Omni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Omni.java
@@ -29,23 +29,33 @@ public class Omni extends LinearOpMode {
         //Repeat while our robot's TeleOp mode is active
         while (opModeIsActive())
         {
+            //If we want to move:
             if (-gamepad1.left_stick_y != 0 || -gamepad1.left_stick_x != 0) {
-                //Find values of left and right joysticks and find relevant power
+                // Find values in each direction of directional (left) joystick and find
+                // relevant power
                 double horiMotor = Range.clip(-gamepad1.left_stick_x, -1.0, 1.0);
                 double vertiMotor = Range.clip(-gamepad1.left_stick_y, -1.0, 1.0);
+
+                //Set each representative motor's power equal to power we will assign real motor
                 topMotor = horiMotor;
                 bottomMotor = horiMotor;
                 rightMotor = vertiMotor;
                 leftMotor = vertiMotor;
             }
+            //else (If we want to rotate):
             else {
+                // Find amount that we will be rotating by using x coordinate of rotational
+                // (right) joystick
                 double rotValue = -gamepad1.right_stick_x;
+
+                //Set each representative motor's power equal to power we will assign real motor
                 topMotor = rotValue;
                 bottomMotor = rotValue;
                 rightMotor = rotValue;
                 leftMotor = rotValue;
             }
 
+            //Set power equal to representative motor we assigned earlier
             bot.topDrive.setPower(topMotor);
             bot.bottomDrive.setPower(bottomMotor);
             bot.rightDrive.setPower(rightMotor);

4c84ab6d608c7ac940ee8d9f056f5904cae05223
rzx0201 <50276524+rzx0201@users.noreply.github.com>
2019-09-28 10:37:55 -0400

    Created program to move Robot in TeleOP in Tank Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Program_in_BRMS.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "TeleOp_Program_in_BRMS")
+@Disabled
+public class TeleOp_Program_in_BRMS extends LinearOpMode {
+    private DcMotor Left;
+    private DcMotor Right;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Left = hardwareMap.dcMotor.get("Left");
+        Right = hardwareMap.dcMotor.get("Right");
+
+        Left.setDirection(DcMotor.Direction.FORWARD);
+        Right.setDirection(DcMotor.Direction.FORWARD);
+
+                waitForStart();
+
+        while (opModeIsActive()) {
+
+         Left.setPower(-gamepad1.left_stick_y);
+         Right.setPower(-gamepad1.right_stick_y);
+                    idle();
+    }
+
+    }
+
+
+
+    }
+
+
+}

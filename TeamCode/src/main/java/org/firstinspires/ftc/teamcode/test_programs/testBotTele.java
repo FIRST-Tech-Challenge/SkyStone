67e3c4cd5a02b7391a2198c3ff578be728e9e64a
legoheads <samedh.desai@gmail.com>
2019-09-27 23:16:48 -0400

    test bot tele fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_programs/testBotTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_programs/testBotTele.java
@@ -20,12 +20,9 @@ public class testBotTele extends LinearOpMode
     DcMotor leftMotorBack;
     DcMotor rightMotorBack;
 
-    Servo glyphFlip;
-
-    //Define relic motors
-    Servo relicGrab;
-    CRServo relicFlip;
-    DcMotor relicSpool;
+    DcMotor intakeLeft;
+    DcMotor intakeRight;
+    DcMotor flipper;
 
     //Define floats to be used as joystick inputs and trigger inputs
     float drivePower;
@@ -33,16 +30,8 @@ public class testBotTele extends LinearOpMode
     float leftTurnPower;
     float rightTurnPower;
 
-    //Define an int to use as gamepad2 initialization
-    int gamepad2Init = 0;
-
-    //Define an elapsed time variable
-    private ElapsedTime runtime = new ElapsedTime();
-    private ElapsedTime flipTime = new ElapsedTime();
-    private ElapsedTime relicTime = new ElapsedTime();
-
-    //Define booleans to make relic movements and shut off the intake wheels when gamepad2 is initialized
-    boolean bMoved = false;
+    float flipPower = (float) 0.3;
+    float maxPower = (float) 1.0;
 
     //Define a function to use to set motor powers
     public void setDriveMotorPowers(float leftFrontPower, float leftBackPower, float rightFrontPower, float rightBackPower)
@@ -66,14 +55,16 @@ public class testBotTele extends LinearOpMode
         rightMotorBack = hardwareMap.dcMotor.get("rightMotorBack");
 
         //Get references to the Servo Motors from the hardware map
-        glyphFlip = hardwareMap.servo.get("glyphFlip");
-        relicGrab = hardwareMap.servo.get("relicGrab");
-        relicFlip = hardwareMap.crservo.get("relicFlip");
+        intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+        intakeRight = hardwareMap.dcMotor.get("intakeRight");
+        flipper = hardwareMap.dcMotor.get("flipper");
 
         leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
         rightMotorFront.setDirection(DcMotorSimple.Direction.FORWARD);
         rightMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //Set the drive motors to brake mode to prevent rolling due to chain
         leftMotorFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -83,12 +74,6 @@ public class testBotTele extends LinearOpMode
 
         //Wait for start button to be clicked
         waitForStart();
-
-        //Reset the flipper
-        glyphFlip.setPosition(0.95);
-
-        //Reset the runtime after the start button is clicked
-        runtime.reset();
 
 //***********************************************************************************************************
         //LOOP BELOW
@@ -104,28 +89,20 @@ public class testBotTele extends LinearOpMode
             leftTurnPower = (float) ((gamepad1.left_trigger + gamepad2.left_trigger) * 0.75);
             rightTurnPower = (float) ((gamepad1.right_trigger + gamepad2.right_trigger) * 0.75);
 
-            //Gamepad2 init
-            if ((Math.abs(gamepad2.left_stick_y) > 0.1) && (gamepad2Init == 0))
-                bMoved = true;
-
-            //If gamepad2 is used, flip down the relic grabber and open the claws
-            if (bMoved)
-            {
-                relicFlip.setPower(0.7);
-                relicTime.reset();
-                relicGrab.setPosition(0.32);
-                bMoved = false;
-                gamepad2Init++;
-
-            }
+            intakeLeft.setPower(maxPower);
+            intakeRight.setPower(maxPower);
 
             //Drive if the joystick is pushed more Y than X
             if (Math.abs(drivePower) > Math.abs(shiftPower))
-                setDriveMotorPowers(-drivePower, drivePower, drivePower, -drivePower);
+            {
+                setDriveMotorPowers(- drivePower, drivePower, drivePower, - drivePower);
+            }
 
             //Shift if the joystick is pushed more on X than Y
             if (Math.abs(shiftPower) > Math.abs(drivePower))
+            {
                 setDriveMotorPowers(shiftPower, shiftPower, shiftPower, shiftPower);
+            }
 
             //If the left trigger is pushed, turn left at that power
             if (leftTurnPower > 0)
@@ -145,61 +122,24 @@ public class testBotTele extends LinearOpMode
                 setDriveMotorPowers((float) 0.0, (float) 0.0, (float) 0.0, (float) 0.0);
             }
 
-            if (drivePower < -0.6)
-            {
-                glyphFlip.setPosition(0.75);
-            }
-
             //If the dpad is pushed down, flip the glyphs into the cryptobox
             //Then reset the flipper
+            if (gamepad1.dpad_up)
+            {
+                flipper.setPower(flipPower);
+                Thread.sleep(300);
+                flipper.setPower(0.0);
+            }
+
             if (gamepad1.dpad_down)
             {
-                glyphFlip.setPosition(0.3);
-                flipTime.reset();
-                if (flipTime.time() > 1.2)
-                {
-                    glyphFlip.setPosition(0.75);
-                }
-            }
-
-            if (relicTime.time() > 1.1)
-            {
-                relicFlip.setPower(0.0);
-            }
-
-            //If the x button is pressed, open the claws
-            if (gamepad2.x)
-            {
-                relicGrab.setPosition(0.32);
-            }
-
-            //If the b button is pressed, close the
-            if (gamepad2.b)
-            {
-                relicGrab.setPosition(1.00);
-            }
-
-            //If the y button is pressed, flip the relic flipper down
-            //Also open the claws after a certain time to drop the relic
-            if (gamepad2.y)
-            {
-                relicFlip.setPower(0.7);
-                relicTime.reset();
-//                functions.crServoTime(relicFlip, (float) 0.7, 1500);
-            }
-
-            //If the a button is pressed, flip the relic flipper up
-            if (gamepad2.a)
-            {
-                //Up while holding relic, since it requires more time
-//                functions.crServoTime(relicFlip, (float) -0.7, 3000);
-                relicFlip.setPower(-0.7);
-                relicTime.reset();
+                flipper.setPower(-flipPower);
+                Thread.sleep(300);
+                flipper.setPower(0.0);
             }
 
             //Count time
             //Update the data
-            telemetry.addData("Status", runtime);
             telemetry.update();
 
             //Always call idle() at the bottom of your while(opModeIsActive()) loop

4eeab7cec2a565d27f297e20bf1a5c910e68cc3b
Joshua You <youjoshua12@gmail.com>
2019-09-29 20:07:45 -0700

    localizer logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PreseasonTest/MecanumLocalizers/ICCLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PreseasonTest/MecanumLocalizers/ICCLocalizer.java
@@ -13,8 +13,12 @@ public class ICCLocalizer implements BulkReadConsumer{
     MA3Encoder rightTrackingWheel = new MA3Encoder(1);
     MA3Encoder horizontalTrackingWheel = new MA3Encoder(2);
 
-
-
+	final double trackWidth = 45.72; // cm
+	
+	private double xCoordinate;
+	private double yCoordinate;
+	private double angle;
+	
     public void init(BulkReadManager bulkReadManager) {
         bulkReadManager.registerHighPriority(leftTrackingWheel);
         bulkReadManager.registerHighPriority(rightTrackingWheel);
@@ -24,5 +28,19 @@ public class ICCLocalizer implements BulkReadConsumer{
     @Override
     public void bulkReadUpdate(RevBulkData revBulkData) {
         
+    }
+    
+    // Arguments are in cm/s
+    private void calculatePosition(double leftWheelVelocity, double rightWheelVelocity, double horizontalWheelVelocity, double deltaTime) {
+    	double angularVelocity = (rightWheelVelocity - leftWheelVelocity) / trackWidth; // rad/s?
+    	double radiusOfCurvature = 0.5 * trackWidth * (leftWheelVelocity + rightWheelVelocity) / (rightWheelVelocity - leftWheelVelocity);
+    	
+   		double translatedXCoordinate = radiusOfCurvature * Math.sin(angle);
+   		double translatedYCoordinate = radiusOfCurvature * -Math.cos(angle);
+   		
+   		xCoordinate = (xCoordinate - radiusOfCurvature * Math.sin(angle)) + translatedXCoordinate * Math.cos(angularVelocity * deltaTime) - translatedYCoordinate * Math.sin(angularVelocity * deltaTime);
+   		yCoordinate = (yCoordinate + radiusOfCurvature * Math.cos(angle)) + translatedXCoordinate * Math.sin(angularVelocity * deltaTime) + translatedYCoordinate * Math.cos(angularVelocity * deltaTime);
+   		angle = angle + angularVelocity * deltaTime;
+   			
     }
 }

12838d3d8cde1352e9f654b7c3ce93ad17a1bc7f
年迈的老秋风Windy <cxcyhdyx@sina.com>
2019-10-01 17:22:13 -0400

    Start working on Linda Core

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_LindaCore.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.david_cao.generation1_linda_code;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.darbots.darbotsftclib.libcore.OpModes.DarbotsBasicOpMode;
+import org.darbots.darbotsftclib.libcore.calculations.dimentionalcalculation.Robot2DPositionIndicator;
+import org.darbots.darbotsftclib.libcore.chassiscontrollers.OmniDrive;
+import org.darbots.darbotsftclib.libcore.sensors.motion_related.RobotMotion;
+import org.darbots.darbotsftclib.libcore.sensors.motion_related.RobotWheel;
+import org.darbots.darbotsftclib.libcore.sensors.motors.RobotMotorController;
+import org.darbots.darbotsftclib.libcore.sensors.motors.RobotMotorWithEncoder;
+import org.darbots.darbotsftclib.libcore.sensors.servos.motor_powered_servos.RobotServoUsingMotor;
+import org.darbots.darbotsftclib.libcore.templates.RobotCore;
+import org.darbots.darbotsftclib.libcore.templates.chassis_related.RobotMotionSystem;
+import org.darbots.darbotsftclib.libcore.templates.motor_related.RobotMotor;
+
+public class Robot4100Generation1_LindaCore extends RobotCore {
+    private OmniDrive m_Chassis;
+    private Servo m_DragServoL, m_DragServoR;
+    private Servo m_Grabber, m_GrabberRot;
+    private RobotServoUsingMotor m_linearSlide;
+
+
+    public Robot4100Generation1_LindaCore(HardwareMap hardwares) {
+        super("4100Generation1_LindaCore.log");
+        RobotMotor
+                LFMotor = new RobotMotorWithEncoder(hardwares.dcMotor.get("LF"),Robot4100Generation1_Settings.motorType),
+                RFMotor = new RobotMotorWithEncoder(hardwares.dcMotor.get("RF"),Robot4100Generation1_Settings.motorType),
+                LBMotor = new RobotMotorWithEncoder(hardwares.dcMotor.get("LB"),Robot4100Generation1_Settings.motorType),
+                RBMotor = new RobotMotorWithEncoder(hardwares.dcMotor.get("RB"),Robot4100Generation1_Settings.motorType);
+        RobotWheel
+                LFWheel = new RobotWheel(new Robot2DPositionIndicator(-Robot4100Generation1_Settings.wheelPosition[0],Robot4100Generation1_Settings.wheelPosition[1],45),Robot4100Generation1_Settings.wheelRadius),
+                RFWheel = new RobotWheel(new Robot2DPositionIndicator(Robot4100Generation1_Settings.wheelPosition[0],Robot4100Generation1_Settings.wheelPosition[1],-45),Robot4100Generation1_Settings.wheelRadius),
+                LBWheel = new RobotWheel(new Robot2DPositionIndicator(-Robot4100Generation1_Settings.wheelPosition[0],-Robot4100Generation1_Settings.wheelPosition[1],135),Robot4100Generation1_Settings.wheelRadius),
+                RBWheel = new RobotWheel(new Robot2DPositionIndicator(Robot4100Generation1_Settings.wheelPosition[0],-Robot4100Generation1_Settings.wheelPosition[1],-135),Robot4100Generation1_Settings.wheelRadius);
+        RobotMotion
+                LFMotion = new RobotMotion(new RobotMotorController(LFMotor,Robot4100Generation1_Settings.CHASSIS_TIMEOUTENABLE,Robot4100Generation1_Settings.CHASSIS_TIMEOUTFACTOR),LFWheel),
+                RFMotion = new RobotMotion(new RobotMotorController(RFMotor,Robot4100Generation1_Settings.CHASSIS_TIMEOUTENABLE,Robot4100Generation1_Settings.CHASSIS_TIMEOUTFACTOR),RFWheel),
+                LBMotion = new RobotMotion(new RobotMotorController(LBMotor,Robot4100Generation1_Settings.CHASSIS_TIMEOUTENABLE,Robot4100Generation1_Settings.CHASSIS_TIMEOUTFACTOR),LBWheel),
+                RBMotion = new RobotMotion(new RobotMotorController(RBMotor,Robot4100Generation1_Settings.CHASSIS_TIMEOUTENABLE,Robot4100Generation1_Settings.CHASSIS_TIMEOUTFACTOR),RBWheel);
+        m_Chassis = new OmniDrive(LFMotion,RFMotion,LBMotion,RBMotion,null);
+        this.m_DragServoL = hardwares.servo.get("servoDragLeft");
+        this.m_DragServoR = hardwares.servo.get("servoDragRight");
+        this.m_Grabber = hardwares.servo.get("servoGrabber");
+        this.m_GrabberRot = hardwares.servo.get("servoGrabberRot");
+        RobotMotor LinearSlideMotor = new RobotMotorWithEncoder(hardwares.dcMotor.get("motorLinearSlide"),Robot4100Generation1_Settings.linearSlideMotorType);
+        RobotMotorController linearSlideController = new RobotMotorController(LinearSlideMotor,Robot4100Generation1_Settings.LINEARSLIDE_TIMEOUTCONTROLENABLE,Robot4100Generation1_Settings.LINEARSLIDE_TIMEOUTFACTOR);
+        this.m_linearSlide = new RobotServoUsingMotor(linearSlideController,Robot4100Generation1_Settings.LINEARSLIDE_START,Robot4100Generation1_Settings.LINEARSLIDE_MIN,Robot4100Generation1_Settings.LINEARSLIDE_MAX);
+    }
+
+    public RobotServoUsingMotor getLinearSlide(){
+        return this.m_linearSlide;
+    }
+
+    public void setDragServoToDrag(boolean drag){
+        if(drag){
+            this.m_DragServoL.setPosition(Robot4100Generation1_Settings.DRAGSERVO_DRAGPOS_L);
+            this.m_DragServoR.setPosition(Robot4100Generation1_Settings.DRAGSERVO_DRAGPOS_R);
+        }else{
+            this.m_DragServoL.setPosition(Robot4100Generation1_Settings.DRAGSERVO_RESTPOS_L);
+            this.m_DragServoR.setPosition(Robot4100Generation1_Settings.DRAGSERVO_RESTPOS_R);
+        }
+    }
+
+    public void setGrabberServoToGrab(boolean grab){
+        if(grab){
+            this.m_Grabber.setPosition(Robot4100Generation1_Settings.GRABBERSERVO_GRABPOS);
+        }else{
+            this.m_Grabber.setPosition(Robot4100Generation1_Settings.GRABBERSERVO_RESTPOS);
+        }
+    }
+
+    public void setGrabberRotServoToOutside(boolean outside){
+        if(outside){
+            this.m_GrabberRot.setPosition(Robot4100Generation1_Settings.GRABBERROTSERVO_OUTSIDEPOS);
+        }else{
+            this.m_GrabberRot.setPosition(Robot4100Generation1_Settings.GRABBERROTSERVO_INSIDEPOS);
+        }
+    }
+
+    @Override
+    public void stop() {
+        this.m_Chassis.deleteAllTasks();
+    }
+
+    @Override
+    public RobotMotionSystem getChassis() {
+        return this.m_Chassis;
+    }
+
+    @Override
+    public boolean isBusy() {
+        return false;
+    }
+
+    @Override
+    public void updateStatus() {
+        this.m_Chassis.updateStatus();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_Settings.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.david_cao.generation1_linda_code;
+
+import org.darbots.darbotsftclib.libcore.motortypes.AndyMark2964;
+import org.darbots.darbotsftclib.libcore.motortypes.AndyMark3637;
+import org.darbots.darbotsftclib.libcore.templates.motor_related.MotorType;
+
+public class Robot4100Generation1_Settings {
+    public static final double[] wheelPosition = {18.415,14};
+    public static final MotorType motorType = new AndyMark3637();
+    public static final double wheelRadius = 5;
+    public static final MotorType linearSlideMotorType = new AndyMark2964();
+    public static final boolean LINEARSLIDE_TIMEOUTCONTROLENABLE = true;
+    public static final double LINEARSLIDE_TIMEOUTFACTOR = 1.5;
+    public static final double LINEARSLIDE_MAX = 1000;
+    public static final double LINEARSLIDE_MIN = 0;
+    public static final double LINEARSLIDE_START = 0;
+
+    public static final boolean CHASSIS_TIMEOUTENABLE = false;
+    public static final double CHASSIS_TIMEOUTFACTOR = 1.0;
+    public static final double DRAGSERVO_RESTPOS_L = 0.0;
+    public static final double DRAGSERVO_RESTPOS_R = 0.0;
+    public static final double DRAGSERVO_DRAGPOS_L = 1.0;
+    public static final double DRAGSERVO_DRAGPOS_R = 1.0;
+    public static final double GRABBERSERVO_RESTPOS = 0.0;
+    public static final double GRABBERSERVO_GRABPOS = 1.0;
+    public static final double GRABBERROTSERVO_INSIDEPOS = 0.0;
+    public static final double GRABBERROTSERVO_OUTSIDEPOS = 1.0;
+}

5e7700028ce8a9460436bbda6f39843ad63e61c1
Lena Ellermann <lena.ellermann@igs-goettingen.de>
2019-10-01 17:49:52 +0200

    added empty autonomous program (we fill it next time)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Autonomous_01.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous (name = "Autonomous")
+public class Autonomous_01 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+    }
+}

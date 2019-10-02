0f90ac0d32aac9895d4160e926ee880e91e3a14a
LexiShew <lexishew@live.com>
2019-10-02 15:32:54 -0700

    added some stuff to op mode to make it work.
    Wheels still running independent of controller for unexplained reason.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DriveTeleop.java
@@ -14,31 +14,35 @@ import java.util.EnumMap;
 
 
 @TeleOp(name = "Drive", group="TeleOp")
-public class DriveTeleop extends LinearOpMode {
+public class DriveTeleop extends OpMode {
 
     private DriveSystem driveSystem;
     private boolean slowDrive;
 
-    public void initialize(){
+    public void init(){
+        telemetry.addLine("in init");
+
         EnumMap<DriveSystem.MotorNames, DcMotor> driveMap = new EnumMap<>(DriveSystem.MotorNames.class);
         for(DriveSystem.MotorNames name : DriveSystem.MotorNames.values()){
+            telemetry.addLine("adding motors");
             driveMap.put(name,hardwareMap.get(DcMotor.class, name.toString()));
         }
+
+        telemetry.update();
 
         driveSystem = new DriveSystem(driveMap, hardwareMap.get(BNO055IMU.class, "imu"));
         slowDrive = false;
     }
 
 
-    public void runOpMode(){
-        initialize();
+    public void loop(){
 
-        float rx = gamepad1.right_stick_x;
-        float ry = gamepad1.right_stick_y;
-        float lx = gamepad1.left_stick_x;
-        float ly = gamepad1.left_stick_y;
 
-        driveSystem.drive(rx, ry, lx, ly, slowDrive);
+            float rx = gamepad1.right_stick_x;
+            float ry = gamepad1.right_stick_y;
+            float lx = gamepad1.left_stick_x;
+            float ly = gamepad1.left_stick_y;
+            driveSystem.drive(rx, ry, lx, ly, slowDrive);
+
     }
-
 }

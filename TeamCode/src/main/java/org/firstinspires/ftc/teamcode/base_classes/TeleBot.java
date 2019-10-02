ad1fa5cefed080b8ba990912c22a5f9a2070a407
c20am <31973507+c20am@users.noreply.github.com>
2019-10-01 21:15:30 -0400

    Added first teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_classes/TeleBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_classes/TeleBot.java
@@ -1,12 +1,24 @@
 package org.firstinspires.ftc.teamcode.base_classes;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.Range;
+
 public class TeleBot extends Robot {
+
 
     //under this number, input won't be taken into account
     public double dead = .13;
 
     //stores the value of sin(45°), or sin(pi/4)
     public double sin45 = Math.sqrt(2)/2;
+
+    public TeleBot(OpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    public double[] getPowers() {
+        return new double[] {frontLeft.getPower(), frontRight.getPower(), backRight.getPower(), backLeft.getPower()};
+    }
 
     /**
      * Takes controller input in order to move the robot
@@ -21,23 +33,56 @@ public class TeleBot extends Robot {
         double rad = 1;
 
         //power for the left hand mecanum wheels (front left, back right)
-        double leftPow;
+        double strafeLeftPow = 0;
 
         //power for the right hand mecanum wheels (front right, back left)
-        double rightPow;
+        double strafeRightPow = 0;
+
+        //power difference dedicated to rotation
+        double rotPow = 0;
 
         //stores the power given to each motor clockwise from front left
         double[] powerValues;
 
         if ((Math.abs(leftX)> dead) || (Math.abs(leftY) > dead) || Math.abs(rightX) > dead) {
             //if the left stick is being used
-            leftPow = rad * sin45 * ((leftX - leftY))/rad;
-            rightPow = rad * sin45 * ((leftX + leftY)/rad);
+            strafeLeftPow = sin45 * ((leftX - leftY));
+            strafeRightPow = sin45 * ((leftX + leftY));
+
+            //either fully rotating one way or another, positive (1) represents clockwise
+            rotPow = (rightX > 0) ? (1) : (-1);
+
+            //sets each motor's power to combination of strafe and rotate
+            powerValues = new double[] {strafeLeftPow + rotPow, strafeRightPow - rotPow,
+                    strafeLeftPow - rotPow, strafeRightPow + rotPow};
+
+            //from Davis's code: rescale values out of the highest power in the array
+            double maximumPower = 0;
+
+            for (int i = 0; i < powerValues.length; i++) {
+                if (Math.abs(powerValues[i]) > maximumPower) {
+                    maximumPower = powerValues[i];
+                }
+            }
+
+            if (maximumPower != 0) {
+                for (int i = 0; i < powerValues.length; i++) {
+                    powerValues[i] = powerValues[i]/Math.abs(maximumPower);
+                }
+            }
+
+            frontLeft.setPower(Range.clip(powerValues[0], -1.0, 1.0));
+            frontRight.setPower(Range.clip(powerValues[0], -1.0, 1.0));
+            backLeft.setPower(Range.clip(powerValues[0], -1.0, 1.0));
+            backRight.setPower(Range.clip(powerValues[0], -1.0, 1.0));
+
+        } else {
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
         }
 
-        if (Math.abs(rightX) > dead) {
-            //if the right stick is being used
-        }
 
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BasicTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.base_classes.Robot;
+import org.firstinspires.ftc.teamcode.base_classes.TeleBot;
+
+@TeleOp(name="Basic OpMode", group="TeleOp")
+public class BasicTeleOp extends LinearOpMode {
+
+    public TeleBot robot = new TeleBot(this);
+    public ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        robot.init();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            robot.controlledDrive();
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}

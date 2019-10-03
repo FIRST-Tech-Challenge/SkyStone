dfe057ba7bf20dc00dddbd599d703cce0bad25af
UMA-CW-FTC-L-03 <UMA-CW-FTC-L-03@ftc.local>
2019-10-03 16:25:02 -0600

    modified:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InAndOut.java files added/modified by Ben Kirkman on UMA-CW-FTC-L-03 Thu Oct  3 16:25:02 MDT 2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InAndOut.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InAndOut.java
@@ -57,7 +57,7 @@ public class InAndOut extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    //private DcMotor rightDrive = null;
 
     @Override
     public void runOpMode() {
@@ -73,7 +73,7 @@ public class InAndOut extends LinearOpMode {
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        //rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -84,7 +84,7 @@ public class InAndOut extends LinearOpMode {
 
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
-            double rightPower;
+           // double rightPower;
 
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
@@ -92,22 +92,22 @@ public class InAndOut extends LinearOpMode {
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
+           // double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+           // rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
+             leftPower  = -gamepad1.left_stick_y ;
             // rightPower = -gamepad1.right_stick_y ;
 
             // Send calculated power to wheels
             leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+            //rightDrive.setPower(rightPower);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower);
             telemetry.update();
         }
     }

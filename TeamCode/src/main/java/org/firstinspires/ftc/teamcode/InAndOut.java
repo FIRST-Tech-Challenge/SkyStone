51af9733a57caceba0031cc1be313e92b6a92d26
UMA-CW-FTC-L-03 <UMA-CW-FTC-L-03@ftc.local>
2019-10-03 16:45:02 -0600

    modified:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InAndOut.java files added/modified by Ben Kirkman on UMA-CW-FTC-L-03 Thu Oct  3 16:45:02 MDT 2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InAndOut.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InAndOut.java
@@ -91,7 +91,8 @@ public class InAndOut extends LinearOpMode {
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
+            boolean forward = gamepad1.y;
+            boolean reverse = gamepad1.a;
            // double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
            // rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;

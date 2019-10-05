2a633f17c9b1f338776c9daf29fd0cf8d66d6055
Ethan <e3lcool@gmail.com>
2019-10-05 14:21:53 -0700

    Added intake and outtake functions for teleop, wrote down some comments in auto to guide order of tasks in auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/BlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/BlueLeft.java
@@ -1,4 +1,7 @@
 package org.firstinspires.ftc.teamcode.Skystone.Auto;
 
 public class BlueLeft{
+    // transport two skystones and other stones if time permits
+
+    // park in building zone (even if other team is in corner)
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/RedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/RedRight.java
@@ -1,44 +1,10 @@
 package org.firstinspires.ftc.teamcode.Skystone.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.teamcode.Skystone.MotionProfiler.PathPoints;
-import org.firstinspires.ftc.teamcode.Skystone.MotionProfiler.Point;
-import org.firstinspires.ftc.teamcode.Skystone.Odometry.Position2D;
-import org.firstinspires.ftc.teamcode.Skystone.Robot;
 
 @Autonomous(name="RedLeft", group ="Concept")
 public class RedRight extends AutoBase{
-    @Override
-    public void runOpMode() {
-        super.runOpMode();
-            Robot robot = new Robot(hardwareMap,telemetry,this);
-            robot.driveMotorsBreakZeroBehavior();
-            robot.setMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.setMotorMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    // transport two skystones and other stones if time permits
 
-            waitForStart();
-
-            Position2D position2D = new Position2D(robot);
-            position2D.startOdometry();
-            PathPoints testPath = new PathPoints(new double[][] {{20,0}}, 9);
-
-        while (opModeIsActive()){
-            robot.goToPoint(20, 0, 0.5, 0.5, 0);
-            sleep(100);
-            boolean detectedStone = false;
-            Point point = new Point(0,0);
-            while (!detectedStone) {
-                if (robot.detectSkystone().equals(true)){
-                    point = robot.detectSkystone();
-                    detectedStone = true;
-                }
-            }
-            robot.goToPoint(point.x, point.y, 0.5, 0.5, 0);
-            telemetry.addData("Skystone Location",robot.detectSkystone());
-            //robot.moveFollowCurve(testPath.targetPoints);
-            sleep(100000);
-        }
-    }
+    // park in building zone
 }

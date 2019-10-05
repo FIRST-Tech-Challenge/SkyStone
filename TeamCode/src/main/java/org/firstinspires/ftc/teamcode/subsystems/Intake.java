8c6ab42d2a275da38df86f37d4689202f61886c8
Aramis9999 <54870634+Aramis9999@users.noreply.github.com>
2019-10-05 13:15:35 -0500

    Added: setPower method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -5,7 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 public class Intake {
     DcMotor leftIntake;
     DcMotor rightIntake;
-
+    
+   
     public Intake(DcMotor leftIntake, DcMotor rightIntake) {
         this.leftIntake = leftIntake;
         this.rightIntake = rightIntake;
@@ -26,4 +27,16 @@ public class Intake {
     public void setRightIntake(DcMotor rightIntake) {
         this.rightIntake = rightIntake;
     }
+    
+    public void setLeftIntakePower(double power) {
+        leftIntake.setPower(power);
+    }
+    
+    public void setRightIntakePower(double power) {
+        rightIntake.setPower(power);
+    }
+    
+    public void setPower(double power) {
+        setLeftIntakePower(power);
+        setRightIntakePower(-power);
 }

08eae6e53df8ceb44efd024aa1ab0d7a579c64f1
thort <44458087+thort1@users.noreply.github.com>
2019-10-09 16:15:19 -0700

    Path organization ameliorated.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BaseOpModeConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BaseOpModeConfig.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;

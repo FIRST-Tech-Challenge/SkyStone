ef91ff90ec51e5140dfc4248809fd5b7a155dbc6
Sharon <beeppatrol@gmail.com>
2019-10-10 21:37:57 -0700

    Changed the hardware map by deleting last years sensors and added the right and left intake motors. I copied the Vuforia libraries in and added the license key. Copied the teleop program from last year and added the intake to run on button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBeep.java
@@ -7,6 +7,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -30,17 +31,12 @@ public class HardwareBeep {
     public DcMotor leftBack = null;
     public DcMotor rightFront = null;
     public DcMotor rightBack = null;
-    public DcMotor lift = null;
-    public DcMotor arm = null;
-    public DcMotor armExtrusion = null;
-    public DcMotor intake = null;
-    public CRServo latch = null;
-    public Servo basket = null;
+    public DcMotor rightIntake = null;
+    public DcMotor leftIntake = null;
     public BNO055IMU imu = null;
     public ModernRoboticsI2cColorSensor colorSensor = null;
     public SensorMB1242 rightSonic = null;
     public SensorMB1242 leftSonic = null;
-    public DigitalChannel touchSensor = null;
 
     // Set local OpMode Members
     HardwareMap hwMap = null;
@@ -68,43 +64,28 @@ public class HardwareBeep {
         leftBack = hwMap.get(DcMotor.class, "left_back");
         rightFront = hwMap.get(DcMotor.class, "right_front");
         rightBack = hwMap.get(DcMotor.class, "right_back");
-        lift = hwMap.get(DcMotor.class, "lift");
-        latch = hwMap.get(CRServo.class, "latch");
-        arm = hwMap.get(DcMotor.class, "arm");
-        armExtrusion = hwMap.get(DcMotor.class, "arm_extrusion");
-        intake = hwMap.get(DcMotor.class, "intake");
-        basket = hwMap.get(Servo.class, "basket");
         imu = hwMap.get(BNO055IMU.class, "imu");
         colorSensor = hwMap.get(ModernRoboticsI2cColorSensor.class, "color_Sensor");
         leftSonic = hwMap.get(SensorMB1242.class, "left_sonic");
         leftSonic.changeI2cAddress(125);
         rightSonic = hwMap.get(SensorMB1242.class, "right_sonic");
         rightSonic.changeI2cAddress(126);
-        touchSensor = hwMap.get(DigitalChannel.class, "sensor_digital");
 
         // Set Motor and Servo Direction
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
-        lift.setDirection(DcMotor.Direction.FORWARD);
-        arm.setDirection(DcMotor.Direction.FORWARD);
-        armExtrusion.setDirection(DcMotor.Direction.FORWARD);
-        intake.setDirection(DcMotor.Direction.FORWARD);
-        latch.setDirection(CRServo.Direction.FORWARD);
+        leftIntake.setDirection(DcMotor.Direction.FORWARD);
+        rightIntake.setDirection(DcMotor.Direction.REVERSE);
 
         // Set Motor to Zero Power Behavior
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armExtrusion.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Set Servos to Zero Power
-        latch.setPower(0);
 
         // Set Motors to Run Without Encoders
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -113,9 +94,8 @@ public class HardwareBeep {
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Set Motors to Run Using Encoders
-        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armExtrusion.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Set IMU Parameters
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();

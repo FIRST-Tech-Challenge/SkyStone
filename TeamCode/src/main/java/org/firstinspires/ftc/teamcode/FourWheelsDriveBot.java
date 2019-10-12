6e44d5cc7f23d201fc83a339d5424f870c3f301e
Kevin Zhou <kevin.zhou@gmi-mr.com>
2019-10-12 09:36:02 -0700

    Refactory : moving code to bot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWheelsDriveBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWheelsDriveBot.java
@@ -4,14 +4,20 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 //@Autonomous(name="Drive Encoder2", group="Exercises")
 
 public class FourWheelsDriveBot
 {
+    static final double MOTOR_TICK_COUNT = 1120;
+
     public DcMotor leftFront = null;
     public DcMotor rightFront = null;
     public DcMotor leftRear = null;
@@ -19,9 +25,17 @@ public class FourWheelsDriveBot
 //    public DcMotor heavyDutyArm = null;
 
     HardwareMap hwMap = null;
-    private ElapsedTime period = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();
+    private LinearOpMode opMode;
 
-    public FourWheelsDriveBot() {
+    public FourWheelsDriveBot(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    public void print(String message){
+        String caption = "4WD";
+        this.opMode.telemetry.addData(caption, message);
+        this.opMode.telemetry.update();
     }
 
     public void init(HardwareMap ahwMap) {
@@ -42,10 +56,56 @@ public class FourWheelsDriveBot
         rightRear.setPower(0);
 
 //        heavyDutyArm.setPower(0);
+        print("Resetting Encoders");
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        print(String.format("Starting at leftFront: %7d, rightFront:%7d, leftRear:%7d, rightRear:%7d",
+                leftFront.getCurrentPosition(),
+                rightFront.getCurrentPosition(),
+                leftRear.getCurrentPosition(),
+                rightRear.getCurrentPosition()));
+
 //        heavyDutyArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void testOneMotor(DcMotor motor, double speed, int direction){
+        // reset the timeout time and start motion.
+        runtime.reset();
+
+        double timeoutS = 5.0;
+        // make 3 turn
+        int target = motor.getCurrentPosition() + (int)MOTOR_TICK_COUNT * 3 * direction;
+        print(String.format("Start %s @ %7d", motor.getDeviceName(), motor.getCurrentPosition()));
+
+        motor.setTargetPosition(target);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(speed);
+
+        while (this.opMode.opModeIsActive() && (runtime.seconds() < timeoutS) && motor.isBusy()) {
+            // Display it for the driver.
+            print(String.format("Running %s to %7d: @ %7d", motor.getDeviceName(), target, motor.getCurrentPosition()));
+        }
+        // Stop all motion;
+        motor.setPower(0);
+
+        print(String.format("Completed! %s @ %7d", motor.getDeviceName(), motor.getCurrentPosition()));
+
+        this.opMode.sleep(3000);
+
+    }
+
+    public void driveStraightByDistance(int direction, float distance){
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoOpMode.java
@@ -14,69 +14,18 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class TestAutoOpMode extends LinearOpMode {
 
-    FourWheelsDriveBot robot = new FourWheelsDriveBot();
-    static final double MOTOR_TICK_COUNT = 1120;
-    private ElapsedTime runtime = new ElapsedTime();
+    FourWheelsDriveBot robot = new FourWheelsDriveBot(this);
 
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
-        telemetry.addData("Status", "Resetting Encoders");
-        telemetry.update();
-
-        robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0",  "Starting at leftFront: %7d, rightFront:%7d, leftRear:%7d, rightRear:%7d",
-                robot.leftFront.getCurrentPosition(),
-                robot.rightFront.getCurrentPosition(),
-                robot.leftRear.getCurrentPosition(),
-                robot.rightRear.getCurrentPosition());
-        telemetry.update();
 
         waitForStart();
 
-        testOneMotor( robot.leftFront, 0.3, 1);
-        testOneMotor( robot.rightFront, 0.3, 1);
-        testOneMotor( robot.leftRear, 0.3, 1);
-        testOneMotor( robot.rightRear,0.3, 1);
-
+        robot.testOneMotor( robot.leftFront, 0.3, 1);
+        robot.testOneMotor( robot.rightFront, 0.3, 1);
+        robot.testOneMotor( robot.leftRear, 0.3, 1);
+        robot.testOneMotor( robot.rightRear,0.3, 1);
     }
 
-    public void testOneMotor(DcMotor motor, double speed, int direction){
-        // reset the timeout time and start motion.
-        runtime.reset();
-
-        double timeoutS = 5.0;
-        // make 3 turn
-        int target = motor.getCurrentPosition() + (int)MOTOR_TICK_COUNT * 3 * direction;
-        telemetry.addData("Path2",  "Start %s @ %7d", motor.getDeviceName(), motor.getCurrentPosition());
-        telemetry.update();
-
-        motor.setTargetPosition(target);
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor.setPower(speed);
-
-        while (opModeIsActive() && (runtime.seconds() < timeoutS) && motor.isBusy()) {
-            // Display it for the driver.
-            telemetry.addData("Path1",  "Running %s to %7d: @ %7d", motor.getDeviceName(), target, motor.getCurrentPosition());
-            telemetry.update();
-        }
-        // Stop all motion;
-        motor.setPower(0);
-
-        telemetry.addData("Path2",  "Completed! %s @ %7d", motor.getDeviceName(), motor.getCurrentPosition());
-        telemetry.update();
-
-        sleep(3000);
-
-    }
 }

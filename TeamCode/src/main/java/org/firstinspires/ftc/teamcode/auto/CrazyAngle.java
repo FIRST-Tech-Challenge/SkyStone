e2198cf4d870b58c7324cbd64ef20c99346e3a0e
GoldenPigFlynn <flynn@flynnconnolly.com>
2019-10-12 17:49:23 -0700

    Organization, Autonomous
    first autonomous, organized all of the code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/CrazyAngle.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+public class CrazyAngle {
+
+    public static float convertNebula(float inputAngle){
+        float angle = reverseAngle(inputAngle);
+        angle += 90;
+        return normalizeAngle(angle);
+    }
+
+    public static float convertFootprint(float inputAngle) {
+        float angle = reverseAngle(inputAngle);
+        if (angle < 0)
+            angle += 180;
+        angle += 90;
+        return normalizeAngle(angle);
+    }
+
+    public static float convertCrater(float inputAngle) {
+        float angle = reverseAngle(inputAngle);
+        if (angle > 90)
+            angle -= 180;
+        angle += 270;
+        return normalizeAngle(angle);
+    }
+
+    public static float convertRover(float inputAngle) {
+        float angle = reverseAngle(inputAngle);
+       if (angle < 0)
+           angle += 180;
+       angle += 270;
+        return normalizeAngle(angle);
+    }
+
+    public static float reverseAngle(float inputAngle) {
+        return (inputAngle * -1.0f);
+    }
+
+    public static float normalizeAngle(float inputAngle) {
+        return (inputAngle + 360.0f) % 360.0f;
+    }
+}

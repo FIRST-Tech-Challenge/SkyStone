dfb1562ea64ca65393090e72cfa999e5350e36a2
Chris Costakes <ccostakes@gmail.com>
2019-10-13 16:12:42 -0400

    Added more saturday code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMAutonomous.java
@@ -1,19 +1,50 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
 
 @Autonomous(name = "MMAutoTest")
 public class MMAutonomous extends LinearOpMode {
 
     Robot robot = new Robot();
+    enum Skystone {LEFT, CENTER, RIGHT}
+    Skystone skystonePos = Skystone.LEFT;
+    double distanceToBuildZone = 0.0; // distance to skybridge from close edge of block
+    double speed = 0.25;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
-        // Distance to quarry from wall is 47.5in
-        robot.driveForwardDistance(47.5, 0.5);
+
+        waitForStart();
+        // Drive to quarry
+        robot.driveForwardDistance(47.0, speed);
+        switch (skystonePos) {
+            case LEFT:
+                distanceToBuildZone = 32;
+                // strafe to block
+                robot.strafeTime(-speed, 500);
+                break;
+            case CENTER:
+                distanceToBuildZone = 28;
+                break;
+            case RIGHT:
+                distanceToBuildZone = 24;
+                // strafe to block
+                robot.strafeTime(speed, 500);
+                break;
+        }
+
+        // back up
+        robot.driveForwardDistance(-6, speed);
+        // turn towards skybridge
+        //robot.turnRight(speed, 2000);
+        // drive to skybridge
+        robot.driveForwardDistance(distanceToBuildZone + 6, speed);
+        // park
+        robot.driveForwardDistance(-6, speed);
+
 
 
     }

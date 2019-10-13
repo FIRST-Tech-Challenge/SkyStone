70cfe0b1aed2a6929ba7df895a43dc7ad43c3d64
Chris Costakes <ccostakes@gmail.com>
2019-10-13 15:44:11 -0400

    remove test commments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1MM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1MM.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-//this is a test
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;

f8de5863238df05267d8e47ace4c03cc5ab729e8
Jared <jaredgoodman03@gmail.com>
2019-10-14 15:53:06 -0700

    Added the files from the Arm branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTeleop.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.components.DriveSystem;
+
+import java.util.EnumMap;
+
+
+@TeleOp(name = "Drive", group="TeleOp")
+public class DriveTeleop extends OpMode {
+
+    private DriveSystem driveSystem;
+
+    public void init(){
+        telemetry.addLine("in init");
+
+        EnumMap<DriveSystem.MotorNames, DcMotor> driveMap = new EnumMap<>(DriveSystem.MotorNames.class);
+        for(DriveSystem.MotorNames name : DriveSystem.MotorNames.values()){
+            telemetry.addLine("adding motors");
+            driveMap.put(name,hardwareMap.get(DcMotor.class, name.toString()));
+        }
+        telemetry.update();
+
+        driveSystem = new DriveSystem(driveMap, hardwareMap.get(BNO055IMU.class, "imu"));
+    }
+
+
+    public void loop(){
+            float rx = gamepad1.right_stick_x;
+            float ry = gamepad1.right_stick_y;
+            float lx = gamepad1.left_stick_x;
+            float ly = gamepad1.left_stick_y;
+            driveSystem.drive(rx, ry, lx, ly);
+    }
+}

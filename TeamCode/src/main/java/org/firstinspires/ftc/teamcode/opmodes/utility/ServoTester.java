8963ab1130ab7d65e879f42e35217c6d9c1acb37
Randall-Scharpf <randall.scharpf@gmail.com>
2019-10-14 22:41:34 -0700

    Prepare servo tester for new robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/utility/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/utility/ServoTester.java
@@ -4,26 +4,51 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import java.util.Map;
+
 @TeleOp(name = "ServoTester", group = "none")
 public class ServoTester extends OpMode {
 
     private Servo arm;
+    private String name;
+    private Object[] servos;
+    private int index;
+    private boolean pressed;
     private double position;
 
     @Override
     public void init() {
-        arm = hardwareMap.get(Servo.class, "arm");
+        servos = hardwareMap.servo.entrySet().toArray();
+        telemetry.addData("Number of servos found", servos.length);
+        telemetry.update();
+        if (servos.length == 0) {
+            throw new IllegalArgumentException("Needs at least one servo plugged in");
+        }
+        index = 0;
         position = 0;
+        pressed = false;
     }
 
     @Override
     public void loop() {
+        arm = (Servo) ((Map.Entry) servos[index]).getValue();
+        name = (String) ((Map.Entry) servos[index]).getKey();
         if (gamepad1.start && !gamepad1.back && position < 1) {
             position = position + 0.001;
         }
-        if (gamepad1.back && !gamepad1.start && position > 1) {
+        if (gamepad1.back && !gamepad1.start && position > 0) {
             position = position - 0.001;
         }
         arm.setPosition(position);
+        telemetry.addData("Servo", name);
+        telemetry.addData("Position", position);
+        telemetry.update();
+        if (gamepad1.dpad_up && !gamepad1.dpad_down) {
+            index++;
+        }
+        if (gamepad1.dpad_down && !gamepad1.dpad_up) {
+            index += servos.length - 1;
+        }
+        index %= servos.length;
     }
 }

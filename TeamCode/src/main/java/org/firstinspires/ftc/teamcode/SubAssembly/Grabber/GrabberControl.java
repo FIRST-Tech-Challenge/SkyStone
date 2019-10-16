093ced351bbbe15a50147e1ca3126a07f0c6f950
gardnermgg <54294170+gardnermgg@users.noreply.github.com>
2019-10-16 14:33:24 -0400

    renamed claimer to grabber

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Grabber/GrabberControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Grabber/GrabberControl.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.SubAssembly.Claimer;
+package org.firstinspires.ftc.teamcode.SubAssembly.Grabber;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;

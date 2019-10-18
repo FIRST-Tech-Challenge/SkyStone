8e3437de8b90c6b6cd8c0b6fa70074affab37789
Nabo Yu <54687548+naboyu@users.noreply.github.com>
2019-10-18 14:59:45 -0700

    B&A v1.5 Fixed more stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+
+public class IntakeTest extends LinearOpMode {
+
+    public void runOpMode() {
+        DcMotor l = hardwareMap.dcMotor.get("0");
+        l.setDirection(DcMotor.Direction.REVERSE);
+        l.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DcMotor r = hardwareMap.dcMotor.get("1");
+        r.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                l.setPower(0.5);
+                r.setPower(0.5);
+            }
+            if (gamepad1.b) {
+                l.setPower(-0.5);
+                r.setPower(-0.5);
+            }
+            idle();
+        }
+    }
+
+}

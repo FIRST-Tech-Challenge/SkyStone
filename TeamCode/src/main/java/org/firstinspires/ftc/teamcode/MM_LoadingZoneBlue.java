368f2793a4e9120d5486bac1bd404a2e467f025c
Noah Costakes <ncostakes@icloud.com>
2019-10-19 17:21:41 -0400

    mirror red auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MM_LoadingZoneBlue.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Loading Zone Blue")
+public class MM_LoadingZoneBlue extends LinearOpMode {
+
+    Robot robot = new Robot();
+    enum Skystone {LEFT, CENTER, RIGHT}
+    Skystone skystonePos = Skystone.LEFT;
+    double distanceToBuildZone = 0.0; // distance to skybridge from close edge of block
+    double speed = 0.4;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        waitForStart();
+        // Drive to quarry
+        robot.driveForwardDistance(47.0 - robot.ROBOT_EXTENDED_LENGTH, speed, this);
+        switch (skystonePos) {
+            case LEFT:
+                distanceToBuildZone = 32 - robot.ROBOT_EXTENDED_LENGTH;
+                // strafe to block
+                robot.strafeTime(-speed, 2000);
+                break;
+            case CENTER:
+                distanceToBuildZone = 28 - robot.ROBOT_EXTENDED_LENGTH;
+                break;
+            case RIGHT:
+                distanceToBuildZone = 24 - robot.ROBOT_EXTENDED_LENGTH;
+                // strafe to block
+                robot.strafeTime(speed, 2000);
+                break;
+        }
+
+        // back up
+        robot.driveForwardDistance(6, -speed, this);
+        // turn towards skybridge
+        robot.turnRight(-speed, 1475);
+        // drive to skybridge
+        robot.driveForwardDistance(distanceToBuildZone + 6, speed, this);
+        // park
+        robot.driveForwardDistance(6, -speed, this);
+
+
+
+    }
+
+
+
+}

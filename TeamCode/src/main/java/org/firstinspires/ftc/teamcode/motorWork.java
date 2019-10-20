76b7f92f697c18095f4632c430d6f0e3f6c41fce
AlphaLocke <47842170+AlphaLocke@users.noreply.github.com>
2019-10-20 09:15:09 -0500

    Encoder Controller Work
    Some changes to the extensionController as well as to backUp file.
    
    Major Changes:
    - motorWork has functional arm extensions controller. Form is very basic as further functionality must be tested in person.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorWork.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorWork.java
@@ -9,6 +9,8 @@ public class motorWork extends LinearOpMode {
 
     public DcMotor armE;
     public DcMotor armR;
+    public double unitRate = 75.59;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,10 +18,30 @@ public class motorWork extends LinearOpMode {
         armE = hardwareMap.get(DcMotor.class, "armExtender");
         armR = hardwareMap.get(DcMotor.class, "armRaiser");
 
+        armE.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armE.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         waitForStart();
         while(opModeIsActive()) {
-            armE.setPower(gamepad1.right_stick_y);
-            armR.setPower(gamepad1.left_stick_y);
+            while ((threshold(armE, 288)) && gamepad1.left_trigger>0) {
+                telemetry.addData("Iteration:", armE.getCurrentPosition());
+                armE.setPower(0.4);
+                telemetry.update();
+            }
+            armE.setPower(0);
         }
+    }
+    public int targetPos(double dist) {
+        int position = (int) Math.round(unitRate * dist);
+        if (0 == 0) {
+            return position;
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean threshold(DcMotor wheel, int targetPos) {
+        int tolerance = targetPos - 5;
+        return wheel.getCurrentPosition() <= tolerance;
     }
 }

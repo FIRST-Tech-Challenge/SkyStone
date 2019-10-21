babfa28cdf4566fa999c1ed175c8763381f1f3bb
Bill Zheng <31973301+BILLLLLLL@users.noreply.github.com>
2019-10-21 15:42:51 -0700

    Update IntakeTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
@@ -8,33 +8,26 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class IntakeTest extends LinearOpMode {
 
-    private DcMotor l = null;
-    private DcMotor r = null;
-
     public void runOpMode() {
         DcMotor l = hardwareMap.dcMotor.get("0");
         l.setDirection(DcMotor.Direction.REVERSE);
         l.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         DcMotor r = hardwareMap.dcMotor.get("1");
         r.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         waitForStart();
         while (opModeIsActive()) {
             while (gamepad1.a) {
-                setPower(0.5);
+                l.setPower(0.5);
+                r.setPower(0.5);
             }
             while (gamepad1.b) {
-                setPower(-0.5);
+                l.setPower(-0.5);
+                r.setPower(-0.5);
             }
-            setPower(0);
+            l.setPower(0);
+            r.setPower(0);
             idle();
         }
-
-    }
-
-    private void setPower(double power) {
-        l.setPower(power);
-        r.setPower(power);
     }
 
 }

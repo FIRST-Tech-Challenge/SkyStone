f2cdf595efd18527bc98b0fd2c7c5f47b985ecdf
Steven K <steven@pointcircle.com>
2019-10-22 16:18:08 -0700

    Bug one

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTelop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTelop.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp(name="Robot: Teleop", group="Steve")
@@ -10,8 +9,7 @@ public class RobotTelop extends LinearOpMode {
 
     /* Declare OpMode members. */
     RobotHardware robot      = new RobotHardware();   // Use a Pushbot's hardware
-    double servoOffset       = 0;                     // Servo mid position
-    final double SERVO_SPEED = 0.02;                  // sets rate to move servo
+    int counter       = 0;                     // Servo mid position
 
     @Override
     public void runOpMode() {
@@ -26,19 +24,11 @@ public class RobotTelop extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Use gamepad left & right Bumpers to move the servo
-            if (gamepad1.right_bumper)
-                servoOffset += SERVO_SPEED;
-            else if (gamepad1.left_bumper)
-                servoOffset -= SERVO_SPEED;
 
-            servoOffset = Range.clip(servoOffset, -0.5, 0.5);
-            robot.servoOne.setPosition(robot.MID_SERVO + servoOffset);
-
-            telemetry.addData("servo_one",  "Offset = %.2f", servoOffset);
+            telemetry.addData("We be counting",  "Counter = %7d", counter);
 
             // Pace this loop so servo speed is reasonable.
-            sleep(50);
+            sleep(1000);
 
         }
     }

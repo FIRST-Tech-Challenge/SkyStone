a7d2c681a4ff04b111afcc90060982db79fd9182
Windy Feng <1745500559@qq.com>
2019-10-23 15:18:50 -0400

    Update Autonomous Program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_BlueScanBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_BlueScanBasic.java
@@ -3,23 +3,19 @@ package org.firstinspires.ftc.teamcode.david_cao.generation1_linda_code;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.darbots.darbotsftclib.libcore.OpModes.DarbotsBasicOpMode;
-import org.darbots.darbotsftclib.libcore.calculations.dimentionalcalculation.XYPlaneCalculations;
+import org.darbots.darbotsftclib.libcore.calculations.dimentionalcalculation.Robot3DPositionIndicator;
 import org.darbots.darbotsftclib.libcore.sensors.cameras.RobotOnPhoneCamera;
-import org.darbots.darbotsftclib.libcore.tasks.chassis_tasks.GyroGuidedTurn;
+import org.darbots.darbotsftclib.libcore.templates.RobotCore;
 import org.darbots.darbotsftclib.libcore.templates.other_sensors.RobotCamera;
-import org.darbots.darbotsftclib.season_specific.skystone.tfod_detection.SkyStoneStoneDifferentiation;
+import org.darbots.darbotsftclib.season_specific.skystone.navigation.SkyStoneNavigation;
 import org.firstinspires.ftc.teamcode.robot_common.Robot4100Common;
 
-import java.util.ArrayList;
-
-@Autonomous(name = "4100Gen1Auto-BlueScanBasic",group="4100")
-public class Robot4100Generation1_BlueScanBasic extends DarbotsBasicOpMode {
-    private SkyStoneStoneDifferentiation m_SkyStoneDetection;
+@Autonomous(group = "4100", name="4100Gen1Auto-BlueScanBasic")
+public class Robot4100Generation1_BlueScanBasic extends DarbotsBasicOpMode<Robot4100Generation1_LindaCore> {
     private Robot4100Generation1_LindaCore m_RobotCore;
-    private float m_OldAng;
-    private boolean m_LastDriveSkyStone = false;
-    private boolean m_OnRecognition = false;
-    private int recognitionResult = 0;
+    private SkyStoneNavigation m_Navigation;
+    private int ScanResult = 0;
+
     @Override
     public Robot4100Generation1_LindaCore getRobotCore() {
         return m_RobotCore;
@@ -28,243 +24,101 @@ public class Robot4100Generation1_BlueScanBasic extends DarbotsBasicOpMode {
     @Override
     public void hardwareInitialize() {
         this.m_RobotCore = new Robot4100Generation1_LindaCore(this.hardwareMap);
-        RobotCamera Camera = new RobotOnPhoneCamera(this,false, RobotOnPhoneCamera.PhoneCameraDirection.Back, Robot4100Common.VUFORIA_LICENSE);
-        this.m_SkyStoneDetection = new SkyStoneStoneDifferentiation(Camera,this.hardwareMap,Robot4100Generation1_Settings.AUTONOMOUS_TENSORFLOW_PREVIEW, Robot4100Generation1_Settings.AUTONOMOUS_MINIMUM_CONFIDENCE);
+        RobotCamera Camera = new RobotOnPhoneCamera(this,Robot4100Generation1_Settings.AUTONOMOUS_TENSORFLOW_PREVIEW, RobotOnPhoneCamera.PhoneCameraDirection.Back, Robot4100Common.VUFORIA_LICENSE);
+        Robot3DPositionIndicator CameraPosition = new Robot3DPositionIndicator(Robot4100Generation1_Settings.AUTONOMOUS_CAMERAPOSONPHONE);
+        this.m_Navigation = new SkyStoneNavigation(CameraPosition,Camera);
+        this.m_Navigation.setActivated(true);
     }
 
     @Override
     public void hardwareDestroy() {
-        this.m_RobotCore.terminate();
-        this.m_SkyStoneDetection.terminate();
-    }
-
-    public void preRecAng(){
-        this.m_RobotCore.getGyro().updateStatus();
-        this.m_OldAng = m_RobotCore.getGyro().getHeading();
-    }
-
-    public boolean fixAng(){
-        this.getRobotCore().getGyro().updateStatus();
-        float newAng = getRobotCore().getGyro().getHeading();
-        float deltaAng = newAng - m_OldAng;
-        deltaAng = XYPlaneCalculations.normalizeDeg(deltaAng);
-        if(deltaAng >= 1) {
-            this.getRobotCore().getChassis().replaceTask(
-                    new GyroGuidedTurn(
-                            this.getRobotCore().getChassis(),
-                            this.getRobotCore().getGyro(),
-                            -deltaAng,
-                            0.05
-                    )
-            );
-            return waitForDrive();
-        }else{
-            return this.opModeIsActive();
-        }
-
-    }
-
-    @Override
-    public boolean waitForDrive(){
-        while(this.opModeIsActive() && this.getRobotCore().getChassis().isBusy()){
-            this.m_RobotCore.updateStatus();
-        }
-        __tryRecognizeStoneOnce();
-        return this.opModeIsActive();
-    }
-
-    public void __tryRecognizeStoneOnce(){
-        if(m_OnRecognition) {
-            ArrayList<SkyStoneStoneDifferentiation.RecognitionResult> recognitionResults = this.m_SkyStoneDetection.getUpdatedRecognitions();
-            if (recognitionResults != null && (!recognitionResults.isEmpty())) {
-                for (SkyStoneStoneDifferentiation.RecognitionResult RRI : recognitionResults) {
-                    if (RRI.getStoneType() == SkyStoneStoneDifferentiation.StoneType.SKYSTONE && (RRI.getTop() < RRI.getImageHeight() / 2) && (RRI.getBottom() > RRI.getImageHeight() / 2)){
-                        m_LastDriveSkyStone = true;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    public void waitForGamepadX(){
-        while(this.opModeIsActive() && gamepad1.x){
-            this.getRobotCore().updateStatus();
-        }
-    }
-
-    public Object getRecognitionResult(){
-        return Integer.toString(this.recognitionResult);
+        this.m_Navigation.setActivated(false);
     }
 
     @Override
     public void RunThisOpMode() {
-        waitForStart();
-        if(!this.opModeIsActive()) {
-            return;
-        }
-        telemetry.addData("recognitionResult",getRecognitionResult());
-        //Drive 34 cm to the right
-        //Each stone is 20cm Wide
-        //Distance between Camera & AutoGrabber = 4.8 cm
-        //wall to the surface of the stone 75 cm
+        this.getRobotCore().getChassis().setGyroGuidedDriveEnabled(true);
+        this.getRobotCore().getChassis().setGyroGuidedDrivePublicStartingAngleEnabled(true);
+        this.getRobotCore().getChassis().updateGyroGuidedPublicStartingAngle();
 
-        double LENGTH_OF_EACH_STONE = Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE;
-
-        m_LastDriveSkyStone = false;
-        m_OnRecognition = true;
-        preRecAng();
         this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedXDistanceTask(
-                42,
-                0.2
+                40,
+                0.6
         ));
         if(!waitForDrive()){
             return;
         }
-        waitForGamepadX();
 
-        this.m_SkyStoneDetection.setActivated(true);
-        this.getRobotCore().getChassis().replaceTask(
-                this.getRobotCore().getChassis().getFixedXDistanceTask(
-                        -5,
-                        0.1
-                )
-        );
-        this.getRobotCore().getChassis().addTask(
-                this.getRobotCore().getChassis().getFixedXDistanceTask(
-                        5,
-                        0.1
-                )
-        );
-        if (!waitForDrive()) {
-            return;
-        }
-
-        if(!fixAng()){
-            return;
-        }
-
-        double firstStepExtraForwardDistance = 0;
-        int[] stepNum = {1, 2, 3};
-
-        for(int i=0;i<3;i++) {
+        double firstScanExtraDistance = 0;
+        int[] stepResult = {1,2,3};
+        for(int i=0; i<3; i++){
             boolean foundStone = false;
-            if (m_LastDriveSkyStone || i==2){
+            if(this.m_Navigation.getDarbotsRobotAxisStonePosition() != null || i == 2){
                 foundStone = true;
             }
             if(foundStone){
-                this.recognitionResult = stepNum[i];
+                this.ScanResult = stepResult[i];
                 break;
             }
-            m_OnRecognition = false;
-            firstStepExtraForwardDistance += LENGTH_OF_EACH_STONE;
-            this.getRobotCore().getChassis().replaceTask(
-                    this.getRobotCore().getChassis().getFixedZDistanceTask(
-                            -LENGTH_OF_EACH_STONE,
-                            0.1
-                    )
-            );
-
-            if(!waitForDrive()){
-                return;
-            }
-            m_OnRecognition = true;
-            if(i==0) {
-                this.getRobotCore().getChassis().replaceTask(
-                        this.getRobotCore().getChassis().getFixedXDistanceTask(
-                                -5,
-                                0.1
-                        )
-                );
-                this.getRobotCore().getChassis().addTask(
-                        this.getRobotCore().getChassis().getFixedXDistanceTask(
-                                5,
-                                0.1
-                        )
-                );
-                if (!waitForDrive()) {
-                    return;
-                }
-            }
-            telemetry.update();
-            waitForGamepadX();
-        }
-
-        telemetry.update();
-
-        //Grab Stone;
-        this.m_SkyStoneDetection.setActivated(false);
-        this.m_OnRecognition = false;
-
-        if(!fixAng()){
-            return;
-        }
-
-        if(true){
+            firstScanExtraDistance += Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE;
             this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
-                    3,
-                    0.1
+                    -Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE,
+                    0.25
             ));
             if(!waitForDrive()){
                 return;
             }
-            waitForGamepadX();
         }
-
+        double firstScanZOffset = 0;
+        Robot3DPositionIndicator firstScanStonePosition = this.m_Navigation.getDarbotsRobotAxisStonePosition();
+        if(firstScanStonePosition != null){
+            firstScanZOffset = firstScanStonePosition.getZ();
+        }
+        firstScanExtraDistance -= firstScanZOffset;
+        firstScanExtraDistance -= Robot4100Generation1_Settings.AUTONOMOUS_DISTANCE_BETWEEN_PHONE_AND_STONEGRABBER;
+        this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
+                firstScanZOffset + Robot4100Generation1_Settings.AUTONOMOUS_DISTANCE_BETWEEN_PHONE_AND_STONEGRABBER,
+                0.25
+        ));
+        if(!waitForDrive()){
+            return;
+        }
         this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedXDistanceTask(
-                75-42+20,
+                75-40+10,
                 0.2
         ));
         if(!waitForDrive()){
             return;
         }
-        waitForGamepadX();
 
         this.getRobotCore().setAutonomousDragStoneServoToDrag(true);
         sleep(400);
-        waitForGamepadX();
 
         this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedXDistanceTask(
-                -70,
-                0.2
-        ));
-        if(!waitForDrive()){
-            return;
-        }
-        waitForGamepadX();
-
-        if(!fixAng()){
-            return;
-        }
-
-        this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
-                130 + firstStepExtraForwardDistance,
+                -50,
                 0.5
         ));
         if(!waitForDrive()){
             return;
         }
 
-        waitForGamepadX();
-
+        this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
+                130 + firstScanExtraDistance,
+                1.0
+        ));
+        if(!waitForDrive()){
+            return;
+        }
 
         this.getRobotCore().setAutonomousDragStoneServoToDrag(false);
-        sleep(300);
-        waitForGamepadX();
-
-        if(!fixAng()){
-            return;
-        }
+        sleep(400);
 
         this.getRobotCore().getChassis().replaceTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
-                -40,
+                -35,
                 0.5
         ));
         if(!waitForDrive()){
             return;
         }
-
-        waitForGamepadX();
-
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_Settings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_Settings.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.david_cao.generation1_linda_code;
 
+import org.darbots.darbotsftclib.libcore.calculations.dimentionalcalculation.Robot3DPositionIndicator;
 import org.darbots.darbotsftclib.libcore.motortypes.AndyMark2964;
 import org.darbots.darbotsftclib.libcore.motortypes.AndyMark3637;
 import org.darbots.darbotsftclib.libcore.templates.motor_related.MotorType;
@@ -9,6 +10,15 @@ public class Robot4100Generation1_Settings {
     public static final double AUTONOMOUS_MINIMUM_CONFIDENCE = 0.5;
     public static final double AUTONOMOUS_LENGTH_FOR_EACH_STONE = 20;
     public static final boolean AUTONOMOUS_TENSORFLOW_PREVIEW = true;
+    public static final double AUTONOMOUS_DISTANCE_BETWEEN_PHONE_AND_STONEGRABBER = 4.0;
+    public static final Robot3DPositionIndicator AUTONOMOUS_CAMERAPOSONPHONE = new Robot3DPositionIndicator(
+            0,
+            10,
+            0,
+            -90,
+            90,
+            0
+    );
     //------------End of Autonomous Settings------------
 
     //------------TeleOp Settings------------

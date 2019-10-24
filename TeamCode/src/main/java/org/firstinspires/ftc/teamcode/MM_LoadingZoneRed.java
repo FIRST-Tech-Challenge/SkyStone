0ca5adba144276a52a5c090874241ca54ae88390
Noah Costakes <ncostakes@icloud.com>
2019-10-23 20:30:09 -0400

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MM_LoadingZoneRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MM_LoadingZoneRed.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name = "Loading Zone Red")
 public class MM_LoadingZoneRed extends LinearOpMode {
@@ -12,9 +13,22 @@ public class MM_LoadingZoneRed extends LinearOpMode {
     double distanceToBuildZone = 0.0; // distance to skybridge from close edge of block
     double speed = 0.4;
 
+    //Gripper Test
+    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   50;     // period of each cycle
+    static final double MAX_POS     =  1.0;     // Maximum rotational position
+    static final double MIN_POS     =  0.0;     // Minimum rotational position
+    Servo servo;
+    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    boolean rampUp = true;
+
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
+
+        servo = hardwareMap.get(Servo.class, "left_hand");
 
         waitForStart();
         // detect sky stone with camera
@@ -37,8 +51,10 @@ public class MM_LoadingZoneRed extends LinearOpMode {
                 robot.strafeTime(speed, 2000);
                 break;
 
-                // Pick Block up with arm
         }
+        // Pick Block up with arm
+
+
         Thread.sleep(500);
         // back up
         robot.driveForwardDistance(6, -speed, this);

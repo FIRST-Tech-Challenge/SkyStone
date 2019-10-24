cfaede2d428ee34e2f276f7536b4112e36bcd4f3
Randall-Scharpf <1017491@etusd.org>
2019-10-23 20:50:25 -0700

    jasons stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/TimedAutoForPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/TimedAutoForPark.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
+import android.graphics.Color;
+
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -21,13 +23,16 @@ public class TimedAutoForPark extends LinearOpMode {
     double aParam = 315;
     double binvParam = 0.605;
     double cParam = 169.7;
-    double maxDist = 6.0; // inches
+    double maxDist = 6.0; // inchesrev control hub
+
+    float hsvValues[] = {0F, 0F, 0F};
+    final double SCALE_FACTOR = 255;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         DcMotorEx leftFront = hardwareMap.get(DcMotorEx.class, "left1");
-        DcMotorEx leftBack = hardwareMap.get(DcMotorEx.class, "left2");
+        /*DcMotorEx leftBack = hardwareMap.get(DcMotorEx.class, "left2");
         DcMotorEx rightFront = hardwareMap.get(DcMotorEx.class, "right1");
         DcMotorEx rightBack = hardwareMap.get(DcMotorEx.class, "right2");
 
@@ -43,6 +48,8 @@ public class TimedAutoForPark extends LinearOpMode {
 
         MecanumDrive train = new MecanumDriveImpl(leftFront, leftBack, rightFront, rightBack, null);
         driveTrain = new MecanumController(train);
+
+*/
 
         Servo servoTest = hardwareMap.get(Servo.class, "armTune");
 
@@ -63,53 +70,41 @@ public class TimedAutoForPark extends LinearOpMode {
         runtime.reset();
         int DST_CONST = 46;
         servoTest.setPosition(0);
+//        secs * spedConst; 0.9 sec/30 inch
         //driveTrain.translate(0.2, 0.75, MecanumDrive.TranslationMethod.CONSTANT_SPEED);
         sleep((long) 27*DST_CONST);
         //need to fix so that its around ~2 cm away from blocks
         while (opModeIsActive()) {
             double avg = 0;
-            double avgRed = 0;
-            double avgGreen = 0;
-            double avgBlue = 0;
-            double initialRtoB = 0;
-            double initialGtoB = 0;
-            double actualAvg = 0;
-            for (int i = 0; i < 10; i++) {
-                double initial = Math.pow((color.rawOptical() - cParam) / aParam, -binvParam);
+            for (int i = 0; i < 10; i++){
+                double initial = Math.pow((color.rawOptical()-cParam)/aParam, -binvParam);
                 avg = avg + initial;
-                avgBlue += color.blue();
-                avgGreen += color.green();
-                avgRed += color.red();
-                //2.05 RtoB
-                //2.94 GtoB with yellow block
             }
-            actualAvg = avg / 10;
-            avgBlue /= 10;
-            avgGreen /= 10;
-            avgRed /= 10;
-            if (runtime.seconds() < 2) {
-                initialRtoB = (avgRed)/(avgBlue);
-                initialGtoB = (avgGreen)/(avgBlue);
-            }
+            double actualAvg = avg/10;
 
-            telemetry.addData("R", avgRed);
-            telemetry.addData("G", avgGreen);
-            telemetry.addData("B", avgBlue);
-            telemetry.addData("RtoB", avgRed / avgBlue);
-            telemetry.addData("GtoB", avgGreen / avgBlue);
+            Color.RGBToHSV((int) (color.red() * SCALE_FACTOR),
+                    (int) (color.green() * SCALE_FACTOR),
+                    (int) (color.blue() * SCALE_FACTOR),
+                    hsvValues);
+
+            telemetry.addData("R", color.red());
+            telemetry.addData("G", color.green());
+            telemetry.addData("B", color.blue());
             telemetry.addData("InputData", actualAvg);
+
             //telemetry.addData("Distance", color.getDistance(DistanceUnit.CM));
             telemetry.update();
             sleep(500);
-            //Reads color values a// nd sends them to driver station//red to blue => 1.7 - 2.5
-            //green to blue => 3 - 4
-
-            if ((avgRed / avgBlue) > initialRtoB && (avgGreen / avgBlue) > initialGtoB && actualAvg < maxDist) {
-//                servoTest.setPosition(1);
-//                driveTrain.translate(0, 0, MecanumDrive.TranslationMethod.CONSTANT_SPEED);
-                telemetry.addData("This detects yellow block", 5);
-//                telemetry.update();
-//                sleep(2000);
+            //Reads color values and sends them to driver station
+            if (color.red() > 2 * color.blue() && color.green() > 3 * color.blue()) {
+                servoTest.setPosition(1);
+                driveTrain.translate(0, 0, MecanumDrive.TranslationMethod.CONSTANT_SPEED);
+                //telemetry.addData("This detects yellow block", 5);
+                telemetry.update();
+//                telemetry.addData("R", color.red());
+//                telemetry.addData("G", color.green());
+//                telemetry.addData("B", color.blue());
+                sleep(2000);
             }
 //            else {
 //                telemetry.addData("Detects it's too close", "true");
@@ -117,7 +112,6 @@ public class TimedAutoForPark extends LinearOpMode {
 //            }
 
             //The if statement above allows the robot to detect yellow to find a block and move the servo accordingly
-            sleep(1);
         }
 
 //        driveTrain.translate(0,0, MecanumDrive.TranslationMethod.CONSTANT_SPEED);

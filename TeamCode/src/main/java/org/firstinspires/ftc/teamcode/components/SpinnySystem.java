93414e00136c3aad9cc443b2b0d243df9aff54bc
LexiShew <lexishew@live.com>
2019-10-25 15:54:53 -0700

    Revert "Adds Log stuff"
    This reverts commit b7eac7733259385a579e1fffd649580abb2227ad.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/SpinnySystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/SpinnySystem.java
@@ -1,14 +1,10 @@
 package org.firstinspires.ftc.teamcode.components;
 
-import android.util.Log;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import java.util.EnumMap;
 
 public class SpinnySystem {
-    private final String TAG = "SpinnySystem";
-
     public enum MotorNames {
         RIGHTINTAKE, LEFTINTAKE
     }
@@ -21,23 +17,25 @@ public class SpinnySystem {
     }
 
     public void initMotors() {
-        Log.d(TAG, "in init");
         motors.forEach((name, motor) -> {
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motor.setDirection(name == MotorNames.LEFTINTAKE ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            if (name.equals(motors.get(MotorNames.LEFTINTAKE))) {
+                motor.setDirection(DcMotorSimple.Direction.FORWARD);
+            } else if (name.equals(motors.get(MotorNames.RIGHTINTAKE))) {
+                motor.setDirection(DcMotorSimple.Direction.REVERSE);
+            }
         });
         setMotorPower(0);
     }
 
     public void setMotorPower(double power) {
-        Log.d(TAG, "in set power");
         for (DcMotor motor : motors.values()) {
             motor.setPower(power);
         }
     }
 
     public void spin(boolean leftBumper, boolean rightBumper) {
-        Log.d(TAG, "in spin");
         if (leftBumper) {
             motors.get(MotorNames.RIGHTINTAKE).setPower(-0.4);
             motors.get(MotorNames.LEFTINTAKE).setPower(0.4);

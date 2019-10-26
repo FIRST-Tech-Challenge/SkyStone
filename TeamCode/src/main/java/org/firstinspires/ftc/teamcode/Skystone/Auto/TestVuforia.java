80c40ac070d9ae01cd7da0c50aabcf9399ca5c83
Khue_Puppy <dominhkhue@gmail.com>
2019-10-26 11:23:04 -0700

    Vuforia function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/TestVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/TestVuforia.java
@@ -20,69 +20,8 @@ import org.firstinspires.ftc.teamcode.Skystone.Robot;
 public class TestVuforia extends AutoBase {
     private static final String VUFORIA_KEY = "AbSCRq//////AAAAGYEdTZut2U7TuZCfZGlOu7ZgOzsOlUVdiuQjgLBC9B3dNvrPE1x/REDktOALxt5jBEJJBAX4gM9ofcwMjCzaJKoZQBBlXXxrOscekzvrWkhqs/g+AtWJLkpCOOWKDLSixgH0bF7HByYv4h3fXECqRNGUUCHELf4Uoqea6tCtiGJvee+5K+5yqNfGduJBHcA1juE3kxGMdkqkbfSjfrNgWuolkjXR5z39tRChoOUN24HethAX8LiECiLhlKrJeC4BpdRCRazgJXGLvvI74Tmih9nhCz6zyVurHAHttlrXV17nYLyt6qQB1LtVEuSCkpfLJS8lZWS9ztfC1UEfrQ8m5zA6cYGQXjDMeRumdq9ugMkS";
 
-    public static double[][] underLander = {{8,0},{8,50},{20,50}};
-
     @Override
     public void runOpMode() {
-
-        // init vuforia
-
-        Robot robot = new Robot(this.hardwareMap, this.telemetry, this);
-
-        waitForStart();
-
-        robot.changeRunModeToUsingEncoder();
-
-        // start odometry
-        Position2D position2D = new Position2D(robot);
-        position2D.startOdometry();
-
-        robot.resetEncoders();
-
-        robot.changeRunModeToUsingEncoder();
-        telemetry.addLine("Got into runopmode");
-
-        robot.moveToPoint(11.5 ,0,0.4,1,Math.toRadians(0));
-        telemetry.addLine("done with move");
-        telemetry.update();
-
-        telemetry.addLine("go to point");
-        int position = 0;
-        int vuforiaPosition = robot.detectTensorflow();
-        robot.intakeRight.setPower(1);
-
-        robot.intakeLeft.setPower(1);
-
-        if (vuforiaPosition == 2) {
-            telemetry.addLine("left");
-            robot.moveToPoint(39, 9, 0.55, 0.5, Math.toRadians(0));
-        } else if (vuforiaPosition == 0){
-            telemetry.addLine("right");
-            robot.moveToPoint(39, -9, 0.55, 0.5, Math.toRadians(0));
-        } else {
-            telemetry.addLine("center");
-            robot.moveToPoint(39, 0, 0.55, 0.5, Math.toRadians(0));
-        }
-        telemetry.addLine("Done with detect");
-        telemetry.update();
-        sleep(1000);
-        robot.intakeLeft.setPower(0);
-        robot.intakeRight.setPower(0);
-
-        //robot.absoluteTurn(Math.toRadians(179),1);
-
-        //robot.moveToPoint(15,0,0.6,0.5,Math.toRadians(0));
-        telemetry.addLine("Done with move");
-
-        //robot.absoluteTurn(Math.toRadians(90),1);
-        telemetry.update();
-//        telemetry.addData("Position: -1:Left, 0:Center, 1:Right", position);
-//        telemetry.update();
-//        telemetry.addLine("go back");
-//        robot.moveToPoint(7,0,0.5,0.5, Math.toRadians(180));
-//        robot.finalTurn(Math.toRadians(90))
-//        telemetry.addLine("go under lander");
-//        PathPoints goUnderLander = new PathPoints(underLander, 9);
-//        robot.moveFollowCurve(goUnderLander.targetPoints);
+        robot.GoToVuforia();
     }
 }

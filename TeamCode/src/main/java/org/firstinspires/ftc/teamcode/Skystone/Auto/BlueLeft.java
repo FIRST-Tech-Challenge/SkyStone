d753c552b6e85d08bcb8a45980b3842ff8fa88f1
Ethan <e3lcool@gmail.com>
2019-10-26 13:32:44 -0700

    Working outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/BlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/BlueLeft.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.Skystone.Auto;
 
 public class BlueLeft{
-    // transport two skystones and other stones if time permits
-
-    // park in building zone (even if other team is in corner)
+    // move foundation into building zone
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/RedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/RedRight.java
@@ -4,7 +4,5 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name="RedLeft", group ="Concept")
 public class RedRight extends AutoBase{
-    // transport two skystones and other stones if time permits
-
-    // park in building zone
+    // move foundation into building zone
 }

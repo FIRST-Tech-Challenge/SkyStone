3927428157bd1b3e55eee039dc1daa645d1ad67e
Randall-Scharpf <1017491@etusd.org>
2019-10-29 15:47:22 -0700

    added color ratios and averages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/TimedAutoForPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/TimedAutoForPark.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -44,7 +45,7 @@ public class TimedAutoForPark extends LinearOpMode {
         MecanumDrive train = new MecanumDriveImpl(leftFront, leftBack, rightFront, rightBack, null);
         driveTrain = new MecanumController(train);
 
-        Servo servoTest = hardwareMap.get(Servo.class, "armTune");
+        Servo Test = hardwareMap.get(Servo.class, "armTune");
 
 //        RevColorSensorV3 color = hardwareMap.get(RevColorSensorV3.class, "sensor_color_distance");
         RevColorSensorV3 color = new RevColorSensorV3(
@@ -62,54 +63,39 @@ public class TimedAutoForPark extends LinearOpMode {
         waitForStart();
         runtime.reset();
         int DST_CONST = 46;
-        servoTest.setPosition(0);
+        Test.setPosition(0);
         //driveTrain.translate(0.2, 0.75, MecanumDrive.TranslationMethod.CONSTANT_SPEED);
         sleep((long) 27*DST_CONST);
         //need to fix so that its around ~2 cm away from blocks
         while (opModeIsActive()) {
             double avg = 0;
-            double avgRed = 0;
-            double avgGreen = 0;
-            double avgBlue = 0;
-            double initialRtoB = 0;
-            double initialGtoB = 0;
-            double actualAvg = 0;
-            for (int i = 0; i < 10; i++) {
-                double initial = Math.pow((color.rawOptical() - cParam) / aParam, -binvParam);
+            double redToBlueAvg = 0;
+            double greenToBlueAvg = 0;
+            for (int i = 0; i < 10; i++){
+                double initial = Math.pow((color.rawOptical()-cParam)/aParam, -binvParam);
                 avg = avg + initial;
-                avgBlue += color.blue();
-                avgGreen += color.green();
-                avgRed += color.red();
-                //2.05 RtoB
-                //2.94 GtoB with yellow block
+                redToBlueAvg += (color.red()/color.blue());
+                greenToBlueAvg += (color.green()/color.blue());
             }
-            actualAvg = avg / 10;
-            avgBlue /= 10;
-            avgGreen /= 10;
-            avgRed /= 10;
+            double actualAvg = avg/10;
             if (runtime.seconds() < 2) {
-                initialRtoB = (avgRed)/(avgBlue);
-                initialGtoB = (avgGreen)/(avgBlue);
+                redToBlueAvg /= 10;
+                greenToBlueAvg /=10;
             }
-
-            telemetry.addData("R", avgRed);
-            telemetry.addData("G", avgGreen);
-            telemetry.addData("B", avgBlue);
-            telemetry.addData("RtoB", avgRed / avgBlue);
-            telemetry.addData("GtoB", avgGreen / avgBlue);
+            telemetry.addData("R", color.red());
+            telemetry.addData("G", color.green());
+            telemetry.addData("B", color.blue());
             telemetry.addData("InputData", actualAvg);
-            //telemetry.addData("Distance", color.getDistance(DistanceUnit.CM));
             telemetry.update();
             sleep(500);
-            //Reads color values a// nd sends them to driver station//red to blue => 1.7 - 2.5
-            //green to blue => 3 - 4
-
-            if ((avgRed / avgBlue) > initialRtoB && (avgGreen / avgBlue) > initialGtoB && actualAvg < maxDist) {
-//                servoTest.setPosition(1);
-//                driveTrain.translate(0, 0, MecanumDrive.TranslationMethod.CONSTANT_SPEED);
-                telemetry.addData("This detects yellow block", 5);
-//                telemetry.update();
-//                sleep(2000);
+            //Reads color values and sends them to driver station
+            if (actualAvg < maxDist && (color.red() / color.blue()) > redToBlueAvg && (color.green()/color.blue()) > greenToBlueAvg) {
+                driveTrain.translate(0, 0, MecanumDrive.TranslationMethod.CONSTANT_SPEED);
+                telemetry.update();
+//                telemetry.addData("R", color.red());
+//                telemetry.addData("G", color.green());
+//                telemetry.addData("B", color.blue());
+                sleep(2000);
             }
 //            else {
 //                telemetry.addData("Detects it's too close", "true");

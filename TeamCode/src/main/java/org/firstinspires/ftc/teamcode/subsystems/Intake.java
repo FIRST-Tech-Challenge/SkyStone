6bd57607554e3d40b7caf9e8c94ab165bbbfdb3f
23caravi <23caravi@elmbookstudets.org>
2019-10-29 19:58:27 -0500

    added high and low positions for wrist and main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,28 +1,63 @@
 package org.firstinspires.ftc.teamcode.subsystems;
-
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
-
-
 
 
 public class Intake extends Subsystem {
     Servo wrist;
     Servo main;
+    double wristHighPosition;
+    double wristLowPosition;
+    double mainHighPosition;
+    double mainLowPosition;
+
+
 
 
     public Intake(HardwareMap hardwareMap) {
         wrist = hardwareMap.servo.get("wrist");
         main = hardwareMap.servo.get("main");
         initServos(new Servo[]{wrist, main});
-        initServo();
+        initIntake();
+    }
+
+    public void initIntake() {
 
     }
 
+// all below are not finished yet, will do saturday, Ian.
 
-    public void initServo() {
+    public void setwrist(double position) {
+        wrist.setPosition(position);
+    }
 
+    public void setmain(double position) {
+        main.setPosition(position);
+    }
+
+// values need to be tested
+
+    public void setWristToHighPosition() {
+        wrist.setPosition(wristHighPosition);
+    }
+
+// values need to be tested
+
+    public void setWristToLowPosition() {
+        wrist.setPosition(wristLowPosition);
+    }
+// values need to be tested
+    public void setMainToHighPosition() {
+        main.setPosition(mainHighPosition);
+    }
+
+// values need to be tested
+
+    public void setMainToLowPosition() {
+        main.setPosition(mainLowPosition);
     }
 
 }
+
+
+    

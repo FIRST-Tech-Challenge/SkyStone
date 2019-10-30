69cb9f181e990f4e27ffc672a16076dee0de93f8
Oliver Yip <oliver.yip@fundingreach.com>
2019-10-30 18:36:11 -0500

    Added gyro assistance to avoid turning in mecanum drive. Added support for touch sensors for arm.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Drive.java
@@ -1,39 +1,74 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp
 public class Mecanum_Drive extends LinearOpMode {
     private DcMotor frontLeft, frontRight, backLeft, backRight;
     private DcMotor upperArm, lowerArm;
     private Servo leftClaw, rightClaw, grabber;
-    float fwd, side, turn, power;
-    double clawOffset = 0.0;
-    double grabberOffset = 0.5;
-    boolean dpad_up, dpad_down, btn_y, btn_a, btn_x, btn_b, left_bumper, right_bumper;
+    private DigitalChannel upper_sensor, lower_sensor;
+    private float fwd, side, turn, power, lower_arm_stick, grabber_stick;
+    private double clawOffset = 0.0;
+    private double grabberOffset = 0.5;
+    private boolean dpad_up, dpad_down, btn_y, btn_a, btn_x, btn_b, left_bumper, right_bumper;
+
+    // Gyro related initialization
+    private BNO055IMU imu;
+    private Orientation lastAngles = new Orientation();
+    private double globalAngle, correction;
 
     public final static double deadzone = 0.2;          // Deadzone for bot movement
     public final static double claw_speed = 0.01;       // Claw movement rate
     public final static double grabber_speed = 0.003;    // Grabber rotation rate
-    public final static double arm_up_power = 0.7;    // Grabber rotation rate
-    public final static double arm_down_power = -0.7;    // Grabber rotation rate
-    public final static int arm_up_step = 10;           // Arm up movement position step
-    public final static int arm_down_step = 10;           // Arm up movement position step
+    public final static double arm_up_power = 1.0;    // Grabber rotation rate
+    public final static double arm_down_power = -1.0;    // Grabber rotation rate
+    public final static int arm_up_step = 30;           // Arm up movement position step
+    public final static int arm_down_step = 30;           // Arm up movement position step
 
     @Override
     public void runOpMode() {
-        // Mecanum wheels
-//        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
-//        frontRight = hardwareMap.get(DcMotor.class, "front_right");
-//        backLeft = hardwareMap.get(DcMotor.class, "back_left");
-//        backRight = hardwareMap.get(DcMotor.class, "back_right");
+        // Gyro initialization and calibration
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
 
-//        frontRight.setDirection(DcMotor.Direction.REVERSE);
-//        backRight.setDirection(DcMotor.Direction.REVERSE);
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!imu.isGyroCalibrated()) {
+            sleep(50);
+            idle();
+        }
+
+        // Reset angle
+        resetAngle();
+
+        // Mecanum wheels
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        backRight = hardwareMap.get(DcMotor.class, "back_right");
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
 
         // Arm motors
 //        upperArm = hardwareMap.get(DcMotor.class, "upper_arm");
@@ -51,6 +86,12 @@ public class Mecanum_Drive extends LinearOpMode {
         rightClaw = hardwareMap.get(Servo.class, "right_claw");
         grabber = hardwareMap.get(Servo.class, "grabber");
 
+        // Touch sensors
+        upper_sensor = hardwareMap.get(DigitalChannel.class, "arm_upper_sensor");
+        upper_sensor.setMode(DigitalChannel.Mode.INPUT);
+        lower_sensor = hardwareMap.get(DigitalChannel.class, "arm_lower_sensor");
+        lower_sensor.setMode(DigitalChannel.Mode.INPUT);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -64,11 +105,25 @@ public class Mecanum_Drive extends LinearOpMode {
             // Bot movement with Mecanum wheels
             power = fwd; //this can be tweaked for exponential power increase
 
-/*            frontLeft.setPower(Range.clip(power + side - turn, -1, 1));
-            frontRight.setPower(Range.clip(power - side + turn, -1, 1));
-            backLeft.setPower(Range.clip(power - side - turn, -1, 1));
-            backRight.setPower(Range.clip(power + side + turn, -1, 1));
-*/
+            if ( turn != 0 ) {
+                // We are turing
+                frontLeft.setPower(Range.clip(power + side - turn, -1, 1));
+                frontRight.setPower(Range.clip(power - side + turn, -1, 1));
+                backLeft.setPower(Range.clip(power - side - turn, -1, 1));
+                backRight.setPower(Range.clip(power + side + turn, -1, 1));
+
+                resetAngle();
+            } else {
+                // Moving in straight line (forward, backward or sideway) without turning; Add gyro assistance
+                correction = checkDirection();
+
+                frontLeft.setPower(Range.clip(power + side - correction, -1, 1));
+                frontRight.setPower(Range.clip(power - side + correction, -1, 1));
+                backLeft.setPower(Range.clip(power - side - correction, -1, 1));
+                backRight.setPower(Range.clip(power + side + correction, -1, 1));
+
+            }
+
 
             // Arm movements
 /*            if (dpad_up) {
@@ -82,11 +137,11 @@ public class Mecanum_Drive extends LinearOpMode {
             } else
                 upperArm.setPower(0.0);
 */
-            if (btn_y) {
+            if (lower_arm_stick > 0 && upper_sensor.getState()) {
                 lowerArm.setTargetPosition(lowerArm.getCurrentPosition() + arm_up_step);
                 lowerArm.setPower(arm_up_power);
                 lowerArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            } else if (btn_a) {
+            } else if (lower_arm_stick < 0 && lower_sensor.getState()) {
                 lowerArm.setTargetPosition(lowerArm.getCurrentPosition() - arm_down_step);
                 lowerArm.setPower(arm_down_power);
                 lowerArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -107,9 +162,9 @@ public class Mecanum_Drive extends LinearOpMode {
 
 
             // Use gamepad X & right B buttons to rotate the grabber
-            if (btn_x)
+            if (grabber_stick < 0)
                 grabberOffset += grabber_speed;
-            else if (btn_b)
+            else if (grabber_stick > 0)
                 grabberOffset -= grabber_speed;
 
             // Move grabber servo to the desired position.
@@ -134,6 +189,9 @@ public class Mecanum_Drive extends LinearOpMode {
         side = gamepad1.left_stick_x;
         turn = gamepad1.right_stick_x;
 
+        lower_arm_stick = gamepad2.left_stick_y;
+        grabber_stick = gamepad2.right_stick_y;
+/*
         dpad_up = gamepad1.dpad_up;
         dpad_down = gamepad1.dpad_down;
         btn_y = gamepad1.y;
@@ -141,13 +199,77 @@ public class Mecanum_Drive extends LinearOpMode {
 
         btn_x = gamepad1.x;
         btn_b = gamepad1.b;
-        left_bumper = gamepad1.left_bumper;
-        right_bumper = gamepad1.right_bumper;
+*/
+
+        left_bumper = gamepad2.left_bumper;
+        right_bumper = gamepad2.right_bumper;
 
         //updates joystick values
         if( Math.abs(fwd) < deadzone ) fwd = 0;
         if( Math.abs(side) < deadzone ) side = 0;
         if( Math.abs(turn) < deadzone ) turn = 0;
+        if( Math.abs(lower_arm_stick) < deadzone ) lower_arm_stick = 0;
+        if( Math.abs(grabber_stick) < deadzone ) grabber_stick = 0;
         //checks deadzones
+    }
+
+    /**
+     * Resets the cumulative angle tracking to zero.
+     */
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+
+    /**
+     * Get current cumulative angle rotation from last reset.
+     * @return Angle in degrees. + = left, - = right.
+     */
+    private double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    /**
+     * See if we are moving in a straight line and if not return a power correction value.
+     * @return Power adjustment, + is adjust left - is adjust right.
+     */
+    private double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = 0.30;
+
+        angle = getAngle();
+
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+
+        correction = correction * gain;
+
+        return correction;
     }
 }

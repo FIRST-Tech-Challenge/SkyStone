6588329edd496c2b49c2fa44c9f722a6dc54e905
Bill Zheng <31973301+BILLLLLLL@users.noreply.github.com>
2019-10-30 15:09:43 -0700

    Create Auto_Test.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Test.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import static java.lang.Math.sqrt;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+
+public class Auto_Test extends MecanumAutoCentral {
+
+    private final double power = 0.6;
+
+    public void runOpMode(){
+        while (opModeIsActive()){
+
+        }
+    }
+}

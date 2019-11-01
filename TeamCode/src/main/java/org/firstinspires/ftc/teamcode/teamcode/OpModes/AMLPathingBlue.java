9f2d2c20cb081038c9fcbe4917cc1aa46b9889cf
shriya3ganesan <43021031+shriya3ganesan@users.noreply.github.com>
2019-11-01 16:42:29 -0500

    Auto Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/OpModes/AMLPathingBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/OpModes/AMLPathingBlue.java
@@ -80,25 +80,12 @@ public class AMLPathingBlue extends LinearOpMode {
 
             case 3:
                 drive.encoderDrive(this, .5, 24, 24, 2);
-                /*outtake.rightVex.setPower(.5);
-                outtake.leftVex.setPower(-.5);
-                sleep(5000);
-                outtake.rightVex.setPower(0);
-                outtake.leftVex.setPower(0);
-                outtake.liftLeft.setPower(.5);
-                outtake.liftRight.setPower(.5);
-                sleep(1000);
-                outtake.liftLeft.setPower(0);
-                outtake.liftRight.setPower(0);*/
+
                 drive.gyroTurn(this, 180, true, 2000);
                 drive.snowWhite();
                 drive.encoderDrive(this, -.5, -36, -36, 2);
-                /*outtake.liftLeft.setPower(-.5);
-                outtake.liftRight.setPower(-.5);
-                sleep(1000);
-                outtake.liftLeft.setPower(0);
-                outtake.liftRight.setPower(0);*/
-                drive.encoderDrive(this, -.5, -15, -15, 2);
+                outtake.Shitty_Auto_Outtake(this);
+                drive.encoderDrive(this, .5, 15, 15, 2);
                 drive.gyroTurn(this, 90, false, 1000);
                 drive.encoderDrive(this, -.5, -72, -72, 2);
                 drive.strafeMove(this, 24, 2, .5);

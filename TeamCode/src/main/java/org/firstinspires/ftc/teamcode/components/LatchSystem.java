2d1f3ca6bc847f8e302320a6db13b8e2d3163182
LexiShew <lexishew@live.com>
2019-11-02 16:55:42 -0700

    Fixes DriveTeleop for testing and finalizes latch system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/LatchSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/LatchSystem.java
@@ -1,48 +1,37 @@
 package org.firstinspires.ftc.teamcode.components;
 
-import android.util.Log;
-
 import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class LatchSystem {
     private Servo servo;
-    private final double DOWN_POSITION = 0.3;
-    private final double UP_POSITION = 0.7;
-
-    public final String TAG = "LatchSystem";
+    private final double DOWN_POSITION = 0.9;
+    private final double UP_POSITION = 0.05;
 
     public LatchSystem(Servo servo) {
         this.servo = servo;
-        init();
+        initServo();
     }
 
-    public void init() {
-        Log.d(TAG, "in init");
+    private void initServo() {
         servo.setPosition(UP_POSITION);
+        servo.close();
     }
 
     public void run(boolean up, boolean down) {
-
         if (up) {
-            Log.d(TAG, "in run -- up");
-            latch();
-        } else if (down) {
-            Log.d(TAG, "in run -- down");
             unlatch();
+        } else if (down) {
+            latch();
         }
-
-        Log.d(TAG, "servo pos: " + servo.getPosition());
     }
 
     public void latch() {
-        Log.d(TAG,"in latch");
         servo.setPosition(DOWN_POSITION);
         servo.close();
     }
 
     public void unlatch() {
-        Log.d(TAG, "unlatch");
         servo.setPosition(UP_POSITION);
         servo.close();
     }

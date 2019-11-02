a8d8e840e7aa5bd88a1971c5ca530ea2c6f6d1e9
Adrian <Adrian_Avram@Outlook.com>
2019-11-01 17:04:29 -0700

    Autonomous updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/base/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/base/BaseOpMode.java
@@ -26,6 +26,7 @@ public abstract class BaseOpMode extends OpMode {
     private boolean stopRequested;
 
     public void init(){
+        stopRequested = false;
         this.msStuckDetectInit = 20000;
         this.msStuckDetectInitLoop = 20000;
         EnumMap<DriveSystem.MotorNames, DcMotor> driveMap = new EnumMap<>(DriveSystem.MotorNames.class);

d38f855fe1cbc4558de87d61820f38ab1748ea7d
LexiShew <lexishew@live.com>
2019-11-02 14:47:23 -0700

    Beautifies code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DriveTeleop.java
@@ -2,30 +2,28 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.components.DriveSystem.Direction;
-import org.firstinspires.ftc.teamcode.components.SpinnySystem;
+import org.firstinspires.ftc.teamcode.components.DriveSystem;
 import org.firstinspires.ftc.teamcode.opmodes.base.BaseOpMode;
 
 @TeleOp(name = "Drive", group="TeleOp")
 public class DriveTeleop extends BaseOpMode {
 
     public void loop(){
-        float rx = gamepad1.right_stick_x;
-        float lx = gamepad1.left_stick_x;
-        float ly = gamepad1.left_stick_y;
 
-            if (gamepad1.a) {
-                driveSystem.driveToPosition(1000, Direction.RIGHT, 0.5);
-            }
-            if (gamepad1.b) {
-                driveSystem.driveToPosition(1000, Direction.LEFT, 0.5);
-            }
-            if (gamepad1.x) {
-                driveSystem.driveToPosition(1000, Direction.FORWARD, 0.5);
-            }
-            if (gamepad1.y) {
-                driveSystem.driveToPosition(1000, Direction.BACKWARD, 0.5);
-            }
-            driveSystem.drive(rx, lx, ly);
+        if (gamepad1.a) {
+            driveSystem.driveToPosition(1000, DriveSystem.Direction.RIGHT, 0.5);
+        }
+        if (gamepad1.b) {
+            driveSystem.driveToPosition(1000, DriveSystem.Direction.LEFT, 0.5);
+        }
+        if (gamepad1.x) {
+            driveSystem.driveToPosition(1000, DriveSystem.Direction.FORWARD, 0.5);
+        }
+        if (gamepad1.y) {
+            driveSystem.driveToPosition(1000, DriveSystem.Direction.BACKWARD, 0.5);
+        }
+        driveSystem.drive(gamepad1.right_stick_x, gamepad1.left_stick_x, gamepad1.left_stick_y);
+
+        spinnySystem.spin(gamepad1.left_bumper, gamepad1.right_bumper);
     }
 }

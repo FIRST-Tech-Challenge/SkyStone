171f5e4dc9b9a550a6ddca26e9f2081a9098bf94
Albert Cai <albertcai101@alberts-mbp.lan>
2019-11-03 15:14:59 -0800

    again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/MainTeleop.java
@@ -25,8 +25,8 @@ public class MainTeleop extends LinearOpMode {
     boolean onSlowDrive, changedSlowDrive = false;
     boolean isRetract = false;
     boolean isExtend = false;
+    boolean isClamp = false;
     public static double powerScaleFactor = 0.9;
-
     @Override
     public void runOpMode() {
         resetRobot();
@@ -43,19 +43,19 @@ public class MainTeleop extends LinearOpMode {
             outtakeLogic();
         }
     }
-
-    private void spoolLogic() {
-        if (gamepad2.dpad_up && robot.getOuttakeSpool().getCurrentPosition() <= 6600) {
+    private void spoolLogic(){
+        if (gamepad2.dpad_up && robot.getOuttakeSpool().getCurrentPosition()<=6600) {
+            robot.getClamp().setPosition(robot.CLAW_SERVO_CLAMPED);
             robot.getOuttakeSpool().setPower(1);
-        } else if (gamepad2.dpad_down && robot.getOuttakeSpool().getCurrentPosition() >= 0) {
+        }else if(gamepad2.dpad_down && robot.getOuttakeSpool().getCurrentPosition()>=0 ){
+            robot.getClamp().setPosition(robot.CLAW_SERVO_CLAMPED);
             robot.getOuttakeSpool().setPower(-1);
-        } else {
+        }else{
             robot.getOuttakeSpool().setPower(0);
         }
         telemetry.addLine("Spool Position " + robot.getOuttakeSpool().getCurrentPosition());
         telemetry.update();
     }
-
     private void resetRobot() {
         robot = new Robot(hardwareMap, telemetry, this);
         robot.setMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -68,14 +68,12 @@ public class MainTeleop extends LinearOpMode {
 //        robot.outtakeArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        robot.outtakeArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-
     private void initServos() {
         robot.getClampPivot().setPosition(robot.OUTTAKE_PIVOT_RETRACTED);
         robot.getIntakePusher().setPosition(robot.PUSHER_RETRACTED);
         robot.getOuttakeExtender().setPosition(robot.OUTTAKE_SLIDE_RETRACTED);
         robot.getClamp().setPosition(robot.CLAW_SERVO_RELEASED);
     }
-
     //teleop methods
     private void driveLogic() {
         // TODO: change all of this stuff to x, y, and turn movements
@@ -119,7 +117,6 @@ public class MainTeleop extends LinearOpMode {
         }
         robot.allWheelDrive(fLPower, fRPower, bLPower, bRPower);
     }
-
     private void slowDriveLogic() {
         //toggle driving speed
         if (powerScaleFactor == 0.3) {
@@ -135,17 +132,16 @@ public class MainTeleop extends LinearOpMode {
             changedSlowDrive = false;
         }
     }
-
     private void intakeLogic() {
         robot.getIntakeLeft().setPower(gamepad2.left_stick_y);
         robot.getIntakeRight().setPower(gamepad2.right_stick_y);
         robot.getIntakeLeft().setPower(gamepad2.left_stick_y);
         robot.getIntakeRight().setPower(gamepad2.right_stick_y);
-        if (gamepad2.left_stick_y > 0 && gamepad2.right_stick_y > 0) {
+        if (gamepad2.left_stick_y != 0 || gamepad2.right_stick_y != 0) {
             robot.getIntakePusher().setPosition(robot.PUSHER_RETRACTED);
+            robot.getClamp().setPosition(0.3);
         }
     }
-
     private void outtakeLogic() {
         currentTime = SystemClock.elapsedRealtime();
         // Logic to control outtake; with a delay on the pivot so that the slides can extend before pivot rotation
@@ -153,46 +149,46 @@ public class MainTeleop extends LinearOpMode {
             robot.getIntakePusher().setPosition(robot.PUSHER_PUSHED); // Push block all the way to clamp
             isExtend = true;
             isRetract = false;
+            isClamp = false;
             outtakeExecutionTime = currentTime;
         } else if (gamepad2.b) { // Deposit and Reset
             robot.getIntakePusher().setPosition(robot.PUSHER_RETRACTED); // Reset intake pusher
             isRetract = true;
             isExtend = false;
+            isClamp = false;
             outtakeExecutionTime = currentTime;
             robot.getClamp().setPosition(robot.CLAW_SERVO_RELEASED); // Release clamp
+        }else if(gamepad2.x){
+            isClamp = true;
+            isExtend = false;
+            isRetract = false;
+            outtakeExecutionTime = currentTime;
+            robot.getIntakePusher().setPosition(robot.PUSHER_PUSHED);
         }
         //extend
-        if (currentTime - outtakeExecutionTime >= 200 && isExtend) {
+        if (currentTime - outtakeExecutionTime >= 300 && isExtend) {
             robot.getClamp().setPosition(robot.CLAW_SERVO_CLAMPED);
         }
-        if (currentTime - outtakeExecutionTime >= 300 && isExtend) {
+        if(currentTime-outtakeExecutionTime >= 400 && isExtend){
             robot.getOuttakeExtender().setPosition(robot.OUTTAKE_SLIDE_EXTENDED);
         }
-        if (currentTime - outtakeExecutionTime >= 1700 && isExtend) {
+        if(currentTime-outtakeExecutionTime >= 1400 && isExtend){
             robot.getClampPivot().setPosition(robot.OUTTAKE_PIVOT_EXTENDED);
             isExtend = false;
         }
         //retract
-        if (currentTime - outtakeExecutionTime >= 250 && isRetract) {
+        if(currentTime-outtakeExecutionTime >= 250 && isRetract){
             robot.getClampPivot().setPosition(robot.OUTTAKE_PIVOT_RETRACTED);
         }
-        if (currentTime - outtakeExecutionTime >= 750 && isRetract) {
+        if(currentTime-outtakeExecutionTime >= 750 && isRetract){
             robot.getOuttakeExtender().setPosition(robot.OUTTAKE_SLIDE_RETRACTED);
-        }
-        if (currentTime - outtakeExecutionTime >= 1000 && isRetract) {
             isRetract = false;
-            robot.getOuttakeSpool().setPower(-1);
-            robot.getOuttakeSpool().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.getOuttakeSpool().setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.getOuttakeSpool().setTargetPosition(0);
-            while (robot.getOuttakeSpool().getCurrentPosition() >= 50) {
-                slowDriveLogic();
-                driveLogic();
-                intakeLogic();
-            }
-            robot.getOuttakeSpool().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.getOuttakeSpool().setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            robot.getOuttakeSpool().setPower(0);
+        }
+        //clamp only
+        if(currentTime-outtakeExecutionTime >= 300 && isClamp){
+            robot.getClamp().setPosition(robot.CLAW_SERVO_CLAMPED);
+            isClamp = false;
+            robot.getIntakePusher().setPosition(robot.PUSHER_RETRACTED);
         }
     }
 }

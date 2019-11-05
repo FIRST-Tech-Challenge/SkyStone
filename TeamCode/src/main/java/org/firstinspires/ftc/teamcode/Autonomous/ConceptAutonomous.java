8fbf9e64ba348daa57054810d97716d74326456b
Simeon Keske <ca-git@noemis.me>
2019-11-05 16:11:52 +0100

    add pseudocode for the autonomous phase

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ConceptAutonomous.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous (name = "ConceptAutonomous")
+public class ConceptAutonomous extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        // Assuming we are Team Blue
+
+        // Drive right, until we see a skystone -> vuforia
+
+        // Center Skystone in Image
+
+        // Drive forward
+
+        // Grab skystone
+
+        // turn 90°
+        
+        // drive left, until touchsensors are pressed
+        
+        // forward till colorsensor1 sees $teamColor line
+        
+        // forward till colorsensor1 sees $teamColor line
+        
+        // drive right, till color2 sees $teamColor
+        
+        // drive back, till color2 not sees $teamColor
+
+        // raise arm, encoder based.
+
+        // drive right, till color1 sees $teamColor 
+
+        // open clamp, encoder based
+
+        // hook platform
+
+        // drive left, till touchsensor is pressed
+
+        // unhook the plate, my friend
+
+        // drive back, till color2 sees $teamColor
+    }
+}

b79330de2776c5d4a33c402e5442ca7f8f91e117
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2019-11-04 18:23:59 -0600

    Added in loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/TeleOpMode.java
@@ -1,14 +1,10 @@
 package org.firstinspires.ftc.teamcode.testClasses;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.subsystems.Arm;
-import org.firstinspires.ftc.teamcode.subsystems.Chassis;
 import org.firstinspires.ftc.teamcode.subsystems.Controller;
-import org.firstinspires.ftc.teamcode.subsystems.Hook;
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
 
 
@@ -21,25 +17,11 @@ public class TeleOpMode extends LinearOpMode {
         DcMotor armMotor = hardwareMap.dcMotor.get("arm");
         telemetry.addData("Init", "v:1.0");
         waitForStart();
+        while (opModeIsActive()) {
             Controller controller = new Controller(gamepad1);
             robot.run(controller);
-            /*
-            HashMap<String, Integer> chassisMotorPositions = chassis.getMotorPositions();
-            for(HashMap.Entry<String, Integer> chassisMotorPosition : chassisMotorPositions.entrySet()){
-               telemetry.addData(chassisMotorPosition.getKey(), chassisMotorPosition.getValue());
-            }
-            */
             telemetry.addData("arm motor: ", armMotor.getCurrentPosition());
             telemetry.update();
         }
-
-
-    public void runHook(Hook hook, Controller controller) {
-
-    }
-
-    public void runChassis(Chassis chassis, Controller controller) {
-
-        //chassis.runChassis(angle, turn, power);
     }
 }  

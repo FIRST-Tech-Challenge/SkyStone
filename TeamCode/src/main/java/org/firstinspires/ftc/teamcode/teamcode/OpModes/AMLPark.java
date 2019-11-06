bde25ef1826bede6fd3e9e0c0c717acb08178dd6
nirmiller <52181530+nirmiller@users.noreply.github.com>
2019-11-06 12:03:33 -0600

    IDK How to do auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/OpModes/AMLPark.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.teamcode.Hardware.DriveTrain;
+
+@Autonomous(name="Park", group="Auto Basic")
+public class AMLPark extends LinearOpMode {
+
+    DriveTrain driveTrain = new DriveTrain();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        driveTrain.initDriveTrain(this);
+
+        waitForStart();
+
+
+        driveTrain.encoderDrive(this, .5, 10, 10, 5);
+    }
+}

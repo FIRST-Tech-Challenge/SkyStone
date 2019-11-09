6159d30c3915e06ed4cf79b85047bba96ad90c5f
23townsk <[23townsk@elmbrookstudents.org]>
2019-11-09 14:01:15 -0600

    Added two packages and two classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotGame/SkystoneAuto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.RobotGame;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class SkystoneAuto extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

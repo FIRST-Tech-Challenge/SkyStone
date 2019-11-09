1c275e2910e46ebe64d3559fe5e32f181fb34fc8
Ethan <e3lcool@gmail.com>
2019-11-09 13:45:44 -0800

    Commented and refined BlueLeft and RedRight, allowing goToSkystone to take robotPosition and moving it to AutoBase. Also refactored extendOuttake in Autobase to depositStone. Also tweaked teleOp (initServos now has timers)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/RedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Auto/RedRight.java
@@ -4,7 +4,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name="RedLeft", group ="Concept")
 public class RedRight extends AutoBase{
-    // transport two skystones and other stones if time permits
 
-    // park in building zone (even if other team is in corner)
+    @Override
+    public void runOpMode() {
+        initLogic();
+
+        // Detect position of Skystone
+        int vuforiaPosition = robot.detectTensorflow();
+
+        // Go to Skystone and intake it
+        goToSkystone(vuforiaPosition,0);
+
+        // Move to other side of the Skybridge
+        robot.moveToPoint(7, -47, 1,  1, Math.toRadians(90));
+
+        // Deposit the Skystone and retract the outtake arm
+        depositStone(robot);
+        retractOuttake(robot);
+
+        // Move back to the second set of Skystones to pick up the second Skystone
+        robot.moveToPoint(0,24,1,1,Math.toRadians(0));
+
+        // Go to the Skystone and intake it
+        goToSkystone(vuforiaPosition,-1); // Negative one because second set of skystones is to the left of the robot
+
+        // Move to other side of the Skybridge
+        robot.moveToPoint(0, -47, 1,  1, Math.toRadians(90));
+
+        // Deposit the Skystone and retract the outtake arm
+        depositStone(robot);
+        retractOuttake(robot);
+    }
 }

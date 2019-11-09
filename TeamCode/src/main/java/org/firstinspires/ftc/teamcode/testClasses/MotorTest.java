49b7d4727272030555ce8f9b604827c0b3f4ee91
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2019-11-09 12:57:23 -0600

    motor test for arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testClasses/MotorTest.java
@@ -10,11 +10,24 @@ public class MotorTest extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-
-        telemetry.setMsTransmissionInterval(1);
         telemetry.addLine("Init | v1.0");
+        DcMotor arm = hardwareMap.dcMotor.get("arm");
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
         while (opModeIsActive()) {
+            telemetry.addData("arm pos: ", arm.getCurrentPosition());
+            if(gamepad1.a){
+                arm.setTargetPosition(arm.getTargetPosition()+1);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(gamepad1.a);
+            }
+            if(gamepad1.b){
+                arm.setTargetPosition(arm.getTargetPosition()-1);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while(gamepad1.b);
+            }
+            telemetry.addData("arm set pos: ", arm.getTargetPosition());
+            telemetry.addData("arm cur pos: ", arm.getCurrentPosition());
             //Other unit test code if you want
             telemetry.update();
         }

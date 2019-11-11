b3902cfde4f9a9bb8bc0e6ccf37178ea8f7c86b4
gronkybong <48972370+gronkybong@users.noreply.github.com>
2019-11-11 15:33:48 -0800

    Update ManualDriveOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveOpMode.java
@@ -15,29 +15,24 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  * reset the forward heading by pressing "x".
  */
 @TeleOp(name = "Mecanum")
-public class ManualDriveOpMode extends OpMode {
-    private FourWheelsDriveBot robot;
+public class ManualDriveOpMode extends LinearOpMode {
+    private ScoopArmBot robot;
     private boolean arcadeMode = false;
     private int gyroCalibratedCount = 0;
 
-    @Override
-    public void init() {
-    }
+
 
     @Override
-    public void init_loop() {
-
-      ScoopArmBot robot = new ScoopArmBot(this);
-    }
-
-    @Override
-    public void loop() {
-        if (gamepad1.a) {
-            robot.resetHeading();
-        }
+    public void runOpMode() {
+        robot = new ScoopArmBot(this);
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                robot.resetHeading();
+            }
             robot.driveByHand(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, arcadeMode);
             robot.scoopSetPosition(gamepad1.b, gamepad1.y, gamepad1.x);
             robot.scoopFreeRun(gamepad1.right_bumper, gamepad1.left_bumper);
         }
     }
+}
 

5710024a25f53b9174ff1e73d45ed6ab01712ad6
evanyans <48900125+evanyans@users.noreply.github.com>
2019-11-11 15:15:45 -0800

    driveByHand
    manual driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveOpMode.java
@@ -1,27 +1,40 @@
-
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-@TeleOp(name="Manual Driving OpMode", group="Linear Opmode")
+/**
+ * Mecanum teleop (with an optional arcade mode)
+ * * Left stick controls x/y translation.
+ * * Right stick controls rotation about the z axis
+ * * When arcade mode is enabled (press "a"), translation direction
+ * becomes relative to the field as opposed to the robot. You can
+ * reset the forward heading by pressing "x".
+ */
+@TeleOp(name = "Mecanum")
+public class ManualDriveOpMode extends OpMode {
+    private FourWheelsDriveBot robot;
+    private boolean arcadeMode = false;
 
-public class ManualDriveOpMode extends LinearOpMode {
-
-    FourWheelsDriveBot robot = new FourWheelsDriveBot(this);
 
     @Override
-    public void runOpMode() {
-        robot.init(hardwareMap);
+    public void init() {
+    }
 
-        waitForStart();
+    @Override
+    public void init_loop() {
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+    }
 
-            robot.driveByVector(gamepad1.left_stick_x, gamepad1.left_stick_y);
+    @Override
+    public void loop() {
+        if (gamepad1.x) {
+            robot.resetHeading();
+        }
+            robot.driveByHand(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, arcadeMode);
         }
     }
-}
+

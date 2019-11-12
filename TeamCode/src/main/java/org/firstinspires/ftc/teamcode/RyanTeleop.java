21463cc7e18f02dae126bba35012d88e03165c26
Ryan <rswart0604@gmail.com>
2019-11-11 21:08:07 -0700

    Changed line 13 to have clamp_set take distance, not the y of the stick. Added notes into program, four slashes = copy/paste

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyanTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyanTeleop.java
@@ -9,29 +9,31 @@ public class RyanTeleop extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
 
-    //
+    //// Takes a distance and adds/subtracts with the previous position
     public void moveClamp(double distance) {
 
-        double clamp_set = (.004 * gamepad2.right_stick_y);
+        double clamp_set = (.004*distance);
         robot.clamp.setPosition(robot.clamp.getPosition() + clamp_set);
     }
-    // Ryan's logic
+    //// Ryan's logic
 
     @Override
     public void runOpMode() {
-        robot.init(hardwareMap, telemetry);
 
+        // Initialize, wait for start
+        robot.init(hardwareMap, telemetry);
         waitForStart();
 
+        //// Begins while loop, updates telemetry
         while (opModeIsActive()) {
             telemetry.addData("Status:", "Started");
             telemetry.update();
 
-            //
+            // inputs the stick's y value into moveClamp
             if (gamepad2.right_stick_y != 0) {
                 moveClamp(gamepad2.right_stick_y);
             }
-            // Ryan's code (under "while opModeIsActive")
+            //// Ryan's code (under "while opModeIsActive")
         }
     }
 }

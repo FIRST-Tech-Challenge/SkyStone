77ec1ab5e54a240ceb871e6a1ea94f42f5d2cd18
UMA-CW-FTC-L-05 <UMA-CW-FTC-L-05@ftc.local>
2019-11-12 16:30:03 -0700

    modified:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOneHardware.java files added/modified by Triston Wilson on  Tue Nov 12 16:30:03 MST 2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOneHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOneHardware.java
@@ -90,8 +90,8 @@ public class RobotOneHardware
         leftDrive.setPower(0);
         rightDrive.setPower(0);
         InAndOut.setPower(0);
-        leftServo.setPosition(1.0);
-        rightServo.setPosition(0.0);
+        leftServo.setPosition(0.25);
+        rightServo.setPosition(0.75);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.

3a8d5a996a727175ccd447cea9cd4d7b1e761417
Gerrit <gjkaup@gmail.com>
2019-11-14 16:03:25 -0700

    Finished the arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GerritTelop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GerritTelop.java
@@ -7,101 +7,62 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class GerritTelop extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
-    int position;  // TODO This should be private and make this explicit it means nothing to me. robot_current_position?
-    float now;     // TODO This should be private and make this explicit.  robot_last_known_position?
-    boolean a;     // TODO I don't think you need this at all.
+    int position;
+    int wanted;
 
     public void leverArmStay() throws InterruptedException {
-        //
-        // Completely foreign to me?  Assuming not complete thought.
-        //
-        if (a = true) {
-            a = false;
-            sleep(10);
-            now = robot.leverArm.getCurrentPosition();
-        }
-        if (a = false){
-            a = true;
-
-        }
-    }
-    public void leverArmStay2() throws InterruptedException {
-        //
-        // This is absolutely the right approach.
-        //
         position = robot.leverArm.getCurrentPosition();
-        telemetry.addData("position%.2d", position);
-            if (position > now) {
+        if (position > wanted) {
                 robot.leverArm.setPower(-.4);
-            }
-            if (position < now) {  // TODO else if..
+        }
+        if (position < wanted) {
                 robot.leverArm.setPower(.4);
-            }  // TODO ELSE set power 0
-
+        }
     }
+
     public void moveLeverArm(double distance) throws InterruptedException {
         telemetry.addData("Value %.2d", distance);
-        position = robot.leverArm.getCurrentPosition() + 1;
+        position = robot.leverArm.getCurrentPosition();
         telemetry.addData("position%.2d", position);
         telemetry.update();
 
-        if (distance > .1) {
+        if (distance > .5) {
             if (position >= robot.ARM_UP_DISTANCE) {
                 robot.leverArm.setPower(0);
             }
-            if (position >= 900) { // TODO - Shouldn't this be a elseif?
-                robot.leverArm.setPower(-.27);
+            else if (position >= 900) {
+                robot.leverArm.setPower(-.02);
             }
-            if (position <= robot.ARM_UP_DISTANCE) {  // TODO - Shouldn't this be a elseif?
+            else if (position <= robot.ARM_UP_DISTANCE) {
                 robot.leverArm.setPower(.35);
             }
         }
-        if (distance < -.1) {
+        if (distance < -.5) {
             if (position <= 100) {
                 robot.leverArm.setPower(0);
             }
-            if (position <= 900) {  // TODO - Shouldn't this be a elseif?
-                robot.leverArm.setPower(.27);
+            else if (position <= 700) {
+                robot.leverArm.setPower(.02);
             }
-            if (position >= 100) {   // TODO - Shouldn't this be a elseif?
+            else if (position >= 100) {
                 robot.leverArm.setPower(-.35);
             }
         }
-        // We need to record our last position
+        wanted = robot.leverArm.getCurrentPosition();
     }
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap, telemetry);
-        a = true;
         waitForStart();
         int counter = 0;
 
-        // Couple thoughts.  What is it we want to do..
-        // When I let go of the stick I need it to freeze to the last known position.
-        // I don't want to do anything more than that.  I wouldn't worry about the button A.
-        // What I would suggest is that if left_stick_y == 0 and right_stick_y == 0 then stay!
-        // Now the leverArmStay is simply going to take the currentPosition.  And we need to compare
-        // that to our last recorded postion....
-        // So...
-        // You will need a top
-
         while (opModeIsActive()) {
 
-            // I would remove this
-            if (gamepad2.a) {
+            if (gamepad2.left_stick_y < .5 && gamepad2.left_stick_y > -.5) {
                 leverArmStay();
             }
-            if (a=false){
-                leverArmStay2();
-            }
-            // I would remove this
-
-            if (gamepad2.left_stick_y > .1 || gamepad2.left_stick_y < -.1) {
+            if (gamepad2.left_stick_y > .5 || gamepad2.left_stick_y < -.5) {
                 moveLeverArm(-gamepad2.left_stick_y);
-            }
-            else {
-                // This not goes to leverArmStay(getCurrentPosition) and remove setPower..
-                robot.leverArm.setPower(0);
             }
         }
     }

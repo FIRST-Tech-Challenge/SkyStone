7c1f54206ecfbfdc7d264f023b92d9a7cc173c06
Lena Ellermann <lena.ellermann@igs-goettingen.de>
2019-11-14 17:46:26 +0100

    hardware test for servo for building team is working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/hardware_tests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/hardware_tests.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareChassis;
+import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareChassisGyro;
 
 @TeleOp (name = "hardware_tests")
 public class hardware_tests extends OpMode {
@@ -13,15 +14,16 @@ public class hardware_tests extends OpMode {
     @Override
     public void init() {
         servo_port0 = hardwareMap.get(Servo.class, "servo_port0");
+        //TO DO: Neue Hardwaremap für Robot erstellen!!
     }
 
     @Override
     public void loop(){
         if (gamepad1.a) {
-            servo_port0.setPosition(0.7);
+            servo_port0.setPosition(0.6);
         }
         else if (gamepad1.b) {
-            servo_port0.setPosition(0.2);
+            servo_port0.setPosition(0.1);
         }
     }
 

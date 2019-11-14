ce278792b276792fc346d10cb33f7b32ae99b112
UMA-CW-FTC-L-05 <UMA-CW-FTC-L-05@ftc.local>
2019-11-14 16:41:06 -0700

    modified:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOneAll.java files added/modified by Triston Wilson on  Thu Nov 14 16:41:06 MST 2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOneAll.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOneAll.java
@@ -133,8 +133,8 @@ public class RobotOneAll extends LinearOpMode {
 
             if (gamepad1.left_bumper == true ) {
                 //left 1, right 0
-                robotOne.leftServo.setPosition(0.85);
-                robotOne.rightServo.setPosition(0.15);
+                robotOne.leftServo.setPosition(0.8);
+                robotOne.rightServo.setPosition(0.2);
             }
 
             // Pace this loop so jaw action is reasonable speed.

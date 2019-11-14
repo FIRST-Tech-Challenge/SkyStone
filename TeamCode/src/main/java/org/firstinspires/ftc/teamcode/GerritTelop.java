78ee6763341885519978a9eb317c5d4264c6cf15
Gerrit <gjkaup@gmail.com>
2019-11-13 20:27:59 -0700

    WORKING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GerritTelop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GerritTelop.java
@@ -10,13 +10,33 @@ public class GerritTelop extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
     int position;
-    float game;
+    float now;
+    boolean a;
+    public void leverArmStay() throws InterruptedException {
+        if (a = true) {
+            a = false;
+            sleep(10);
+            now = robot.leverArm.getCurrentPosition();
+        }
+        if (a = false){
+            a = true;
 
+        }
+    }
+    public void leverArmStay2() throws InterruptedException {
+            if (position > now) {
+                robot.leverArm.setPower(-.4);
+            }
+            if (position < now) {
+                robot.leverArm.setPower(.4);
+            }
+    }
     public void moveLeverArm(double distance) throws InterruptedException {
         telemetry.addData("Value %.2d", distance);
         position = robot.leverArm.getCurrentPosition() + 1;
         telemetry.addData("position%.2d", position);
         telemetry.update();
+
         if (distance > .1) {
             if (position >= robot.ARM_UP_DISTANCE) {
                 robot.leverArm.setPower(0);
@@ -43,11 +63,17 @@ public class GerritTelop extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap, telemetry);
+        a = true;
         waitForStart();
         int counter = 0;
         while (opModeIsActive()) {
-
-            if (gamepad2.left_stick_y != 0) {
+            if (gamepad2.a) {
+                leverArmStay();
+            }
+            if (a=false){
+                leverArmStay2();
+            }
+            if (gamepad2.left_stick_y > .1 || gamepad2.left_stick_y < -.1) {
                 moveLeverArm(-gamepad2.left_stick_y);
             }
             else {

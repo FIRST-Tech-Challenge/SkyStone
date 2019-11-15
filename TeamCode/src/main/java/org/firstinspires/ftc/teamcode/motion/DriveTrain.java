b2a12f25a5bbd66e547bae5c3f2193bff9dc267f
Gerrit <gjkaup@gmail.com>
2019-11-14 17:35:07 -0700

    Reworking!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motion/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motion/DriveTrain.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.motion;
 
+import org.firstinspires.ftc.teamcode.RobotHardware;
+
 public class DriveTrain {
+
+    public void drive_train(RobotHardware robot, double leftStick, double rightStick) {
+
+        if (leftStick > .4 || leftStick < -.4) {
+            robot.leftDrive.setPower(leftStick);
+        }
+        if (rightStick > .4 || rightStick < -.4) {
+            robot.rightDrive.setPower(rightStick);
+        }
+        if (rightStick < .4 && rightStick > -.4) {
+            robot.rightDrive.setPower(0);
+        }
+        if (leftStick < .4 && leftStick > -.4) {
+            robot.leftDrive.setPower(0);
+        }
+    }
 }

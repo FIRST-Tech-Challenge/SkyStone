3290cf47bf090772f23f7733e5b2f0d32b103796
JimmyLi2020 <lix@avonoldfarms.com>
2019-11-16 09:52:18 -0500

    updated structure tester to test both cr and normal servo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/StructureTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/StructureTester.java
@@ -2,30 +2,44 @@ package org.firstinspires.ftc.teamcode.teleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "StructureTester")
 public class StructureTester extends OpMode {
 
-    private Servo hand = null;
-    double handPos = 0.0d;
+    private CRServo crservo = null;
+    private Servo servo = null;
+    double servoPos = 0.0d;
 
     @Override
     public void init() {
-        hand = hardwareMap.servo.get("hand");
+        crservo = hardwareMap.crservo.get("crservo");
+        crservo.setDirection(DcMotorSimple.Direction.FORWARD);
+        servo = hardwareMap.servo.get("servo");
+        servo.setDirection(Servo.Direction.FORWARD);
     }
 
     @Override
     public void loop() {
-        telemetry.addData("handPos", handPos);
+        telemetry.addData("servoPos", servoPos);
         telemetry.update();
         if (gamepad1.dpad_down) {
-            handPos = Range.clip(handPos + 0.1, 0.0, 0.5);
-            hand.setPosition(handPos);
+            crservo.setPower(0.5);
         } else if (gamepad1.dpad_up) {
-            handPos = Range.clip(handPos - 0.1, 0.0, 0.5);
-            hand.setPosition(handPos);
+            crservo.setPower(-0.5);
+        } else {
+            crservo.setPower(0.0);
+        }
+
+        if (gamepad1.y) {
+            servoPos = Range.clip(servoPos + 0.1, 0.0, 0.5);
+            servo.setPosition(servoPos);
+        } else if (gamepad1.a) {
+            servoPos = Range.clip(servoPos - 0.1, 0.0, 0.5);
+            servo.setPosition(servoPos);
         }
     }
 }

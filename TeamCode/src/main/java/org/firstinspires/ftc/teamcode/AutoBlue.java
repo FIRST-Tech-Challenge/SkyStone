068c68e64ff90809a96c77b85e9ec01e27de1601
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2019-11-20 11:58:47 -0500

    Create AutoBlue.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name="Auto Blue", group="Autonomous")
+
+public class AutoBlue {
+}

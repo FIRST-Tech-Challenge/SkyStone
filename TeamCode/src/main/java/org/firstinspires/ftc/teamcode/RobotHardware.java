fad6eeb129b3c090c6020a2d044e977b75abea49
Steven K <steven@pointcircle.com>
2019-11-20 21:07:07 -0700

    Vuforia and TensorFlow progress.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -5,7 +5,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 
 /**
@@ -42,6 +46,31 @@ public class RobotHardware {
     public static final double CLAMP_CLOSE_DISTANCE = 0.75;
     public static final double CLAMP_ROTATOR_BEGINNING_SERVO = 0;
     public static final double KICKER_START = 0;
+
+
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";
+
+
+    private static final String VUFORIA_KEY = "AXl4o5z/////AAABmQyBF0iAaUTcguyLoBFeK1A7RHUVrQdTS" +
+            "sPDqn4DelLm7BtbLuahVuZvBzuq5tPGrvi7D25P3xRzVgT1d+cADoNAMxuRVZs24o87S6gH0mM+Q/OrrQr5" +
+            "7pTiumNffyuzBI728d+XgQJImM0rBxGcpwej8Ok0ZSCNIzzxVNf06dRwLEwu6jf0mCiA9yyffMFzreeL8UR" +
+            "wm/xxuDsYxY7NrVtjlmslMTiu3nAUboaDP8jkhKvl8623x57MhYt4hof+iegRYjJzt+Knb5m5SfY5urWFGF" +
+            "sLjZ4dqAhzXNiJmmKbKojUfjgvUld91gWm0UOXHkoezBuBVnLFasNmChD2uxpGGGeNdW1MvGitjFEvckKJ";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tensorFlowEngine} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    public TFObjectDetector tensorFlowEngine;
+
 
     /* local OpMode members. */
     HardwareMap hardwareMap     =  null;
@@ -119,9 +148,38 @@ public class RobotHardware {
             kicker = null;
         }
 
+        initVuforia(telemetry);
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTFOD(telemetry);
+        } else {
+            telemetry.addData("Warning", "Tensor Flow Object Detection not compatible");
+        }
 
         telemetry.update();
 
+    }
 
+    private void initVuforia(Telemetry telemetry) {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "camera_1");
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        telemetry.addData("Status", "Vuforia Initialized");
+
+    }
+
+    private void initTFOD(Telemetry telemetry) {
+        /* Initialize Tensor Flow Object Detection */
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tensorFlowEngine = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tensorFlowEngine.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+        telemetry.addData("Status", "Tensor Flow Object Detection Initialized");
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StevenTelop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StevenTelop.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+import java.util.List;
+
 
 @TeleOp(name="Steve Robot Test", group="Steve")
 public class StevenTelop extends LinearOpMode {
@@ -16,28 +20,37 @@ public class StevenTelop extends LinearOpMode {
 
         robot.init(hardwareMap, telemetry);
 
+        if (robot.tensorFlowEngine != null) {
+            robot.tensorFlowEngine.activate();
+        }
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            if (robot.tensorFlowEngine != null) {
+                List<Recognition> updatedRecognitions = robot.tensorFlowEngine.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+                    // step through the list of recognitions and display boundary info.
+                    int i = 0;
+                    for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+                    }
+                    telemetry.update();
+                }
 
-            robot.clamp.setPosition(.5);
-            sleep(1000);
-
-            robot.clamp.setPosition(-0.5);
-            sleep(1000);
-
-            telemetry.addData("We be counting",  "Counter = %7d", counter);
-            telemetry.update();
-
-
-            counter += 1;
-
-            if (counter == 5) {
-                break;
             }
 
+        }
+
+        if (robot.tensorFlowEngine != null) {
+            robot.tensorFlowEngine.shutdown();
         }
     }
 }

c03dbdc52b9043fa9d4ba44c9e3d3938f7e2cf81
Frerik <iktz10@icloud.com>
2019-11-22 15:51:04 +0100

    removed hardwaremap again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/hardware_tests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/hardware_tests.java
@@ -6,24 +6,22 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareChassis;
-import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareChassisGyro;
-import org.firstinspires.ftc.teamcode.HardwareMaps.HardwareNeutral;
-
 @TeleOp (name = "hardware_tests")
 public class hardware_tests extends OpMode {
     HardwareMap hwChss = hardwareMap;
 
 
-    Servo servo_hub1_port0 = null;
-    DcMotor motor_hub1_port0 = null;
-    DcMotor motor_hub1_port1 = null;
+    Servo servo_1 = null;
+    DcMotor motor_1 = null;
+    DcMotor motor_2 = null;
+    //Servo servo_c = null;
 
     @Override
     public void init() {
-        servo_hub1_port0 = hardwareMap.get(Servo.class, "servo_hub1_port0");
-        motor_hub1_port0 = hardwareMap.get(DcMotor.class, "motor_hub1_port0");
-        motor_hub1_port1 = hardwareMap.get(DcMotor.class, "motor_hub1_port1");
+        servo_1 = hardwareMap.get(Servo.class, "servo_hub1_port0");
+        motor_1 = hardwareMap.get(DcMotor.class, "motor_hub1_port0");
+        motor_2 = hardwareMap.get(DcMotor.class, "motor_hub1_port1");
+        //servo_c = hardwareMap.get(Servo.class, "c_servo_hub1_port1");
 
         //TO DO: Neue Hardwaremap für Robot erstellen!!
     }
@@ -33,29 +31,35 @@ public class hardware_tests extends OpMode {
         //move one servo from one psoition to another and back
         //using gamepad1.a und gamepad1.b
         if (gamepad1.a) {
-            servo_hub1_port0.setPosition(0.6);
+            servo_1.setPosition(0.6);
         }
         else if (gamepad1.b) {
-            servo_hub1_port0.setPosition(0.1);
+            servo_1.setPosition(0.1);
         }
 
         //use one motor at a time with stick y value
         //using gamepad1.right_stick_y
         else if (gamepad1.right_stick_y > 0 || gamepad1.right_stick_y < 0) {
-            motor_hub1_port0.setPower(gamepad1.right_stick_y);
+            motor_1.setPower(gamepad1.right_stick_y);
         }
 
         //use two motors in different directions with stick y value
         //using gamepad1.left_stick_y
         else if (gamepad1.left_stick_y > 0 || gamepad1.left_stick_y < 0) {
-            motor_hub1_port0.setPower(gamepad1.left_stick_y);
-            motor_hub1_port1.setPower(-gamepad1.left_stick_y);
+            motor_1.setPower(gamepad1.left_stick_y);
+            motor_2.setPower(-gamepad1.left_stick_y);
+        }
+
+        //use one continuous servo
+        //using gamepad1.x and y
+        else if (gamepad1.x) {
+            //servo_c.
         }
 
         //if nothing is pressed set all motors power 0
         else {
-            motor_hub1_port0.setPower(0);
-            motor_hub1_port1.setPower(0);
+            motor_1.setPower(0);
+            motor_2.setPower(0);
         }
     }
 

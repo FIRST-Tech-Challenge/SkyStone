484afcdd3d58afed367179042607582579cb0643
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2019-11-22 12:05:54 -0500

    Update PIDTuningOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidBullshit/PIDTuningOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidBullshit/PIDTuningOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.pidBullshit;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Maccabot;
 
@@ -12,15 +13,29 @@ public class PIDTuningOpMode extends OpMode {
 
     Maccabot robot;
 
+    private Servo servo;
+    private double encoder;
+
     @Override
     public void init() {
         robot = new Maccabot(this);
         robot.initializeRobot();
+        servo = hardwareMap.servo.get("servo");
+        encoder = 0;
     }
 
     @Override
     public void loop() {
         robot.mecanumDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
-        robot.intake(gamepad1.a, gamepad1.b);
+        robot.intake(gamepad2.left_trigger, gamepad2.right_trigger);
+        if(gamepad2.a){
+            servo.setPosition(0.5);
+
+        }
+        else if(gamepad2.b){
+            servo.setPosition(-0.5);
+
+        }
+        telemetry.addData("encoder value:", encoder);
     }
 }

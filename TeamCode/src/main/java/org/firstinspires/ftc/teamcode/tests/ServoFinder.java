b5706b8956844d451aeaf245a04c369fa82502f2
Jared <jaredgoodman03@gmail.com>
2019-11-30 12:09:36 -0800

    Create servo pos finder

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ServoFinder.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Servo Finder", group = "Test")
+public class ServoFinder extends OpMode {
+
+    public Servo pivot;
+    public Servo elbow;
+    public Servo wrist;
+    public double speed = 0.001;
+
+    public void init() {
+        pivot = hardwareMap.get(Servo.class, "pivot");
+        elbow = hardwareMap.get(Servo.class, "gripper");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+
+    }
+    public void loop() {
+        if (gamepad1.a) {
+            pivot.setPosition(pivot.getPosition() + speed);
+        }
+
+        if (gamepad1.b) {
+            elbow.setPosition(elbow.getPosition() + speed);
+        }
+        if (gamepad1.y) {
+            wrist.setPosition(wrist.getPosition() + speed);
+        }
+
+        if (gamepad1.dpad_down) {
+            pivot.setPosition(pivot.getPosition() - speed);
+        }
+        if (gamepad1.dpad_right) {
+            elbow.setPosition(elbow.getPosition() - speed);
+        }
+        if (gamepad1.dpad_up) {
+            wrist.setPosition(wrist.getPosition() - speed);
+        }
+
+        telemetry.addData("Pivot: ", pivot.getPosition());
+        telemetry.addData("Elbow: ", elbow.getPosition());
+        telemetry.addData("Wrist: ", wrist.getPosition());
+        telemetry.update();
+    }
+}

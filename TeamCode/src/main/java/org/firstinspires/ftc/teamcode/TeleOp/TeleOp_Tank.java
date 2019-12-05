e41f1391b23b89d123c2487ba624ef53c280a654
Timothy (Tikki) Cui <ticui21@icstudents.org>
2019-12-05 09:53:49 -0600

    Implement custom library

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_Tank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_Tank.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.SourceFiles.Trobot;
+
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
@@ -20,104 +22,54 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Tank Mode", group = "Tank Mode")
 public class TeleOp_Tank extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor drive_FL, drive_FR, drive_RL, drive_RR;
-    private DcMotor intake_L, intake_R;
-    private Servo latch_L, latch_R;
-
-    private boolean latched = false;
-    private String servoStatus = "Pending";
+    public Trobot trobot;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        drive_FL = hardwareMap.get(DcMotor.class, "front left");
-        drive_FR = hardwareMap.get(DcMotor.class, "front right");
-        drive_RL = hardwareMap.get(DcMotor.class, "rear left");
-        drive_RR = hardwareMap.get(DcMotor.class, "rear right");
-
-        intake_L = hardwareMap.get(DcMotor.class, "left intake");
-        intake_R = hardwareMap.get(DcMotor.class, "right intake");
-
-        latch_L = hardwareMap.get(Servo.class, "left servo");
-        latch_R = hardwareMap.get(Servo.class, "right servo");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        drive_FR.setDirection(DcMotor.Direction.REVERSE);
-        drive_RL.setDirection(DcMotor.Direction.REVERSE);
+        trobot = new Trobot(hardwareMap);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
+        trobot.runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             // Tank Mode uses both sticks to control each side.
-            drive_FL.setPower(gamepad1.left_stick_y);
-            drive_FR.setPower(gamepad1.right_stick_y);
-            drive_RL.setPower(gamepad1.left_stick_y);
-            drive_RR.setPower(gamepad1.right_stick_y);
+            double leftPower = gamepad1.left_stick_y;
+            double rightPower = gamepad1.right_stick_y;
+
+            trobot.drivetrain.drive(leftPower, rightPower);
 
             // Set D-Pad for strafing -> not used for Joe 2019-2020
-//            if (gamepad1.dpad_left) {
-//                drive_FL.setPower(-0.5);
-//                drive_FR.setPower(0.5);
-//                drive_RL.setPower(0.25);
-//                drive_RR.setPower(-0.25);
-//            } else if (gamepad1.dpad_right) {
-//                drive_FL.setPower(0.5);
-//                drive_FR.setPower(-0.5);
-//                drive_RL.setPower(-0.25);
-//                drive_RR.setPower(0.25);
-//            }
+            if (gamepad1.dpad_left) {
+                trobot.drivetrain.strafe("LEFT");
+            } else if (gamepad1.dpad_right) {
+                trobot.drivetrain.strafe("RIGHT");
+            }
 
             // Map triggers to intake motors
-            float LT = gamepad1.left_trigger;
-            float RT = gamepad1.right_trigger;
-
-            if (LT > 0 && RT == 0) {
-                intake_L.setPower(LT * -0.5);
-                intake_R.setPower(LT * 0.5);
-            } else if (RT > 0 && LT == 0) {
-                intake_L.setPower(RT * 0.25);
-                intake_R.setPower(RT * -0.25);
+            if (gamepad1.left_trigger > 0 && gamepad1.right_trigger == 0) {
+                trobot.component.intake("INTAKE");
+            } else if (gamepad1.right_trigger > 0 && gamepad1.left_trigger == 0) {
+                trobot.component.intake("RELEASE");
             } else {
-                intake_L.setPower(0);
-                intake_R.setPower(0);
+                trobot.component.intake("STOP");
             }
 
             // Map bumpers to foundation latches
-            boolean LB = gamepad1.left_bumper;
-            boolean RB = gamepad1.right_bumper;
-
-            if (LB) {
-                latch_L.setPosition(1);
-                latch_R.setPosition(0);
-
-                latched = true;
-            } else if (RB) {
-                latch_L.setPosition(0.35);
-                latch_R.setPosition(0.65);
-
-                latched = false;
-            }
-
-            if (latched) {
-                servoStatus = "latched";
-            } else {
-                servoStatus = "unlatched";
+            if (gamepad1.left_bumper) {
+                trobot.component.latch("LATCH");
+            } else if (gamepad1.right_bumper) {
+                trobot.component.latch("UNLATCH");
             }
 
             // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Run Time: " + trobot.runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", -gamepad1.left_stick_y, -gamepad1.right_stick_y);
-            telemetry.addData("Servos", servoStatus);
+            telemetry.addData("Servos", trobot.component.latchStatus);
             telemetry.update();
         }
     }

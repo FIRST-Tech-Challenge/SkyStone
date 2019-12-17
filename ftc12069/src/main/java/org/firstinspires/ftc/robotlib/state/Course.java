6982b1ca3dbd9b3f775267b2a062a1daba24e09e
Sei4or <sei4or@gmail.com>
2019-12-16 19:01:29 -0800

    Added Abstract Course

---
+++ ftc12069/src/main/java/org/firstinspires/ftc/robotlib/state/Course.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.robotlib.state;
+
+/**
+ * Abstract representations of course angles (basically simple enum).
+ */
+public class Course {
+    public static final int FORWARD = 0;
+    public static final int BACKWARD = 180;
+    public static final int LEFT = -90;
+    public static final int RIGHT = 90;
+}

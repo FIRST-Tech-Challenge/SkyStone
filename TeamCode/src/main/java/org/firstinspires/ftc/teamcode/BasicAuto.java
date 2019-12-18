2cfcefa5de6aa43240e7dc3d33a4370204b60bb0
Unknown <joshua.winebrener@gmail.com>
2019-12-18 14:55:24 -0600

    Modified basic auto from pasted version
    Now waits 27 or so seconds, then drives under the bridge.  During initloop, the driver can input red or blue alliance so the robot turns in the right direction.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -27,8 +27,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -50,32 +51,25 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
-public class BasicOpMode_Iterative extends OpMode
+@Autonomous(name="Basic Auto", group="Iterative Opmode")
+public class BasicAuto extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private HardwarePushbot robot = new HardwarePushbot();
+    private double waitTime = 26.0;
+    private double driveTime = 0.65;
+    private double rotateTime = 0.92;
+    private boolean blueTeam = true;
+
 
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
+        robot.init(hardwareMap);
         telemetry.addData("Status", "Initialized");
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -86,6 +80,10 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void init_loop() {
+        if (gamepad1.x) blueTeam = true;
+        else if (gamepad1.b) blueTeam = false;
+        if (blueTeam) telemetry.addData("Team", "Blue");
+        else telemetry.addData("Team", "Red");
     }
 
     /*
@@ -101,32 +99,13 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
-
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
-
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        if (runtime.seconds() > waitTime + 4 * driveTime + rotateTime) robot.mecanumDrive(0, 0, 0);
+        else if (runtime.seconds() > waitTime + 3 * driveTime + rotateTime) robot.mecanumDrive(0.2, 0, 0);
+        else if (runtime.seconds() > waitTime + 2 * driveTime + rotateTime) robot.mecanumDrive(0, 0, 0);
+        else if (runtime.seconds() > waitTime + 2 * driveTime) robot.mecanumDrive(0, 0, 0.2);
+        else if (runtime.seconds() > waitTime + driveTime) robot.mecanumDrive(0, 0, 0);
+        else if (runtime.seconds() > waitTime) robot.mecanumDrive(0.2, 0, 0);
+        else robot.mecanumDrive(0, 0, 0);
     }
 
     /*
@@ -134,6 +113,7 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void stop() {
+
     }
 
 }

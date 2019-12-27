8c35076102253822a46bcad42c0c6aea60fa12fc
AlphaLocke <47842170+AlphaLocke@users.noreply.github.com>
2019-12-27 13:38:32 -0600

    Auto1 secure version file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto1.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 /*
 Autonomous ONE will do simple operation regarding the grabbing of the score depot and parking from the depot side
@@ -21,7 +23,14 @@ public class auto1 extends LinearOpMode {
     public DcMotor BL;
     public DcMotor BR;
 
-    public Servo hooker;
+    public Servo hooker;\
+
+    public DistanceSensor distanceSensor;
+    
+    private double TURNSPEED = 0.4;
+    private double STRAFESPEED = 0.7;
+    private double STALKSPEED = 0.3;
+    double triggerDist = 10.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -36,78 +45,101 @@ public class auto1 extends LinearOpMode {
 
         hooker = hardwareMap.get(Servo.class, "hook");
 
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "dist");
+
         ElapsedTime runtime = new ElapsedTime();
         ElapsedTime backgroundTime = new ElapsedTime();
 
         waitForStart();
 
-        while(opModeIsActive()) {
-            //Go forward
-            backgroundTime.reset();
-            drive(1, backgroundTime.seconds(), 5);
-            dropDL();
+        //Go forward until waffle is within proximity
+        driveForward();
 
-            //Go backwards
-            backgroundTime.reset();
-            drive(2, backgroundTime.seconds(), 5);
-            raiseDL();
+        while(!tripWireActive(triggerDist)){
+            telemetry.addData("Closing in on target", !tripWireActive(triggerDist));
+            telemetry.update();
+            sleep(3000);
+            }
 
+        dropDL();
+
+        sleep(2000);
+
+        driveBackward();
+
+        while()
         }
+
+
+
+
+
+    }
+
+    public boolean tripWireActive(double triggerDist) {
+        if (distanceSensor.getDistance(DistanceUnit.CM)<triggerDist){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void rest() {
+        TL.setPower(0);
+        TR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
     }
 
     public void dropDL() {
-        hooker.setPosition(0.9);
-    }
-
-    public void raiseDL() {
         hooker.setPosition(0.1);
     }
 
-    public void drive(int cas3, double timeStart, double duration) {
-        switch (cas3)
-        {
-            case 1 :
-                /*
-                drive forward for x time
-                 */
-                while (timeStart<=time){
-                    TL.setPower(0.5);
-                    TR.setPower(0.5);
-                    BL.setPower(0.5);
-                    BR.setPower(0.5);
-                }
-            case 2 :
-                /*
-                drive backward for x time
-                 */
-                while(timeStart<=time){
-                    TL.setPower(-0.5);
-                    TR.setPower(-0.5);
-                    BL.setPower(-0.5);
-                    BR.setPower(-0.5);
-                }
-            case 3 :
-                /*
-                Turn ninety degrees right
-                 */
-                while (timeStart<=time){
-                    TL.setPower(0.5);
-                    BL.setPower(0.5);
-                    TR.setPower(-0.5);
-                    BR.setPower(-0.5);
-                }
-            case 4 :
-                /*
-                Turn ninety degrees left
-                 */
-                while (timeStart<=time) {
-                    TL.setPower(-0.5);
-                    BL.setPower(-0.5);
-                    TR.setPower(0.5);
-                    BR.setPower(0.5);
-                }
-        }
+    public void raiseDL() {
+        hooker.setPosition(0.9);
     }
 
+    public void driveForward(){
+        TL.setPower(STALKSPEED);
+        TR.setPower(STALKSPEED);
+        BL.setPower(STALKSPEED);
+        BR.setPower(STALKSPEED);
+    }
+
+    public void driveBackward() {
+        TL.setPower(-STALKSPEED);
+        BL.setPower(-STALKSPEED);
+        TR.setPower(-STALKSPEED);
+        BR.setPower(-STALKSPEED);
+    }
+
+    public void strafeLeft() {
+        TL.setPower(-STRAFESPEED);
+        BL.setPower(STRAFESPEED);
+        BR.setPower(STRAFESPEED);
+        TR.setPower(-STRAFESPEED);
+    }
+
+    public void strafeRight() {
+        TL.setPower(STRAFESPEED);
+        BL.setPower(-STRAFESPEED);
+        BR.setPower(-STRAFESPEED);
+        TR.setPower(STRAFESPEED);
+    }
+    
+    public void turnRight() {
+        TL.setPower(STALKSPEED);
+        TR.setPower(-STALKSPEED);
+        BL.setPower(STALKSPEED);
+        BR.setPower(-STALKSPEED);
+    }
+
+    public void turnLeft() {
+        TL.setPower(-STALKSPEED);
+        BL.setPower(-STALKSPEED);
+        TR.setPower(STALKSPEED);
+        BR.setPower(STALKSPEED);
+    }
 
 }

59c555dcbeaa4dae202e313eea16aa19afa2a936
AlphaLocke <47842170+AlphaLocke@users.noreply.github.com>
2020-01-03 11:57:52 -0600

    Update TensorFlowObj.java
    Took out the disabled tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObj.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObj.java
@@ -50,7 +50,6 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
 public class TensorFlowObj extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";

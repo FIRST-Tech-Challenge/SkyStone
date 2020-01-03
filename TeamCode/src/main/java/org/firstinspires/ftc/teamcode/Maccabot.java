8befaa08ba06fe36625fb8a285e34194767a7f8d
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2020-01-03 12:46:47 -0500

    Added Gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Maccabot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Maccabot.java
@@ -1,11 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import static android.os.SystemClock.sleep;
 
 public class Maccabot {
 
@@ -21,6 +29,10 @@ public class Maccabot {
     private Servo chad;
 
     private double liftTargetPos = 0;
+
+    public BNO055IMU imu;
+    Orientation lastAngles = new Orientation();
+    double                  globalAngle, power = .30, correction;
 
     // Intake Motors TBD
     // private DcMotor intake_left, intake_right;
@@ -75,6 +87,17 @@ public class Maccabot {
         back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intake_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intake_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        BNO055IMU.Parameters param = new BNO055IMU.Parameters();
+
+        param.mode                = BNO055IMU.SensorMode.IMU;
+        param.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        param.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        param.loggingEnabled      = false;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(param);
     }
 
     public void drive(double flPower, double frPower, double blPower, double brPower){
@@ -153,6 +176,9 @@ public class Maccabot {
     }
 
     public void auto_forward(int pos, double power){//method for autored for the robot to go forward
+
+        correction = checkDirection();
+
         back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -167,19 +193,19 @@ public class Maccabot {
         back_left.setDirection(DcMotorSimple.Direction.FORWARD);
         back_right.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        back_left.setPower(power);
+        back_left.setPower(power - correction);
         back_left.setTargetPosition(pos);
         back_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        front_left.setPower(power);
+        front_left.setPower(power - correction);
         front_left.setTargetPosition(-pos);
         front_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        front_right.setPower(power);
+        front_right.setPower(power + correction);
         front_right.setTargetPosition(-pos);
         front_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        back_right.setPower(power);
+        back_right.setPower(power + correction);
         back_right.setTargetPosition(pos);
         back_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
@@ -189,6 +215,7 @@ public class Maccabot {
         parentOpMode.telemetry.addData("back-right", back_right.getCurrentPosition());
 
         parentOpMode.telemetry.update();
+
 
 
     }
@@ -423,5 +450,99 @@ public class Maccabot {
         back_right.setPower(power);
 
     }
+
+    public double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    public double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = .10;
+
+        angle = getAngle();
+
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+
+        correction = correction * gain;
+
+        return correction;
+    }
+    public void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+    public void rotate(int degrees, double power)
+    {
+        double  leftPower, rightPower;
+
+        // restart imu movement tracking.
+        resetAngle();
+
+        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+        // clockwise (right).
+
+        if (degrees < 0)
+        {   // turn right.
+            leftPower = power;
+            rightPower = -power;
+        }
+        else if (degrees > 0)
+        {   // turn left.
+            leftPower = -power;
+            rightPower = power;
+        }
+        else return;
+
+        // set power to rotate.
+        drive(leftPower, rightPower, leftPower, rightPower);
+
+        // rotate until turn is completed.
+        if (degrees < 0)
+        {
+            // On right turn we have to get off zero first.
+            while (getAngle() == 0) {}
+
+            while (getAngle() > degrees) {}
+        }
+        else    // left turn.
+            while (getAngle() < degrees) {}
+
+        // turn the motors off.
+        setpower0();
+
+        // wait for rotation to stop.
+        sleep(1000);
+
+        // reset angle tracking on new heading.
+        resetAngle();
+    }
+    public boolean isGyroCalibrated(){return imu.isGyroCalibrated();}
 
 }

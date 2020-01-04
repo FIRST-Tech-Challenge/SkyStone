a6d88fd3511958d97d8f9b936bf7a56b04466591
RS <6tigersrds@gmail.com>
2020-01-04 07:49:45 -0500

    Add some test ops

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/SimpleBot.java
@@ -0,0 +1,73 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.bots;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.components.DriveTrain;
+import org.firstinspires.ftc.teamcode.components.Grapple;
+import org.firstinspires.ftc.teamcode.components.GyroNavigator;
+import org.firstinspires.ftc.teamcode.components.Intake;
+import org.firstinspires.ftc.teamcode.components.Logger;
+import org.firstinspires.ftc.teamcode.components.Ramp;
+import org.firstinspires.ftc.teamcode.components.SkystoneFinder;
+import org.firstinspires.ftc.teamcode.components.WebCamNavigator;
+import org.firstinspires.ftc.teamcode.components.WebCamera;
+
+public class SimpleBot extends Bot {
+
+    /* BotComponents */
+
+    public Logger logger = null;
+    public DriveTrain driveTrain = null;
+
+    /* Constructor */
+    public SimpleBot() {
+
+    }
+
+    public SimpleBot(OpMode aOpMode) {
+        this(aOpMode, false, false);
+    }
+
+    public SimpleBot(OpMode aOpMode, boolean enableTrace, boolean enableTelemetry) {
+
+        logger = new Logger("SimpleBot", aOpMode, enableTrace, enableTelemetry);
+        driveTrain = new DriveTrain(logger, aOpMode, "frontLeftMotor", "frontRightMotor",
+                "backLeftMotor", "backRightMotor");
+
+    }
+
+    public void initAll() {
+        driveTrain.init(DriveTrain.InitType.INIT_4WD);
+    }
+
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTrain.java
@@ -61,7 +61,7 @@ public class DriveTrain extends BotComponent {
     private double COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                              (WHEEL_DIAMETER_INCHES * 3.1415);
 
-    private double MAX_INCHES_PER_SECOND   = 12;
+    private double MAX_INCHES_PER_SECOND   = 6;
 
     /* Constructor */
     public DriveTrain() {

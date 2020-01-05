b44a1096556891bd38c6492a81e3c69aa07c1d5c
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2020-01-05 11:08:08 -0500

    Create auto_competition1_outside.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_competition1_outside.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Auto Outside", group="Autonomous")
+public class auto_competition1_outside extends LinearOpMode {
+    private Maccabot robot;
+
+
+    public void runOpMode() throws InterruptedException {
+        robot = new Maccabot(this);
+        robot.initializeRobot();
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            robot.auto_forward(800, 0.65);
+            sleep(1000);
+            robot.auto_straferight(800, 0.65);
+            sleep(800);
+            stop();
+        }
+
+    }
+}

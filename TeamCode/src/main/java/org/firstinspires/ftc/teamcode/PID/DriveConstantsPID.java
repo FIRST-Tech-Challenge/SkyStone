829e64a8f7f36e4d6a407d7d728b05cc8845e6b5
wangx <wxiaodong@gmail.com>
2020-01-07 09:44:50 -0800

    path test done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/DriveConstantsPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/DriveConstantsPID.java
@@ -121,7 +121,7 @@ public class DriveConstantsPID {
      * forces acceleration-limited profiling).
      */
     public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(
-            24.0, 12.0, 0.0,
+            45.0, 20.0, 0.0,
             Math.toRadians(180.0), Math.toRadians(180.0), 0.0
     );
     public static DriveConstraints STRAFE_BASE_CONSTRAINTS = new DriveConstraints(
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/calibration/PathTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/calibration/PathTest.java
@@ -21,45 +21,49 @@ import org.firstinspires.ftc.teamcode.PID.mecanum.SampleMecanumDriveREV;
 @Autonomous(group = "drive")
 public class PathTest extends LinearOpMode {
     private Trajectory trajectory;
-    private BaseTrajectoryBuilder builder;
+    private BaseTrajectoryBuilder builder, strafe_builder;
     private Pose2d current_pose;
+    private String TAG = "PathTest";
     @Override
     public void runOpMode() throws InterruptedException {
         DriveConstantsPID.updateConstantsFromProperties();
+        RobotLog.dd(TAG, "create non-strafe drive");
         SampleMecanumDriveBase drive = new SampleMecanumDriveREV(hardwareMap, false);
+        RobotLog.dd("Current Position", drive.getPoseEstimate().toString());
+        current_pose = drive.getPoseEstimate();
+
+        RobotLog.dd(TAG, "create strafe drive");
+        SampleMecanumDriveBase strafe_drive = new SampleMecanumDriveREV(hardwareMap, true);
+        RobotLog.dd("Current Position", strafe_drive.getPoseEstimate().toString());
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        Trajectory trajectory = drive.trajectoryBuilder()
-                .strafeLeft(DriveConstantsPID.TEST_DISTANCE)
-                .build();
-
-        drive.getLocalizer().setPoseEstimate(new Pose2d(new Vector2d(drive.getPoseEstimate().getX(),
+        RobotLog.dd(TAG, "update pose for strafe drive");
+        strafe_drive.getLocalizer().setPoseEstimate(new Pose2d(new Vector2d(drive.getPoseEstimate().getX(),
                 drive.getPoseEstimate().getY()), drive.getExternalHeading()));
+        strafe_drive.getLocalizer().update();
+
+        strafe_drive.setPoseEstimate(current_pose);
+
+        strafe_builder = new TrajectoryBuilder(strafe_drive.getPoseEstimate(), DriveConstantsPID.STRAFE_BASE_CONSTRAINTS);
+
+        strafe_builder = strafe_builder.strafeTo(new Vector2d(drive.getPoseEstimate().getX(),24));
+
+        trajectory = strafe_builder.build();
+        strafe_drive.followTrajectorySync(trajectory);
+
+        RobotLog.dd(TAG, "update pose for drive after strafing: " + strafe_drive.getPoseEstimate().toString());
+        drive.getLocalizer().setPoseEstimate(new Pose2d(new Vector2d(strafe_drive.getPoseEstimate().getX(),
+                strafe_drive.getPoseEstimate().getY()), strafe_drive.getExternalHeading()));
         drive.getLocalizer().update();
-        current_pose = drive.getPoseEstimate();
 
-        drive = new SampleMecanumDriveREV(hardwareMap, true);
-        drive.setPoseEstimate(current_pose);
-        RobotLog.dd("Current Position", drive.getPoseEstimate().toString());
-
-        builder = new TrajectoryBuilder(drive.getPoseEstimate(), DriveConstantsPID.BASE_CONSTRAINTS);   //-34.752, -63.936
-        if(DriveConstantsPID.RUN_USING_ODOMETRY_WHEEL) {
-
-            builder = builder//.strafeTo(new Vector2d(drive.getPoseEstimate().getX(), -63.936 + 5.0))
-                    /*.strafeTo(new Vector2d(drive.getPoseEstimate().getX(), -63.936 + 5.0 * 2))
-                    .strafeTo(new Vector2d(drive.getPoseEstimate().getX(), -63.936 + 5.0 * 3))
-                    .strafeTo(new Vector2d(drive.getPoseEstimate().getX(), -63.936 + 5.0 * 4))
-                    .strafeTo(new Vector2d(drive.getPoseEstimate().getX(), -63.936 + 5.0 * 5))*/
-                    .setReversed(true).lineTo(new Vector2d(-51, -39));
-
-        } else {
-            builder = builder.strafeTo(new Vector2d(drive.getPoseEstimate().getX(),-30)).setReversed(true)
-                    .lineTo(new Vector2d(-50, -30));
-        }
-        trajectory = builder.build();   //x - 2.812, y + 7.984
+        builder = new TrajectoryBuilder(drive.getPoseEstimate(), DriveConstantsPID.BASE_CONSTRAINTS);
+        builder.forward(72);
+        trajectory = builder.build();
         drive.followTrajectorySync(trajectory);
+        RobotLog.dd(TAG, "done testing, current pose: " + drive.getPoseEstimate().toString());
+
     }
 }

6272d623f1dc1117e202bb6efa3ae32357b33915
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2020-01-08 16:50:09 -0500

    Update MainTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -32,7 +32,7 @@ public class MainTeleOp extends OpMode {
 
     public void loop() {
         // Run a mecanum drive on the first gamepad - left stick translates, right stick rotates
-        robot.mecanumDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        robot.mecanumDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x);
         // runs intake on second driver left stick
         robot.intake(gamepad2.left_stick_y);
         // runs rack on second driver right stick

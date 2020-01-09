1c2ffc15dc19266af1dc8dacd95e6b2b66c676bc
Nathan Lesser <nlesser16@gmail.com>
2020-01-09 15:30:34 -0500

    Introduced jerk-based limiting for fast and standard motion profiles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/DriveConstants.java
@@ -69,12 +69,12 @@ public class DriveConstants {
      * forces acceleration-limited profiling).
      */
     public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(
-            30.0, 30.0, 0.0,
-            Math.toRadians(180.0), Math.toRadians(180.0), 0.0
+            30.0, 30.0, 120.0,
+            Math.toRadians(180.0), Math.toRadians(180.0), 720.0
     );
     public static DriveConstraints FAST_CONSTRAINTS = new DriveConstraints(
-            40.0, 30.0, 0.0,
-            Math.toRadians(180.0), Math.toRadians(180.0), 0
+            40.0, 40.0, 160.0,
+            Math.toRadians(180.0), Math.toRadians(180.0), 720.0
     );
 
 

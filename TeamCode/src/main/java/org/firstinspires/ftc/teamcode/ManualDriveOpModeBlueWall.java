6844b08cb8e0c231d663f56784e631b64ab36537
Eric <eyang420@gmail.com>
2020-01-11 17:33:35 -0800

    capstone(djsjfl)
    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveOpModeBlueWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDriveOpModeBlueWall.java
@@ -13,7 +13,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  */
 @TeleOp(name = "Manual Drive Blue")
 public class ManualDriveOpModeBlueWall extends LinearOpMode {
-    private ButtArmBot robot = new ButtArmBot(this);
+    private servoDropBot robot = new servoDropBot(this);
     private boolean arcadeMode = false;
     private int gyroCalibratedCount = 0;
 
@@ -33,7 +33,7 @@ public class ManualDriveOpModeBlueWall extends LinearOpMode {
             robot.manualPickupSkystone(gamepad1.dpad_left);
             robot.manualDropSkystone(gamepad1.dpad_right);
             robot.toggleButtArm(gamepad1.right_stick_button, gamepad1.left_stick_button);
-
+            robot.toggleServoDrop(gamepad2.a);
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoDropBot.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class servoDropBot extends ButtArmBot{
+    public Servo servoDrop = null;
+    double position = 0.1;
+
+    public servoDropBot(LinearOpMode opMode) {
+        super(opMode);
+    }
+
+    @Override
+    public void init(HardwareMap ahwMap) {
+        super.init(ahwMap);
+
+        servoDrop = hwMap.servo.get("servoDrop");
+
+        servoDrop.setPosition(position);
+    }
+    public void toggleServoDrop(boolean Abutton){
+        if(Abutton){
+            servoDrop.setPosition(position-0.2);
+        }
+    }
+
+
+}

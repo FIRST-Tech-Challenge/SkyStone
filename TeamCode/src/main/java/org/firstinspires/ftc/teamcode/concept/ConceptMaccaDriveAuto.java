a31dc520930a6717c084d1d468870741c02048df
Nathan Lesser <nlesser16@gmail.com>
2020-01-13 13:24:03 -0500

    Added "THE SONG" at Micah's request

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/concept/ConceptMaccaDriveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/concept/ConceptMaccaDriveAuto.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.concept;
 
+import android.media.MediaPlayer;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.hardware.MaccabotV2;
 
 public class ConceptMaccaDriveAuto extends LinearOpMode {
@@ -10,10 +13,18 @@ public class ConceptMaccaDriveAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        // Micah's greatest contribution to the code
+        MediaPlayer mp = MediaPlayer.create(hardwareMap.appContext, R.raw.poortown_scrub_noah_cut);
+
         robot = new MaccabotV2(this);
         robot.initialize(true);
 
+        mp.start();
+
         waitForStart();
+
+        mp.stop();
 
         telemetry.clearAll();
         telemetry.addLine("OpMode Started.");

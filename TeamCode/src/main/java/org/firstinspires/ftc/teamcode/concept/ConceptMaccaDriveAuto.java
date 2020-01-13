5f98d50821c99ef5c8ee8e2953dd5df2e2068246
Nathan Lesser <nlesser16@gmail.com>
2020-01-12 23:25:28 -0500

    Added new TeleOpV2 OpMode and ConceptMaccaDriveAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/concept/ConceptMaccaDriveAuto.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.concept;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MaccabotV2;
+
+public class ConceptMaccaDriveAuto extends LinearOpMode {
+
+    MaccabotV2 robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new MaccabotV2(this);
+        robot.initialize(true);
+
+        waitForStart();
+
+        telemetry.clearAll();
+        telemetry.addLine("OpMode Started.");
+
+        robot.drive.setLinearTargets(1000, 1000);
+        telemetry.addLine("Targets set.");
+
+        while (opModeIsActive() && robot.drive.isDriveBusy()) {
+            robot.drive.runToTargets(40);
+        }
+        robot.drive.setMotorPowers(0, 0, 0, 0);
+        telemetry.addLine("Target achieved. All clear!");
+        sleep(5000);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpV2.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.MaccabotV2;
+
+public class TeleOpV2 extends OpMode {
+
+    MaccabotV2 robot;
+
+    @Override
+    public void init() {
+        robot = new MaccabotV2(this);
+        robot.initialize(false);
+    }
+
+    @Override
+    public void start() {
+        telemetry.clearAll();
+        telemetry.addLine("4466 TELEOP v2 | ACTIVE");
+        robot.drawTelemetry(); // puts up debug values for use during practice
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        robot.drive.arcadeMecanumDrive(gamepad1.left_stick_x,
+                -gamepad1.left_stick_y, -gamepad1.right_stick_x);
+
+        robot.intake.runIntake(gamepad1.right_trigger - gamepad1.left_trigger);
+
+        robot.lift.runLift(gamepad2.right_stick_y);
+
+        robot.lift.runRack(gamepad2.left_stick_y);
+
+        robot.lift.runChad(gamepad2.right_bumper, gamepad2.left_bumper);
+    }
+}

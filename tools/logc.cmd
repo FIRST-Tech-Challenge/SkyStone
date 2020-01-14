ab6ef924f43ae933b5000ba66dcc55ef98c75fcd
wangx <wxiaodong@gmail.com>
2020-01-13 19:07:12 -0800

    .

---
+++ tools/logc.cmd
@@ -0,0 +1,31 @@
+@echo off
+@echo cmd logfilename ps_name
+mv %1.log %1_old.log
+adb logcat -c
+start setprop.cmd 1
+IF "%2"==""  (
+	adb shell ps |grep com.qualcomm.ftcrobotcontroller>ps.txt
+) ELSE (
+	adb shell ps |grep %2 >ps.txt
+)
+set /p ftcrobot=<ps.txt
+IF "%ftcrobot%"=="" (
+	@echo "com.qualcomm.ftcrobotcontroller has died!!!"
+	goto XX
+)
+echo %ftcrobot%
+
+for /F "tokens=1,2 delims=: " %%a in ("%ftcrobot%") do (
+   @rem echo %%a
+   echo %%b
+   set var=%%b
+)
+IF "%1"==""  (
+	rem adb logcat -b all -v color --pid %var% 
+	adb logcat -b all -v color |grep %var%
+) ELSE (
+	rem adb logcat -b all  --pid %var% |wtee %1.log
+	adb logcat -b all -v color |grep %var% |wtee %1.log
+)
+
+:XX

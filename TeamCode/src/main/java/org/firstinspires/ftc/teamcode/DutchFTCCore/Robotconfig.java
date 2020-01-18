d449b36f30b1b513bfd7a9727e88f9337c081b74
JanTheBeast <janvanbaast@live.nl>
2020-01-18 22:15:16 +0100

    Added drivetrain types
    This is a first implementation of the different drivetrain types, perhaps later a system will be implemented so that you can only select one at a time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DutchFTCCore/Robotconfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DutchFTCCore/Robotconfig.java
@@ -10,6 +10,13 @@ public class Robotconfig {
     public static String MotorFrontRight = "MotorFrontRight";
     public static String MotorBackRight = "MotorBackRight";
 
+    //drivetrains
+    public static boolean TankDrive2Motors = false;
+    public static boolean TankDrive1Motor = true;
+    public static boolean MechanumDrive = false;
+    public static boolean H_Drive = false;
+    public static boolean CrossOmniDrive = false;
+
     //logging
     public static String teamName = "FTCunits";
     public static boolean isUsed

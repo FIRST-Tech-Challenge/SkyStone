eb1383c8a4f5ef3772453e2a309e5aa7d4b532e4
kzhou57 <kevin.zhou@gmi-mr.com>
2020-01-18 12:01:54 -0800

    Revert "Added a few things"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWheelsDriveBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourWheelsDriveBot.java
@@ -6,14 +6,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 //@Autonomous(name="Drive Encoder2", group="Exercises")
 
-public class FourWheelsDriveBot extends LinearOpMode {
+public class FourWheelsDriveBot
+{
 
     // Gobilda 435 rpm DC motor : Encoder Countable Events Per Revolution (Output Shaft) : 383.6 * 2 (2:1 bevel gear ratio)
     static final double DRIVING_MOTOR_TICK_COUNT = 767;
@@ -264,10 +269,6 @@ public class FourWheelsDriveBot extends LinearOpMode {
         leftRear.setPower(power);
         rightRear.setPower(power);
         while (this.opMode.opModeIsActive() && leftFront.isBusy()) {
-            leftFront.setPower(power);
-            rightFront.setPower(power);
-            leftRear.setPower(power);
-            rightRear.setPower(power);
             // Display it for the driver.
             print(String.format("Target : %7d @ leftFront: %7d, rightFront:%7d, leftRear:%7d, rightRear:%7d",
                     target,
@@ -287,93 +288,5 @@ public class FourWheelsDriveBot extends LinearOpMode {
                 rightFront.getCurrentPosition(),
                 leftRear.getCurrentPosition(),
                 rightRear.getCurrentPosition()));
-    }
-
-    public void driveStraightByDistanceWithoutEncoder(int direction, double distance, double maxPower) {
-        // distance (in mm) = revolution * pi * diameter (100 mm)
-        int target = (int) (distance / 3.1415 / 100 * DRIVING_MOTOR_TICK_COUNT);
-        int startingPosition = leftFront.getCurrentPosition();
-        switch (direction) {
-            case DIRECTION_FORWARD:
-                leftFront.setTargetPosition(leftFront.getCurrentPosition() + target);
-                rightFront.setTargetPosition(rightFront.getCurrentPosition() + target);
-                leftRear.setTargetPosition(leftRear.getCurrentPosition() + target);
-                rightRear.setTargetPosition(rightRear.getCurrentPosition() + target);
-                break;
-            case DIRECTION_BACKWARD:
-                leftFront.setTargetPosition(leftFront.getCurrentPosition() - target);
-                rightFront.setTargetPosition(rightFront.getCurrentPosition() - target);
-                leftRear.setTargetPosition(leftRear.getCurrentPosition() - target);
-                rightRear.setTargetPosition(rightRear.getCurrentPosition() - target);
-                break;
-            case DIRECTION_LEFT:
-                leftFront.setTargetPosition(leftFront.getCurrentPosition() - target);
-                rightFront.setTargetPosition(rightFront.getCurrentPosition() + target);
-                leftRear.setTargetPosition(leftRear.getCurrentPosition() + target);
-                rightRear.setTargetPosition(rightRear.getCurrentPosition() - target);
-                break;
-            case DIRECTION_RIGHT:
-                leftFront.setTargetPosition(leftFront.getCurrentPosition() + target);
-                rightFront.setTargetPosition(rightFront.getCurrentPosition() - target);
-                leftRear.setTargetPosition(leftRear.getCurrentPosition() - target);
-                rightRear.setTargetPosition(rightRear.getCurrentPosition() + target);
-                break;
-            case DIRECTION_RQUARTER:
-                leftFront.setTargetPosition(leftFront.getCurrentPosition() + target);
-                rightFront.setTargetPosition(rightFront.getCurrentPosition() - target);
-                leftRear.setTargetPosition(leftRear.getCurrentPosition() + target);
-                rightRear.setTargetPosition(rightRear.getCurrentPosition() - target);
-                break;
-            case DIRECTION_LQUARTER:
-                leftFront.setTargetPosition(leftFront.getCurrentPosition() - target);
-                rightFront.setTargetPosition(rightFront.getCurrentPosition() + target);
-                leftRear.setTargetPosition(leftRear.getCurrentPosition() - target);
-                rightRear.setTargetPosition(rightRear.getCurrentPosition() + target);
-                break;
-            default:
-                String msg = String.format("Unaccepted direction value (%d) for driveStraightByDistance()", direction);
-                print(msg);
-        }
-
-        double power = maxPower;
-
-        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftFront.setPower(power);
-        rightFront.setPower(power);
-        leftRear.setPower(power);
-        rightRear.setPower(power);
-        while (this.opMode.opModeIsActive() && leftFront.isBusy()) {
-            leftFront.setPower(power);
-            rightFront.setPower(power);
-            leftRear.setPower(power);
-            rightRear.setPower(power);
-            // Display it for the driver.
-            print(String.format("Target : %7d @ leftFront: %7d, rightFront:%7d, leftRear:%7d, rightRear:%7d",
-                    target,
-                    leftFront.getCurrentPosition(),
-                    rightFront.getCurrentPosition(),
-                    leftRear.getCurrentPosition(),
-                    rightRear.getCurrentPosition()));
-        }
-        // Stop all motion;
-        leftFront.setPower(0);
-        rightFront.setPower(0);
-        leftRear.setPower(0);
-        rightRear.setPower(0);
-        print(String.format("Arrive target : %7d @ leftFront: %7d, rightFront:%7d, leftRear:%7d, rightRear:%7d",
-                target,
-                leftFront.getCurrentPosition(),
-                rightFront.getCurrentPosition(),
-                leftRear.getCurrentPosition(),
-                rightRear.getCurrentPosition()));
-
-    }
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-
     }
 }

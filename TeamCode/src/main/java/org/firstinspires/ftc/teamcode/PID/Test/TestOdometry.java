73f415afcd0e2ff2a6aff44ee9067f167407a3e0
wangx <wxiaodong@gmail.com>
2020-01-20 08:16:07 -0800

    cleaned up code to avoid human error and added back logging to for characterizing delay performance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/Test/TestOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/Test/TestOdometry.java
@@ -68,7 +68,7 @@ public class TestOdometry extends LinearOpMode {
 
         double driftRemover = 0.0;
 
-        drive.updateDriveConstants(false);
+        drive.resetFollowerWithParameters(false);
 
         while(opModeIsActive()){
             builder = new TrajectoryBuilder(drive.getPoseEstimate(), DriveConstantsPID.BASE_CONSTRAINTS);
@@ -195,7 +195,7 @@ public class TestOdometry extends LinearOpMode {
             public Object invoke() {
                 Thread thread = new Thread() {
                     public void run() {
-                        drive.updateDriveConstants(strafe);
+                        drive.resetFollowerWithParameters(strafe);
                     }
                 };
                 thread.start();

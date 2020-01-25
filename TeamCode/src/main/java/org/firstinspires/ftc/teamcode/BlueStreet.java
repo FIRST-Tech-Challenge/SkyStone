81e3522260a8402c31df5fd0a5b1b027e4ddb747
gronkybong <48972370+gronkybong@users.noreply.github.com>
2020-01-24 16:30:56 -0800

    blue auto is tweaked

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStreet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueStreet.java
@@ -13,8 +13,8 @@ public class BlueStreet extends LinearOpMode {
     int direction_forward, direction_backward;
 
     int[] distFirstMove = new int[]{300, 100, -100};
-    int[] distFoundMove = new int[]{2000, 1800, 1600};
-    int[] distBackMove = new int[]{1600, 2500, 2300};
+    int[] distFoundMove = new int[]{1700, 1500, 1300};
+    int[] distBackMove = new int[]{1400, 2250, 2000};
 
     int skystonePostition;
     protected void setDirection(){
@@ -30,11 +30,11 @@ public class BlueStreet extends LinearOpMode {
         waitForStart();
         setDirection();
         skystonePostition = robot.detectSkystone();
-            robot.driveUntilDistance(20, 0.3, 0);
+            robot.driveUntilDistance(19, 0.3, 0);
             robot.driveStraightByDistance(robot.DIRECTION_FORWARD, distFirstMove[skystonePostition - 1]);
 
             robot.pickupSkyStone();
-            RobotLog.d(String.format("Picked up Skystone!"));
+
             sleep(500);
 //            robot.driveUntilDistance(35, 0.3, 1);
 
@@ -43,37 +43,33 @@ public class BlueStreet extends LinearOpMode {
 
             robot.driveStraightByGyro(direction_forward, distFoundMove[skystonePostition - 1], 1, false);
 
-
-            RobotLog.d(String.format("Made it to the foundation!"));
-
-
 //            robot.goBacktoStartAngle();
 
             robot.driveStraightByDistance(robot.DIRECTION_LEFT, 200, 0.5);
             robot.dropSkyStone();
-            RobotLog.d(String.format("Dropped off Skystone!"));
+
             robot.originalPosition();
             robot.driveStraightByDistance(robot.DIRECTION_RIGHT, 200, 0.5);
-            RobotLog.d(String.format("Moved away from foundation!"));
+
 
 //            robot.driveStraightByDistance(direction_backward, distBackMove[skystonePostition - 1], 0.8);
 
             robot.driveStraightByGyro(direction_backward, distBackMove[skystonePostition - 1], 0.8, false);
 
 
-            RobotLog.d(String.format("Back at the quarry!"));
 
 
 //            robot.goBacktoStartAngle();
 
 //            sleep(1 * 1000);
 
-            robot.driveUntilDistance(20, 0.3, 0);
+            robot.driveStraightByDistance(robot.DIRECTION_RIGHT, 200, 0.5);
+
+            robot.driveUntilDistance(19, 0.3, 0);
 
             robot.pickupSkyStone();
             sleep(500);
 
-            RobotLog.d(String.format("Picked up Skystone!"));
 
 //            robot.driveUntilDistance(35, 0.3, 0);
 //            robot.driveStraightByDistance(direction_forward, distBackMove[skystonePostition - 1], 0.8);
@@ -81,19 +77,15 @@ public class BlueStreet extends LinearOpMode {
             robot.driveStraightByGyro(direction_forward, distBackMove[skystonePostition - 1] + 300, 1, false);
 
 
-            RobotLog.d(String.format("Made it to the foundation!"));
-
 
             robot.driveStraightByDistance(robot.DIRECTION_LEFT, 200, 0.5);
             robot.dropSkyStone();
-            RobotLog.d(String.format("Dropped off Skystone!"));
 
-            robot.driveStraightByDistance(robot.DIRECTION_RIGHT, 200, 0.5);
-            RobotLog.d(String.format("Moved away from foundation!"));
+            robot.driveStraightByDistance(robot.DIRECTION_RIGHT, 100, 0.5);
 
 
-            robot.driveStraightByDistance(direction_backward, 1300, 1);
-            RobotLog.d(String.format("Parked!"));
+            robot.driveStraightByDistance(direction_backward, 1400, 1);
+
 
         }
 

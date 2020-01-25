77567e4cd312be4b0fda112faa1dba2cf6d529c1
20seethalla <shreyas.seethalla@gmail.com>
2020-01-25 14:54:11 -0500

    FIRST PLACE AT COMP!!!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSBuildingZoneR.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSBuildingZoneR.kt
@@ -22,37 +22,52 @@ class SSBuildingZoneR : LinearOpMode()
         //robot.vSlide?.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         //robot.vSlide?.mode = DcMotor.RunMode.RUN_USING_ENCODER
         robot.vSlide?.mode = DcMotor.RunMode.RUN_TO_POSITION
+        robot.vSlide?.targetPosition = robot.vSlide!!.currentPosition
+
         waitForStart()
         robot.leftHook?.position = 0.0
         robot.rightHook?.position = 0.0
-        robot.vSlide?.targetPosition = 50 + robot.vSlide!!.currentPosition
-        robot.vSlide?.power = 0.5
+        //robot.vSlide?.targetPosition = 50 + robot.vSlide!!.currentPosition
+        robot.vSlide?.power = 1.0
         pause()
         robot.drive(0.50) //Drives Forward to the Stones
-        sleep(1550)
+        sleep(1700)
         pause()
         robot.claw?.position = 0.0
         pause()
-        robot.vSlide?.targetPosition = 50 + robot.vSlide!!.currentPosition
         sleep(500)
+        /*robot.vSlide?.targetPosition = 50 + robot.vSlide!!.currentPosition
+        sleep(500)*/
         robot.drive(-0.50)
-        sleep(450)
+        sleep(500)
         pause()
         robot.strafe(-1.0)//Heads to Foundation
         sleep(3550)
+        pause()
+        robot.vSlide?.targetPosition = 2500 + robot.vSlide!!.currentPosition
+        sleep(750)
+        robot.hSlide?.position = 0.3
+        sleep(500)
+        robot.hSlide?.position = 0.5
         robot.drive(0.5)
-        sleep(250)
+        sleep(900)
         pause()
         robot.claw?.position = robot.clawPinchPos
         pause()
+        robot.drive(-0.5)
+        sleep(1200)
+        pause()
+        robot.vSlide?.targetPosition = robot.vSlide!!.currentPosition - 2500
+        sleep(750)
+        pause()
         robot.strafe(0.5)
-        sleep(500)
+        sleep(2100)
         robot.claw?.position = 0.0
         pause()
         robot.strafe(0.5)
-        sleep(500)
+        sleep(2500)
         pause()
-        robot.vSlide?.targetPosition = robot.vSlide!!.currentPosition - 100
+        //robot.vSlide?.targetPosition = robot.vSlide!!.currentPosition - 100
 
         //https://www.reddit.com/r/FTC/comments/78l5o0/how_to_program_encoders/
         telemetry.addData("Status: ", "Autonomous Terminated")

06921c5f88996424e1a275f607582a3a1c4d7a84
Newton Busters <newtonbustersftc@gmail.com>
2020-01-27 20:05:06 -0600

    Rename as the AutonomousOptions, remove the old one that uses X,Y button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOptions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOptions.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.util.Arrays;
@@ -12,161 +11,211 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-    @TeleOp(name="Autonomous Options", group="Main")
-    @Disabled
-    public class AutonomousOptions extends OpMode {
+@TeleOp(name="Autonomous Options", group="Main")
 
-        // ADD preference names here
-        public static final String START_POS_MODES_PREF = "starting position";
-        public static final String DELAY_PREF = "delay";
-        public static final String  PARKING_PREF = "parking";
-        private static final String NONE = "none";
-        public static final String FOUNDATION_PREF = "foundation";
-        public static final String DELIVER_ROUTE_PREF = "deliver mode";
-        public static final String PARKING_ONLY_PREF = "only park";
-        public static final String STONE_PREF = "two skystones";
-        public static final String FIRST_BLOCK_BY_WALL_PREF = "first block by wall";
-        public static final String SECOND_SKYSTONE_GROUP_POSITION_PREF = "second group skystone(red or blue 2)";
+public class AutonomousOptions extends OpMode {
 
-        // ADD preference values here
-        public static final String[] START_POS_MODES = {"BLUE_2", "BLUE_3", "BLUE_5", "RED_2", "RED_3", "RED_5"};
-        public static final String[] DELAYS = {"0 " + "sec", "1 sec", "2 sec", "3 sec", "4 sec", "5 sec"};
-        public static final String[] PARKING_LOCATION = {"BridgeWall", "BridgeNeutral"};
-        public static final String[] MOVE_FOUNDATION = {"move", "no move", "move only"};
-        public static final String[] DELIVER_ROUTE = {"BridgeWall", "BridgeNeutral"};
-        public static final String[] PARKING_ONLY = {"yes", "no"};
-        public static final String[] TWO_SKYSTONES = {"yes", "no"};
-        public static final String[] PICK_FIRST_BLOCK_BY_WALL ={"yes", "no"};
-        public static final String[] SECOND_SKYSTONE_GROUP_POSITION ={"yes","no"};
+    // ADD preference names here
+    public static final String START_POS_MODES_PREF = "starting position";
+    public static final String DELAY_PREF = "delay";
+    public static final String  PARKING_PREF = "parking";
+    public static final String FOUNDATION_PREF = "foundation";
+    public static final String DELIVER_ROUTE_PREF = "deliver mode";
+    public static final String PARKING_ONLY_PREF = "only park";
+    public static final String STONE_PREF = "skystones";
+    // ADD preference values here
+    public static final String[] START_POS_MODES = {"BLUE_2", "BLUE_3", "BLUE_5", "RED_2", "RED_3", "RED_5"};
+    public static final String[] DELAYS = {"0 " + "sec", "1 sec", "2 sec", "3 sec", "4 sec", "5 sec"};
+    public static final String[] PARKING_LOCATION = {"BridgeWall", "BridgeNeutral"};
+    public static final String[] MOVE_FOUNDATION = {"move", "no move", "move only"};
+    public static final String[] DELIVER_ROUTE = {"BridgeWall", "BridgeNeutral"};
+    public static final String[] PARKING_ONLY = {"yes", "no"};
+    public static final String[] STONE_OPTIONS = {"no", "group1","group2", "both"};
+    public static final String[] PICK_FIRST_BLOCK_BY_WALL ={"yes", "no"};
+    private static final String NONE = "none";
+    public static Map<String, String[]> prefMap = new HashMap<>();
+    private static String[] prefKeys = {START_POS_MODES_PREF,STONE_PREF, DELAY_PREF, DELIVER_ROUTE_PREF,FOUNDATION_PREF,PARKING_ONLY_PREF,
+            PARKING_PREF};
+    private static int keyIdx = 0;
 
-        public static Map<String, String[]> prefMap = new HashMap<>();
-        static {
-            // ADD entries to preference map here
-            prefMap.put(DELAY_PREF, DELAYS);
-            prefMap.put(START_POS_MODES_PREF, START_POS_MODES);
-            prefMap.put(PARKING_PREF, PARKING_LOCATION);
-            prefMap.put(FOUNDATION_PREF, MOVE_FOUNDATION);
-            prefMap.put(DELIVER_ROUTE_PREF, DELIVER_ROUTE);
-            prefMap.put(PARKING_ONLY_PREF, PARKING_ONLY);
-            prefMap.put(STONE_PREF, TWO_SKYSTONES);
-            prefMap.put(FIRST_BLOCK_BY_WALL_PREF, PICK_FIRST_BLOCK_BY_WALL);
-            prefMap.put(SECOND_SKYSTONE_GROUP_POSITION_PREF,SECOND_SKYSTONE_GROUP_POSITION);
+    //private static String[] prefKeys = prefMap.keySet().toArray(new String[prefMap.keySet().size()]);
 
-        }
-        private static String[] prefKeys = prefMap.keySet().toArray(new String[prefMap.keySet().size()]);
-        private static int keyIdx = 0;
-
-
-        static {
-            Arrays.sort(prefKeys);
-        }
-
-        public boolean isXPressed;
-        public boolean isYPressed;
-        private State menuState = State.DisplayAll;
-        private SharedPreferences prefs;
-        private SharedPreferences.Editor editor;
-        private int selectionIdx = 0;
-
-        public static SharedPreferences getSharedPrefs(HardwareMap hardwareMap) {
-            return hardwareMap.appContext.getSharedPreferences("autonomous", 0);
-        }
-
-        private int getIndex(String val, String[] array) {
-            if(array!=null){
-                for (int i = 0; i < array.length; i++) {
-                    if (array[i].equals(val)) {
-                        return i;
-                    }
-                }
-            }
-            return -1;
-        }
-
-        @Override
-        public void init() {
-            prefs = getSharedPrefs(hardwareMap);
-            editor = prefs.edit();
-            editor.apply();
-            for (String key : prefs.getAll().keySet()) {
-                telemetry.addData(key, prefs.getString(key, NONE));
-            }
-        }
-
-        @Override
-        public void loop() {
-            switch (menuState) {
-                case DisplayAll:
-                    displayAll();
-                    break;
-                case DisplaySingle:
-                    displaySingle();
-                    break;
-            }
-        }
-
-        private void displayAll () {
-            telemetry.clear();
-            telemetry.addData("Choose", "X - accept | Y - change");
-//            for (String key : prefKeys) {
-//                telemetry.addData(key, prefs.getString(key, NONE));
-//            }
-            for (String key : prefs.getAll().keySet()) {
-                telemetry.addData(key, prefs.getString(key, NONE));
-            }
-            if (gamepad1.y && !isYPressed) {
-                isYPressed = true;
-                menuState = State.DisplaySingle;
-            }
-            if (!gamepad1.y) {
-                isYPressed = false;
-            }
-        }
-
-        private void displaySingle () {
-            telemetry.clear();
-            telemetry.addData("Choose", "X - accept Y - change");
-            String key = prefKeys[keyIdx];
-            String[] array = prefMap.get(key);
-
-            if (key != null) {
-                String prefValue = prefs.getString(key, NONE);
-                selectionIdx = getIndex(prefValue, array);
-                telemetry.addData(key, "***" + prefValue);
-                updateTelemetry(telemetry);
-            }
-            //accept and no change in current key, move to next key
-            if (gamepad1.x && !isXPressed) {
-                int nextKeyIdx = keyIdx+1;
-                if (nextKeyIdx >= prefKeys.length && gamepad1.x) {
-                    keyIdx = 0;
-                    menuState = State.DisplayAll;
-                }else {
-                    keyIdx = nextKeyIdx;
-                }
-                isXPressed = true;
-            }
-            if (!gamepad1.x) {
-                isXPressed = false;
-            }
-
-            //change to next idx in array
-           if (gamepad1.y && !isYPressed) {
-                selectionIdx++;  //value[] idx
-                if (selectionIdx >= array.length && gamepad1.y) {
-                    selectionIdx = 0;
-                }
-                editor.putString(key, array[selectionIdx]);
-                editor.apply();
-                isYPressed = true;
-            }
-           if (!gamepad1.y) {
-               isYPressed = false;
-           }
-        }
-
-        enum State  {DisplayAll, DisplaySingle}
+    static {
+        // ADD entries to preference map here
+        prefMap.put(DELAY_PREF, DELAYS);
+        prefMap.put(START_POS_MODES_PREF, START_POS_MODES);
+        prefMap.put(PARKING_PREF, PARKING_LOCATION);
+        prefMap.put(FOUNDATION_PREF, MOVE_FOUNDATION);
+        prefMap.put(DELIVER_ROUTE_PREF, DELIVER_ROUTE);
+        prefMap.put(PARKING_ONLY_PREF, PARKING_ONLY);
+        prefMap.put(STONE_PREF, STONE_OPTIONS);
     }
+
+//    static {
+//        Arrays.sort(prefKeys);
+//    }
+
+    public boolean isUpPressed;
+    public boolean isDownPressed;
+    public boolean isRightPressed;
+    public boolean isLeftPressed;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+    private int selectionIdx = 0;
+
+    public static SharedPreferences getSharedPrefs(HardwareMap hardwareMap) {
+        return hardwareMap.appContext.getSharedPreferences("autonomous", 0);
+    }
+
+    private int getIndex(String val, String[] array) {
+        if (array!=null) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].equals(val)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void init() {
+        prefs = getSharedPrefs(hardwareMap);
+        editor = prefs.edit();
+        editor.apply();
+        for (int i = 0; i < prefKeys.length; i++ ) {
+            telemetry.addData(prefKeys[i], prefs.getString(prefKeys[i], NONE));
+        }
+    }
+
+    @Override
+    public void loop() {
+        displayAll();
+    }
+
+    private void displayAll () {
+        telemetry.clear();
+//        telemetry.addData("Choose", "X - accept | Y - change");
+//        for (String key : prefs.getAll().keySet()) {
+//            telemetry.addData(key, prefs.getString(key, NONE));
+//            if(keyIdx == )
+//        }
+
+        for (int i = 0; i < prefKeys.length; i++ ) {
+
+            if (keyIdx == i) {
+                String cap = prefKeys[i].toUpperCase();
+                telemetry.addData("*" + cap + "*", prefs.getString(prefKeys[i], NONE));
+            } else {
+                telemetry.addData(prefKeys[i], prefs.getString(prefKeys[i], NONE));
+            }
+        }
+
+        String key = prefKeys[keyIdx];
+        String[] array = prefMap.get(key);
+
+        if (key != null) {
+            String prefValue = prefs.getString(key, NONE);
+            selectionIdx = getIndex(prefValue, array);
+            updateTelemetry(telemetry);
+        }
+        //accept and no change in current key, move to next key
+        if (gamepad1.dpad_down && !isDownPressed) {
+            int nextKeyIdx = keyIdx+1;
+
+            if (nextKeyIdx >= prefKeys.length && gamepad1.dpad_down) {
+                keyIdx = 0;
+            } else {
+                keyIdx = nextKeyIdx;
+            }
+
+            isDownPressed = true;
+        }
+
+        if (!gamepad1.dpad_down) {
+            isDownPressed = false;
+        }
+
+        if (gamepad1.dpad_up && !isUpPressed) {
+            int nextKeyIdx = keyIdx - 1;
+
+            if (nextKeyIdx <= 0 && gamepad1.dpad_up) {
+                keyIdx = prefKeys.length - 1;
+            } else {
+                keyIdx = nextKeyIdx;
+            }
+
+            isUpPressed = true;
+        }
+
+        if (!gamepad1.dpad_up) {
+            isUpPressed = false;
+        }
+
+        //change to next idx in array
+        if (gamepad1.dpad_right && !isRightPressed) {
+            selectionIdx++;  //value[] idx
+
+            if (selectionIdx >= array.length && gamepad1.dpad_right) {
+                selectionIdx = 0;
+            }
+
+            editor.putString(key, array[selectionIdx]);
+            updateAutoPref(key, array[selectionIdx]);
+            editor.apply();
+            isRightPressed = true;
+        }
+
+        if (!gamepad1.dpad_right) {
+            isRightPressed = false;
+        }
+
+        if (gamepad1.dpad_left && !isLeftPressed) {
+            selectionIdx--;  //value[] idx
+
+            if (selectionIdx <= 0 && gamepad1.dpad_left) {
+                selectionIdx = array.length-1;
+            }
+
+            editor.putString(key, array[selectionIdx]);
+            updateAutoPref(key, array[selectionIdx]);
+            editor.apply();
+            isLeftPressed = true;
+        }
+
+        if (!gamepad1.dpad_left) {
+            isLeftPressed = false;
+        }
+//        if (gamepad1.y && !isYPressed) {
+//            isYPressed = true;
+//            menuState = State.DisplaySingle;
+//        }
+//        if (!gamepad1.y) {
+//            isYPressed = false;
+//        }
+    }
+    void updateAutoPref(String key, String value) {
+        if (key.equals(START_POS_MODES_PREF)) {
+            if (value.equals("RED_5") || value.equals("BLUE_5")) {
+                editor.putString(STONE_PREF, "no");
+            }
+            if(value.equals("RED_3") || value.equals("BLUE_3")){
+                editor.putString(STONE_PREF, "group2");
+            }
+            if (value.equals("RED_2") || value.equals("BLUE_2")) {
+                editor.putString(STONE_PREF, "group1"); // default
+            }
+        }
+        if(key.equals(PARKING_ONLY_PREF) && value.equals("yes")) {
+            editor.putString(STONE_PREF, "no");
+            editor.putString(FOUNDATION_PREF, "no move");
+        }
+        if(key.equals(FOUNDATION_PREF) && value.equals("move only")) {
+            editor.putString(STONE_PREF, "no");
+        }
+
+    }
+}
 
 
 

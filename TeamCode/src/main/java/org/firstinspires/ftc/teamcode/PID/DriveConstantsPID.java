d1f59d15edf1729adccc11014516d7dd53885b24
wangx <wxiaodong@gmail.com>
2020-01-28 03:05:09 -0800

    not using properties

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/DriveConstantsPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/DriveConstantsPID.java
@@ -32,12 +32,12 @@ import java.util.List;
 @Config
 public class DriveConstantsPID {
 
-    public static final boolean RUN_USING_PARAMTER_FROM_PROPERTIES = true;
+    public static final boolean RUN_USING_PARAMTER_FROM_PROPERTIES = false;
 
     public static boolean RUN_USING_ODOMETRY_WHEEL = true;
     public static boolean RUN_USING_IMU_LOCALIZER = true;
     public static boolean BRAKE_ON_ZERO = false;
-    public static boolean USING_BULK_READ = false;
+    public static boolean USING_BULK_READ = true;
     public static boolean USING_STRAFE_DIAGNAL = true;
     public static double odoEncoderTicksPerRev = 1565.0;
     public static double imuPollingInterval = 10;

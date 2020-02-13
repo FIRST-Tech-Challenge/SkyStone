0a820a8eb5e6336c4883e2836cbb5c10320ff2a7
Albert Cai <albertcai101@alberts-mbp.lan>
2020-02-13 12:31:53 -0800

    small angle odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Odometry/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Odometry/Odometry.java
@@ -31,51 +31,13 @@ public class Odometry {
 
     double moveScaleFactor = (0.004177098/125) * 123;
     double turnScaleFactor = 0.000301686*240;
-    double strafeScaleFactor = 0.004135465;
-    double strafePredictionScalingFactor = 0.092;
+    double strafePredictionFactor = 0.092;
 
 //    double encoderToInches = ;
 
     public Odometry() {
 
     }
-
-
-//    double oldLEncoder;
-//    double oldREndcoder;
-//    double oldSEncoder;
-//
-//    public void odometry() {
-//        double newLEncoder = -1 * robot.getfLeft().getCurrentPosition();
-//        double newREncoder = -1 * robot.getfRight().getCurrentPosition();
-//        double newSEncoder = -1 * robot.getbLeft().getCurrentPosition();
-//
-//        double deltaL = (newLEncoder - oldLEncoder) * encoderToInches;
-//        double deltaR = (newREncoder - oldREndcoder) * encoderToInches;
-//        double deltaS = (newSEncoder - oldSEncoder) * encoderToInches;
-//
-//        double totalL = newLEncoder * encoderToInches;
-//        double totalR = newREncoder * encoderToInches;
-//        double totalS = newLEncoder * encoderToInches;
-//
-//        oldLEncoder = newLEncoder;
-//        oldREndcoder = newREncoder;
-//        oldSEncoder = newSEncoder;
-//
-//        worldAngle = (totalL - totalR)/13.5;
-//        double deltaAngle = (deltaL - deltaR)/13.5;
-//
-//        double localX;
-//        double localY;
-//
-//        if (deltaAngle == 0){
-//            localY = deltaS;
-//            localX = deltaR;
-//        } else {
-//            localY = 2 * Math.sin()
-//        }
-//
-//    }
 
     public void circularOdometry (Robot robot) {
         double leftPodNew = -1 * robot.getfLeft().getCurrentPosition(); // fix this for new odo config
@@ -129,13 +91,31 @@ public class Odometry {
         worldY += dXPrime * Math.sin(midAngle) + dYPrime * Math.cos(midAngle);
     }
 
+    public void smallAngleOdometry (double dLeftPod, double dRightPod, double dMecanumPod) {
+        double dLeftPodInches = dLeftPod * moveScaleFactor;
+        double dRightPodInches = dRightPod * moveScaleFactor;
+        double dMecanumPodInches = dMecanumPod * moveScaleFactor;
+
+        worldAngle = (((dLeftPod+oldLeftPod) * moveScaleFactor) - ((dRightPod + oldRightPod) * moveScaleFactor)) * turnScaleFactor;
+
+        double dAngle = (dLeftPodInches - dRightPodInches) * turnScaleFactor;
+        double dRobotX = (dLeftPodInches + dRightPodInches) * .5;
+        double dRobotY = dMecanumPodInches + strafePredictionFactor * dAngle;
+
+
+        // Update world x and y positions of the robot by converting from robot's x'y' coordinate
+        // system to the global xy coordinate system
+        worldX += dRobotX * Math.cos(worldAngle + dAngle * 0.5) - dRobotY * Math.sin(worldAngle + dAngle * 0.5);
+        worldY += dRobotX * Math.sin(worldAngle + dAngle * 0.5) + dRobotY * Math.cos(worldAngle + dAngle * 0.5);
+    }
+
     private double circularOdometrySinXOverX(double x) {
         if (Math.abs(x) < .00005) { // If the ratio is close enough to the limit, make it the limit
             return 1;
         } else {
             return Math.sin(x) / x;
         }
-    };
+    }
 
     public void linearOdometry (Robot robot) {
         double leftPodNew = -1 * robot.getfLeft().getCurrentPosition(); // fix this for new odo config
@@ -166,45 +146,6 @@ public class Odometry {
         worldX += dXPrime * Math.cos(midAngle) + dYPrime * Math.sin(midAngle);
         worldY += -1 * dXPrime * Math.sin(midAngle) + dYPrime * Math.cos(midAngle);
     }
-
-//    public void constantVelocityOdometry() {
-//
-//        double fLeftNEW = robot.getfLeft().getCurrentPosition();
-//        double fRightNEW = robot.getfRight().getCurrentPosition();
-//        double bLeftNEW = robot.getbLeft().getCurrentPosition();
-//        double bRightNEW = robot.getbRight().getCurrentPosition();
-//
-//        // find robot position
-//        double fl = 2 * Math.PI * (fLeftNEW - fLeftOLD) / robot.getEncoderPerRevolution();
-//        double fr = 2 * Math.PI * (fRightNEW - fRightOLD) / robot.getEncoderPerRevolution();
-//        double bl = 2 * Math.PI * (bLeftNEW - bLeftOLD) / robot.getEncoderPerRevolution();
-//        double br = 2 * Math.PI * (bRightNEW - bRightOLD) / robot.getEncoderPerRevolution();
-//
-//        double xDeltaRobot = robot.getWheelRadius()/4 * (fl + bl + br + fr);
-//        double yDeltaRobot = robot.getWheelRadius()/4 * (-fl + bl - br + fr);
-//        double angleDeltaRobot = robot.getWheelRadius()/4 *(-fl/(robot.getL()+robot.getW()) - bl/(robot.getL()+robot.getW()) + br/(robot.getL()+robot.getW()) + fr/(robot.getL()+robot.getW()));
-//
-//        //converting to global frame
-//        if (angleDeltaRobot == 0){
-//            xPosGlobal += xDeltaRobot * Math.cos(angleGlobal) - yDeltaRobot * Math.sin(angleGlobal);
-//            yPosGlobal += xDeltaRobot * Math.sin(angleGlobal) + yDeltaRobot * Math.cos(angleGlobal);
-//        } else {
-//            xPosGlobal += (Math.cos(angleGlobal) * Math.sin(angleDeltaRobot) - (Math.cos(angleDeltaRobot) - 1) * Math.sin(angleGlobal)) * xDeltaRobot / angleDeltaRobot + (Math.cos(angleGlobal) * (Math.cos(angleDeltaRobot) - 1) - Math.sin(angleGlobal) * Math.sin(angleDeltaRobot)) * yDeltaRobot / angleDeltaRobot;
-//            yPosGlobal += ((Math.cos(angleDeltaRobot) - 1) * Math.sin(angleGlobal) + (Math.cos(angleGlobal)) * Math.sin(angleDeltaRobot)) * yDeltaRobot / angleDeltaRobot + (Math.cos(angleGlobal) * (Math.cos(angleDeltaRobot) - 1) + Math.sin(angleGlobal) * Math.sin(angleDeltaRobot)) * xDeltaRobot / angleDeltaRobot;
-//        }
-//
-//        angleGlobal = angleWrap((robot.getWheelCircumference() * (fLeftNEW)/robot.getEncoderPerRevolution() - robot.getWheelCircumference() * (fRightNEW)/robot.getEncoderPerRevolution()) / 14 * 0.51428571428);
-//
-//        fLeftOLD = fLeftNEW;
-//        fRightOLD = fRightNEW;
-//        bLeftOLD = bLeftNEW;
-//        bRightOLD = bRightNEW;
-//
-//        robot.getTelemetry().addLine("XPOS: " + xPosGlobal);
-//        robot.getTelemetry().addLine("YPOS: " + yPosGlobal);
-//        robot.getTelemetry().addLine("ANGPOS: " + Math.toDegrees(Math.toDegrees(angleGlobal)));
-//        robot.getTelemetry().update();
-//    }
 
     public void resetOdometry(){
         xPosGlobal = 0;

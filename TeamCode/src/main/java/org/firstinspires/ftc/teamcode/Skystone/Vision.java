e962a927459f9e66b1e53f3b3949bbff8d52f90b
Ethan <e3lcool@gmail.com>
2020-02-23 15:36:29 -0800

    cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/Vision.java
@@ -32,7 +32,7 @@ public class Vision {
     private final String VUFORIA_KEY = "AbSCRq//////AAAAGYEdTZut2U7TuZCfZGlOu7ZgOzsOlUVdiuQjgLBC9B3dNvrPE1x/REDktOALxt5jBEJJBAX4gM9ofcwMjCzaJKoZQBBlXXxrOscekzvrWkhqs/g+AtWJLkpCOOWKDLSixgH0bF7HByYv4h3fXECqRNGUUCHELf4Uoqea6tCtiGJvee+5K+5yqNfGduJBHcA1juE3kxGMdkqkbfSjfrNgWuolkjXR5z39tRChoOUN24HethAX8LiECiLhlKrJeC4BpdRCRazgJXGLvvI74Tmih9nhCz6zyVurHAHttlrXV17nYLyt6qQB1LtVEuSCkpfLJS8lZWS9ztfC1UEfrQ8m5zA6cYGQXjDMeRumdq9ugMkS";
 
     public enum Location {
-        CENTER, LEFT, RIGHT, UNKNOWN;
+        CENTER, LEFT, RIGHT, UNKNOWN
     }
 
     File captureDirectory = AppUtil.ROBOT_DATA_DIR;

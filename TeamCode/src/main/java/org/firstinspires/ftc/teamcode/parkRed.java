b80ca6360583ed0b503bb888b84f1eaa976caa50
ayuram <54257595+ayuram@users.noreply.github.com>
2020-10-11 16:15:07 -0700

    k

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parkRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parkRed.java
@@ -6,19 +6,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
 public class parkRed extends LinearOpMode {
-    AutonomousMethods bot;
+    autoCopy bot;
 
     public void runOpMode() throws InterruptedException {
 
-
-    bot = new AutonomousMethods(DcMotor.RunMode.RUN_USING_ENCODER, hardwareMap);
-
-    bot.locate(false, false, false);
-    waitForStart();
-    bot.initialDown();
-    sleep(2000);
-    bot.armIn();
-    sleep(1000);
-    bot.forward(0.5, 18);
 }
 }
